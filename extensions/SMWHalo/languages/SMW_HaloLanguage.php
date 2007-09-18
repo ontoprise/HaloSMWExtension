68fc42f79e26c1fd8f373dc1eddb1e0e5a1badc8
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2007-09-18 08:36:35 +0000

    added getAllDataTypeLabels()
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@21 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/languages/SMW_HaloLanguage.php
+++ extensions/SMWHalo/languages/SMW_HaloLanguage.php
@@ -27,6 +27,17 @@ abstract class SMW_HaloLanguage {
 	}
 
 	/**
+	 * Return all labels that are available as names for built-in datatypes. Those
+	 * are exactly the types that users can access via [[has type::...]] (more
+	 * built-in types may exist for internal purposes but the user won't need to
+	 * know this).
+	 */
+	function getAllDatatypeLabels() {
+		return $this->smwDatatypeLabels;
+	}
+
+
+	/**
 	 * Find the internal message id of some localised message string
 	 * for a datatype. If no type of the given name exists (maybe a 
 	 * custom of compound type) then FALSE is returned.
