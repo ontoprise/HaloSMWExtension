3ad95d68613e7b1f3a06f60aee94b2761fe8ae22
halo12 <halo12@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2007-09-27 09:43:41 +0000

    deploy version and tools
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@141 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/SMWHalo/maintenance/makeBuildWin.bat
@@ -0,0 +1,39 @@
+@ECHO OFF
+
+REM Windows batch file for creating SMW Halo deploy version with SMW/MW patches
+
+set OUTPUT_DIR=c:\temp\halosmw
+IF NOT EXIST %OUTPUT_DIR% goto CREATEDIRS
+
+rm -r -f %OUTPUT_DIR%
+
+REM Create directories
+
+:CREATEDIRS
+mkdir %OUTPUT_DIR%
+mkdir %OUTPUT_DIR%\skins\common
+mkdir %OUTPUT_DIR%\skins\ontoskin
+mkdir %OUTPUT_DIR%\includes
+
+mkdir %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes
+mkdir %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes\storage
+mkdir %OUTPUT_DIR%\extensions\SMWHalo
+
+REM copy files
+
+xcopy ..\* %OUTPUT_DIR%\extensions\SMWHalo /S /EXCLUDE:excludeFiles.dat /Y
+xcopy ..\..\SemanticMediaWiki\includes\SMW_QueryProcessor.php %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes /Y
+xcopy ..\..\SemanticMediaWiki\includes\storage\SMW_SQLStore.php %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes\storage /Y
+xcopy ..\..\..\includes\User.php %OUTPUT_DIR%\includes /Y
+xcopy ..\..\..\skins\common\ajax.js %OUTPUT_DIR%\skins\common /Y
+xcopy ..\..\..\skins\ontoskin %OUTPUT_DIR%\skins\ontoskin /S /EXCLUDE:excludeFiles.dat /Y
+
+REM Pack SMWHalo Extension
+
+cd bin
+7z.exe a halosmw.zip %OUTPUT_DIR%\*
+cd..
+
+REM Remove temp files
+
+rm -r -f %OUTPUT_DIR%
