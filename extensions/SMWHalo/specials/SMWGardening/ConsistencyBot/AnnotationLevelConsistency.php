f87f430455a1fc2aa3cce7e305d503f692176af5
halo12 <halo12@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2007-11-15 15:25:43 +0000

    bugfix
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@435 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/specials/SMWGardening/ConsistencyBot/AnnotationLevelConsistency.php
+++ extensions/SMWHalo/specials/SMWGardening/ConsistencyBot/AnnotationLevelConsistency.php
@@ -282,10 +282,10 @@ require_once("$smwgHaloIP/includes/SMW_GraphHelper.php");
 	 				
 	 				// compare number of appearance with defined cardinality
 	 				if ($num < $minCards) {
-	 					$this->gi_store->addGardeningIssueAboutArticles($this->bot->getBotID(), SMW_GARDISSUE_TOO_LOW_CARD, $subject, $a, $num);
+	 					$this->gi_store->addGardeningIssueAboutArticles($this->bot->getBotID(), SMW_GARDISSUE_TOO_LOW_CARD, $subject[0], $a, $num);
 					} 
 					if ($num > $maxCards) {
-						$this->gi_store->addGardeningIssueAboutArticles($this->bot->getBotID(), SMW_GARDISSUE_TOO_HIGH_CARD, $subject, $a, $num);
+						$this->gi_store->addGardeningIssueAboutArticles($this->bot->getBotID(), SMW_GARDISSUE_TOO_HIGH_CARD, $subject[0], $a, $num);
 					}
  				}
  			}
