325b1992b9333904f8098054c48be34d1a13c522
halo12 <halo12@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2007-12-03 14:30:55 +0000

    config page implemented
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@532 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/config/index.js
+++ extensions/SMWHalo/config/index.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	var sc = document.getElementById('collation');
-	sc.setAttribute("disabled", "disabled");
+	//sc.setAttribute("disabled", "disabled");
 	document.getElementById('sc').onclick = function(event) {
 		
 		if (sc.hasAttribute('disabled')) {
