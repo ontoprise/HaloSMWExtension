2fe1b835f06ea8c8889e6e98ceec720cc6a5554d
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-01-18 13:35:46 +0000

    added adaptive job rate
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@713 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/maintenance/SMW_runJobsInBackground.php
+++ extensions/SMWHalo/maintenance/SMW_runJobsInBackground.php
@@ -32,11 +32,22 @@
  $socket = socket_create_listen("9876"); // port is freely chosen
  socket_set_nonblock($socket);
  
- print "\nRunning jobs... (rate is: $rate jobs/second)\n";
+ define('MAX_THREADS_CONSIDERED', 10);
+ 
+ print "-------------------------------------------------\n";
+ print " Running jobs... ($rate jobs/second)    		 \n";
+ print "-------------------------------------------------\n";
 	for (;;) {
-		sleep(1/$rate);
+		
+		// make sleeping time adaptive to database load.
+		$currentThreadNum = smwfGetDBUserThreadNum($dbw);
+		$currentThreadNum = $currentThreadNum <= MAX_THREADS_CONSIDERED ? 
+							$currentThreadNum : MAX_THREADS_CONSIDERED;
+		
+		sleep(1/$rate + $currentThreadNum);
 		
 		$job = Job::pop();
+		
 		
 		$accept_sock = @socket_accept($socket);	
 		if ($accept_sock !== false) {
@@ -59,4 +70,25 @@
 		
 	}
 
+/**
+ * Returns number of threads created by $wgDBuser
+ */
+function smwfGetDBUserThreadNum(& $db) {
+	global $wgDBuser;
+	$count = 0;
+	$res = $db->query( 'SHOW PROCESSLIST' );
+		# Find slave SQL thread
+		while ( $row = $db->fetchObject( $res ) ) {
+			/* This should work for most situations - when default db 
+			 * for thread is not specified, it had no events executed, 
+			 * and therefore it doesn't know yet how lagged it is.
+			 *
+			 * Relay log I/O thread does not select databases.
+			 */
+			if ( $row->User == $wgDBuser ) {
+				$count++;
+			}
+		}
+		return $count;
+}
 ?>
