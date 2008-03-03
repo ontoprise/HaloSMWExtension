10cbca698728a729af4be273db09b71ab84d2766
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-03-03 09:19:14 +0000

    fix
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@905 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/specials/SMWFindWork/SMW_SuggestStatisticsSQL.php
+++ extensions/SMWHalo/specials/SMWFindWork/SMW_SuggestStatisticsSQL.php
@@ -6,6 +6,7 @@
  */
  global $smwgHaloIP;
  require_once($smwgHaloIP . '/specials/SMWFindWork/SMW_SuggestStatistics.php');
+ require_once($smwgHaloIP . '/includes/SMW_DBHelper.php');
  
  class SMWSuggestStatisticsSQL extends SMWSuggestStatistics {
  	
