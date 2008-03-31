4558868d83fda3256457dd99251a258f29ad164b
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-03-31 16:56:46 +0000

    fixes
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@969 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests/createSPARQLQueries.php
+++ extensions/SMWHalo/tests/createSPARQLQueries.php
@@ -14,6 +14,8 @@
  * Author: kai
  */
 
+define('USE_PREFIX', "_");
+
 define('USE_LEAF_QUERIES_NUM', 20);
 define('USE_LEAF_PROPERTIES_NUM', 20);
 define('MAX_PROPS_IN_QUERY', 4);
@@ -97,6 +99,7 @@ function serializeQueriesToXML($filename, $queries) {
  	foreach($q_array as $q) {
  		$q = str_replace("<", "&lt;", $q);
  		$q = str_replace(">", "&gt;", $q);
+ 		$q = str_replace("\"", "&quot;", $q);
  		$q = str_replace("\n", " ", $q);
  		if ($q == "") continue;
  		$xml_result .= "<testCase name=\"Query".$i."\">\n";
@@ -112,9 +115,10 @@ function serializeQueriesToXML($filename, $queries) {
 }
 
 function getStandardPrefixes() {
-	return "PREFIX cat:  <http://halowiki.org/category#>\n".
-	       "PREFIX prop:  <http://halowiki.org/property#>\n".
-	       "PREFIX a: <http://www.halowiki.org#>\n";
+	return "PREFIX cat:  <http://www.halowiki.org/category#>\n".
+	       "PREFIX prop:  <http://www.halowiki.org/property#>\n".
+	       "PREFIX a: <http://www.halowiki.org#>\n".
+	       "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n ";
 }
 
 function getVar($j) {
@@ -140,7 +144,7 @@ function createCategoryQueries() {
             
         do {
             $superCat = !empty($superCats) ? $superCats[0] : $leaf;
-            $categoryQueries[] = $prefixes."SELECT ?x WHERE { ?x rdf:type cat:".$superCat->getDBkey().". }";
+            $categoryQueries[] = $prefixes."SELECT ?x WHERE { ?x rdf:type cat:".USE_PREFIX.$superCat->getDBkey()." . }";
             
             $superCats = smwfGetSemanticStore()->getDirectSuperCategories($superCat);
         } while (!empty($superCats));
@@ -166,12 +170,12 @@ function createCategoryQueries() {
             $j = 0;
             foreach($properties as $prop) {
                 list($p, $minCard, $maxCard, $type, $symCat, $transCat, $range) = $prop;
-                $property_restr .= "?x ".$p->getDBkey()." ".getVar($j).".\n";
+                $property_restr .= "?x prop:".USE_PREFIX.$p->getDBkey()." ".getVar($j).".\n";
                 if ($j >= MAX_PROPS_IN_QUERY) break;
                 $j++;
             }
             
-            $categoryQueries[] = $prefixes."SELECT ?x ".getVarString(0, $j)." WHERE { ?x rdf:type cat:".$superCat->getDBkey().".\n ".$property_restr." }";
+            $categoryQueries[] = $prefixes."SELECT ?x ".getVarString(0, $j-1)." WHERE { ?x rdf:type cat:".USE_PREFIX.$superCat->getDBkey()." .\n ".$property_restr." }";
             $superCats = smwfGetSemanticStore()->getDirectSuperCategories($superCat);
         } while (!empty($superCats));
         $i++;
@@ -196,12 +200,12 @@ function createCategoryQueries() {
             $j = 0;
             foreach($properties as $prop) {
                 list($p, $minCard, $maxCard, $type, $symCat, $transCat, $range) = $prop;
-                $property_restr .= " OPTIONAL {?x ".$p->getDBkey()." ".getVar($j)."}. \n";
+                $property_restr .= " OPTIONAL {?x prop:".USE_PREFIX.$p->getDBkey()." ".getVar($j)."}. \n";
                 if ($j >= MAX_PROPS_IN_QUERY) break;
                 $j++;
             }
             
-            $categoryQueries[] = $prefixes."SELECT ?x ".getVarString(0, $j)." WHERE { ?x rdf:type cat:".$superCat->getDBkey().".\n ".$property_restr." }";
+            $categoryQueries[] = $prefixes."SELECT ?x ".getVarString(0, $j-1)." WHERE { ?x rdf:type cat:".USE_PREFIX.$superCat->getDBkey()." .\n ".$property_restr." }";
             $superCats = smwfGetSemanticStore()->getDirectSuperCategories($superCat);
         } while (!empty($superCats));
         $i++;
@@ -226,12 +230,13 @@ function createCategoryPropertyQueriesUnion() {
             $j = 0;
             foreach($properties as $prop) {
                 list($p, $minCard, $maxCard, $type, $symCat, $transCat, $range) = $prop;
-                $property_restr .= " UNION { ?x rdf:type cat:".$superCat->getDBkey().". ?x ".$p->getDBkey()." ".getVar($j)."} \n";
+                $property_restr .= ($j > 0 ? " UNION " : " ")." { ?x rdf:type cat:".USE_PREFIX.$superCat->getDBkey()." . ?x prop:".USE_PREFIX.$p->getDBkey()." ".getVar($j)."} \n";
                 if ($j >= MAX_PROPS_IN_QUERY) break;
                 $j++;
             }
-            
-            $categoryQueries[] = $prefixes."SELECT ?x ".getVarString(0, $j)." WHERE { ".$property_restr." }";
+            if ($j >= 2) {
+                $categoryQueries[] = $prefixes."SELECT ?x ".getVarString(0, $j-1)." WHERE { ".$property_restr." }";
+            }
             $superCats = smwfGetSemanticStore()->getDirectSuperCategories($superCat);
         } while (!empty($superCats));
         $i++;
@@ -258,15 +263,15 @@ function createCategoryPropertyQueriesUnion() {
             foreach($annotationValues as $values) {
                 list($smwValue, $p) = $values;
                 if ($smwValue instanceof SMWNumberValue) {
-                    $property_restr .= " ?x prop:".$p->getDBkey()." ".getVar($j).". FILTER(".getVar($j)." > 50)\n";
+                    $property_restr .= " ?x prop:".USE_PREFIX.$p->getDBkey()." ".getVar($j)." . FILTER(".getVar($j)." > 50)\n";
                 } else {
-                    $property_restr .= " ?x prop:".$p->getDBkey()." ".getVar($j).". FILTER(".getVar($j)." = \"".$smwValue->getXSDValue()."\")\n";
+                    $property_restr .= " ?x prop:".USE_PREFIX.$p->getDBkey()." ".getVar($j)." . FILTER(".getVar($j)." = \"".$smwValue->getXSDValue()."\")\n";
                    
                 }
                 $j++;
             }
             if ($property_restr == '') continue;
-            $categoryQueries[] = $prefixes."SELECT ?x ".getVarString(0, $j-1)." WHERE { ?x rdf:type cat:".$superCat->getDBkey().".\n ".$property_restr." }";
+            $categoryQueries[] = $prefixes."SELECT ?x ".getVarString(0, $j-1)." WHERE { ?x rdf:type cat:".USE_PREFIX.$superCat->getDBkey()." .\n ".$property_restr." }";
             $superCats = smwfGetSemanticStore()->getDirectSuperCategories($superCat);
             $maxTry--;
         } while (!empty($superCats) && $maxTry > 0);
@@ -294,9 +299,9 @@ function createPropertyWithConstraintQueries() {
             foreach($annotationValues as $values) {
                 list($smwValue, $p) = $values;
                 if ($smwValue instanceof SMWNumberValue) {
-                    $property_restr .= " ?x prop:".$p->getDBkey()." ".getVar($j).". FILTER(".getVar($j)." > 50)\n";
+                    $property_restr .= " ?x prop:".USE_PREFIX.$p->getDBkey()." ".getVar($j)." . FILTER(".getVar($j)." > 50)\n";
                 } else {
-                    $property_restr .= " ?x prop:".$p->getDBkey()." ".getVar($j).". FILTER(".getVar($j)." = \"".$smwValue->getXSDValue()."\")\n";
+                    $property_restr .= " ?x prop:".USE_PREFIX.$p->getDBkey()." ".getVar($j)." . FILTER(".getVar($j)." = \"".$smwValue->getXSDValue()."\")\n";
                    
                 }
                 $j++;
@@ -324,7 +329,7 @@ function createPropertyWithConstraintQueries() {
             $maxTry = 10;
         do {
             $superProp = $superProp == NULL ? $superProps[0] : $superProp;
-            $propertyQueries[] = $prefixes."SELECT ?x WHERE {?x prop:".$superProp->getDBkey()." ".getVar(0).". }";
+            $propertyQueries[] = $prefixes."SELECT ?x WHERE {?x prop:".USE_PREFIX.$superProp->getDBkey()." ".getVar(0)." . }";
             
             $superProps = smwfGetSemanticStore()->getDirectSuperProperties($superProp);
             $superProp = NULL;
@@ -345,10 +350,10 @@ function createPropertyWithConstraintQueries() {
  	  printProgress($i / DEFAULT_QUERY_NUM);
       list($a, $v) = getArbitraryAttributeAnnotation();
       if (is_numeric($v)) {
-      	 $literalMatchingQueries[] = $prefixes."SELECT ?x WHERE {?x prop:".$a->getDBkey()." \"$v\"^^<http://www.w3.org/2001/XMLSchema#float>. }";
+      	 $literalMatchingQueries[] = $prefixes."SELECT ?x WHERE {?x prop:".USE_PREFIX.$a->getDBkey()." \"$v\"^^<http://www.w3.org/2001/XMLSchema#float> . }";
       } 
       else {
-      	 $literalMatchingQueries[] = $prefixes."SELECT ?x WHERE {?x prop:".$a->getDBkey()." \"$v\". }";
+      	 $literalMatchingQueries[] = $prefixes."SELECT ?x WHERE {?x prop:".USE_PREFIX.$a->getDBkey()." \"$v\" . }";
       }
  	}
  	return $literalMatchingQueries;
@@ -360,11 +365,11 @@ function createPropertyWithConstraintQueries() {
     for($i = 0; $i < DEFAULT_QUERY_NUM; $i++) {
       printProgress($i / DEFAULT_QUERY_NUM);
       $graph = getRelationGraph(MAX_DEPTH);
-      $q = $prefixes."SELECT ?x WHERE {";
+      $q = $prefixes."SELECT ".getVar(0)." WHERE {";
       $j = 0;
       foreach($graph as $n) {
      	 list($s, $p, $o) = $n;
-     	 $q .= getVar($j)." prop:".$p->getDBkey()." ".getVar($j+1).".";
+     	 $q .= getVar($j)." prop:".USE_PREFIX.$p->getDBkey()." ".getVar($j+1)." .";
      	 $j++;
       }
       $q .= "}";
@@ -382,7 +387,7 @@ function createPropertyWithConstraintQueries() {
       list($s, $p, $o) = $a;
       printProgress($i / DEFAULT_QUERY_NUM);
       
-      $smyPropQueries[] = $prefixes."SELECT ?x WHERE { a:".$o->getDBkey()." prop:".$p->getDBkey()." ?x. }";
+      $smyPropQueries[] = $prefixes."SELECT ?x WHERE { a:".USE_PREFIX.$o->getDBkey()." prop:".USE_PREFIX.$p->getDBkey()." ?x . }";
       $i++;
     }
     return $smyPropQueries;
@@ -397,7 +402,7 @@ function createPropertyWithConstraintQueries() {
       list($s, $p, $o) = $a;
       printProgress($i / DEFAULT_QUERY_NUM);
       
-      $transPropQueries[] = $prefixes."SELECT ?x WHERE { a:".$s->getDBkey()." prop:".$p->getDBkey()." ?x. }";
+      $transPropQueries[] = $prefixes."SELECT ?x WHERE { a:".USE_PREFIX.$s->getDBkey()." prop:".USE_PREFIX.$p->getDBkey()." ?x . }";
       $i++;
     }
     return $transPropQueries;
@@ -412,7 +417,7 @@ function createPropertyWithConstraintQueries() {
       list($s, $p, $o, $inv_p) = $a;
       printProgress($i / DEFAULT_QUERY_NUM);
       
-      $invPropQueries[] = $prefixes."SELECT ?x WHERE { a:".$o->getDBkey()." prop:".$inv_p->getDBkey()." ?x. }";
+      $invPropQueries[] = $prefixes."SELECT ?x WHERE { a:".USE_PREFIX.$o->getDBkey()." prop:".USE_PREFIX.$inv_p->getDBkey()." ?x . }";
       $i++;
     }
     return $invPropQueries;
@@ -531,13 +536,18 @@ function getArbitraryInstances($limit) {
     return $result; 
  }
  
-function getArbitraryProperties($limit) {
+function getArbitraryProperties($limit, $object_prop = false) {
    $result=array(); 
     $db = wfGetDB(DB_MASTER);
-   $res = $db->query('SELECT page_title FROM page WHERE page_namespace = 102 AND page_is_redirect = 0 ORDER BY RAND() LIMIT '.$limit);
+    if ($object_prop) {
+    	 $res = $db->query('SELECT DISTINCT r1.relation_title AS title FROM smw_relations r1 WHERE NOT EXISTS (SELECT r2.subject_id FROM smw_attributes r2 WHERE r2.subject_title = r1.relation_title AND (r2.attribute_title = \'Has_min_cardinality\' OR r2.attribute_title = \'Has_max_cardinality\')) ORDER BY RAND() LIMIT '.$limit);
+    } else {
+        $res = $db->query('SELECT page_title AS title FROM page p WHERE page_namespace = 102 AND page_is_redirect = 0 WHERE NOT EXISTS (SELECT r2.subject_id FROM smw_attributes r2 WHERE r2.subject_title = p.page_title AND (r2.attribute_title = \'Has_min_cardinality\' OR r2.attribute_title = \'Has_max_cardinality\')) ORDER BY RAND() LIMIT '.$limit);
+    	
+    }
     if($db->numRows( $res ) > 0) {
             while($row = $db->fetchObject($res)) {
-                $result[] =Title::newFromText($row->page_title, SMW_NS_PROPERTY);
+                $result[] =Title::newFromText($row->title, SMW_NS_PROPERTY);
                 
             }
         }
@@ -548,7 +558,7 @@ function getArbitraryProperties($limit) {
 function getArbitraryRelations($limit) {
     $result=array(); 
     $db = wfGetDB(DB_MASTER);
-   $res = $db->query('SELECT DISTINCT relation_title FROM smw_relations ORDER BY RAND() LIMIT '.$limit);
+   $res = $db->query('SELECT DISTINCT r1.relation_title FROM smw_relations r1 WHERE NOT EXISTS (SELECT r2.subject_id FROM smw_attributes r2 WHERE r2.subject_title = r1.relation_title AND (r2.attribute_title = \'Has_min_cardinality\' OR r2.attribute_title = \'Has_max_cardinality\')) ORDER BY RAND() LIMIT '.$limit);
     if($db->numRows( $res ) > 0) {
             while($row = $db->fetchObject($res)) {
                 $result[] =Title::newFromText($row->relation_title, SMW_NS_PROPERTY);
@@ -722,7 +732,7 @@ function getInverseProperties($limit) {
  	$i = 0;
     foreach($properties as $p) {
     	printProgress($i / CREATE_NUM_OF_INVPROPS);
-         $someprop = getArbitraryProperties(1);   
+         $someprop = getArbitraryProperties(1, true);   
     	 addText($p, "\n[[Is inverse of::Property:".$someprop[0]->getDBkey()."]]");
     	 $i++;
     }   
