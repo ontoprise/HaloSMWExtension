b5f14e3e6f91e8235f7816de129f18c8bff1670a
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-05-19 12:52:47 +0000

    ws fix
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@1041 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if ($action == 'get_wsdl') {
     $contents = fread ($handle, filesize ("webservices.wsdl"));
     fclose($handle);
     
-    echo str_replace("localhost", $_SERVER['SERVER_ADDR'], $contents);
+    echo str_replace("{{wiki-path}}", $_SERVER['SERVER_ADDR'].$_SERVER['SCRIPT_NAME'], $contents);
     exit;
 }
 
