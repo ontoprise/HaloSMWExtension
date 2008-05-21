2f9ccf5f34b485db1bd03daa525cb16b38e4a20b
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-05-21 09:48:34 +0000

    fix
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@1063 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/specials/SMWGardening/SMW_AsyncBotStarter.php
+++ extensions/SMWHalo/specials/SMWGardening/SMW_AsyncBotStarter.php
@@ -90,7 +90,7 @@ require_once("SMW_GardeningLog.php");
  if ($bot != null) { 
  	echo ("Starting bot: $botID\n");
  	// run bot
- 	global $smwgGardeningBotDelay;
+ 	global $smwgGardeningBotDelay, $wgContLang;
  	try { 
  		$bot->setTaskID($taskid);
  		// initialize term signal socket
@@ -113,7 +113,7 @@ require_once("SMW_GardeningLog.php");
  		if ($log != NULL && $log != '') {
  			$glp = Title::newFromText(wfMsg('gardeninglog'), NS_SPECIAL);
  			$log .= "\n\n".wfMsg('smw_gardeninglog_link', "[$wgServer$wgScript/".$glp->getNsText().":".$glp->getText()."?bot=$botID ".$glp->getText()."]");
- 			$log .= "\n[[category:GardeningLog]]";
+ 			$log .= "\n[[".$wgContLang->getNsText(NS_CATEGORY).":".wfMsg('smw_gardening_log_cat')."]]";
  		}
  		
  		// mark as finished
@@ -123,7 +123,7 @@ require_once("SMW_GardeningLog.php");
  	} catch(Exception $e) {
  	    
  		$log = 'Something bad happened during execution of "'.$botID.'": '.$e->getMessage();
- 		$log .= "\n[[category:GardeningLog]]";
+ 		 $log .= "\n[[".$wgContLang->getNsText(NS_CATEGORY).":".wfMsg('smw_gardening_log_cat')."]]";
  		echo $log;
  	    
  		$title = SMWGardeningLog::getGardeningLogAccess()->markGardeningTaskAsFinished($taskid, $log);
@@ -168,42 +168,6 @@ require_once("SMW_GardeningLog.php");
  		return $a;
  	}
  
- // get user email to send a message to him.
- /*$userEmail = getEmailFromUserId($userId);
  
- if ($smtpServerIP && $userEmail != null && $userEmail != '') { 
- 	// send email when finished.
- 	echo "Sending email to: ".$userEmail;
- 	sendmail($userEmail, $bot);
- }*/
- 
- /**
-  * Sends a mail to $to using server $smtpServerIP as SMTP server
-  * $bot is the bot which has finished.
-  */
- function sendmail($to, $bot) {
- 	$header = "From: $to" . "\r\n" .
-   	"Reply-To: $to" . "\r\n" .
-   	"X-Mailer: PHP/" . phpversion();
-  	
- 	mail($to, $bot->getLabel()." has finished", wfMsg('smw_autogen_mail'), $header);
- 
- }
- 
- /**
-  * Returns user email from user id.
-  */
- function getEmailFromUserId($userId) {
- 	$db =& wfGetDB( DB_SLAVE );
- 	$res = $db->select( $db->tableName('user'), 
-		                    'user_email',
-		                    'user_id = '.$db->addQuotes($userId), 'SMW::getEmailFromUserId', NULL );
-	if($db->numRows( $res ) > 0) {
-		$row = $db->fetchObject($res);
-		return $row->user_email;
-		
-	}
-	return NULL;
- }
 
 ?>
