8e8e501e3492ddb916a994429a755cab40b097fe
ingo <ingo@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-05-25 17:59:08 +0000

    added some methods needed for the ws syntax parser and the wspropertypairdetector
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@1066 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/specials/SMWWebService/storage/SMW_WSStorageSQL.php
+++ extensions/SMWHalo/specials/SMWWebService/storage/SMW_WSStorageSQL.php
@@ -1,39 +1,39 @@
 <?php
 /*  Copyright 2008, ontoprise GmbH
-*  This file is part of the halo-Extension.
-*
-*   The halo-Extension is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   The halo-Extension is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *  This file is part of the halo-Extension.
+ *
+ *   The halo-Extension is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The halo-Extension is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
- * This file provides the access to the MediaWiki SQL database tables that are 
+ * This file provides the access to the MediaWiki SQL database tables that are
  * used by the web service extension.
- * 
+ *
  * @author Thomas Schweitzer
- * 
+ *
  */
 
 global $smwgHaloIP;
 require_once $smwgHaloIP . '/includes/SMW_DBHelper.php';
 
 /**
- * This class encapsulates all methods that care about the database tables of 
+ * This class encapsulates all methods that care about the database tables of
  * the web service extension.
  *
  */
 class WSStorageSQL {
-	
+
 	/**
 	 * Initializes the database tables of the web service extensions. These are:
 	 * - Wiki Web Service Definition: smw_ws_wwsd
@@ -52,7 +52,7 @@ class WSStorageSQL {
 		if ($db->tableExists($wwsdTable) === true) {
 			return;
 		}
-		
+
 		DBHelper::setupTable($wwsdTable, array(
 				  'web_service_id'		 =>  'INT(8) UNSIGNED NOT NULL PRIMARY KEY',
 				  'uri'  	             =>  'VARCHAR(1024) NOT NULL' ,
@@ -76,7 +76,7 @@ class WSStorageSQL {
 				  'last_update'    	=>  'DATETIME NOT NULL' ,
 				  'last_access'    	=>  'DATETIME NOT NULL' ,
 				  'PRIMARY KEY(web_service_id,param_set_id)' =>  ''), $db, true);
-		
+
 		// create parameter table
 		$paramTable = $db->tableName('smw_ws_parameters');
 		DBHelper::setupTable($paramTable, array(
@@ -92,7 +92,7 @@ class WSStorageSQL {
 				  'web_service_id'	=>  'INT(8) UNSIGNED NOT NULL',
 				  'param_set_id'  	=>  'INT(8) UNSIGNED NOT NULL',
 				  'PRIMARY KEY(property_id,page_id,web_service_id,param_set_id)' => ''), $db, true);
-		
+
 		// create articles table
 		$articlesTable = $db->tableName('smw_ws_articles');
 		DBHelper::setupTable($articlesTable, array(
@@ -100,22 +100,22 @@ class WSStorageSQL {
 				  'param_set_id'  	=>  'INT(8) UNSIGNED NOT NULL' ,
 				  'page_id'      	=>  'INT(8) UNSIGNED NOT NULL' ,
 				  'PRIMARY KEY(web_service_id,param_set_id,page_id)' =>  ''), $db, true);
-		
+
 	}
-	
+
 	/**
 	 * Stores a web service in the database.
 	 *
 	 * @param WebService $webService
-	 * 
+	 *
 	 * @return bool
 	 * 	  <true>, if successful
 	 *    <false>, otherwise
-	 * 
+	 *
 	 */
 	public function storeWS(WebService &$webService) {
- 		$db =& wfGetDB( DB_MASTER );
- 		try {
+		$db =& wfGetDB( DB_MASTER );
+		try {
 			$db->replace($db->tableName('smw_ws_wwsd'), null, array(
 					  'web_service_id'	=>  $webService->getArticleID(),
 					  'uri'  	        =>  $webService->getURI() ,
@@ -128,26 +128,26 @@ class WSStorageSQL {
 					  'update_delay'    =>  $webService->getUpdateDelay(),
 					  'span_of_life'    =>  $webService->getSpanOfLife() ,
 					  'expires_after_update'
-					                    =>  $webService->doesExpireAfterUpdate() ? 'true' : 'false',
+					  =>  $webService->doesExpireAfterUpdate() ? 'true' : 'false',
 					  'confirmed'       =>  $webService->isConfirmed() ? 'true' : 'false'));
- 		} catch (Exception $e) {
- 			echo $e->getMessage();
- 			return false;
- 		}
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false;
+		}
 		return true;
-				
+
 	}
-	
+
 	/**
-	 * Retrieves the wiki web service definition (WWSD) of the web service with 
+	 * Retrieves the wiki web service definition (WWSD) of the web service with
 	 * the page id <$id> from the database.
 	 *
 	 * @param int $id
 	 * 		The unique page ID of the web service's WWSD
-	 * 
+	 *
 	 * @return WebService
 	 * 		An instance of class WebService or
-	 * 		<null>, if the requested WWSD does not exist. 
+	 * 		<null>, if the requested WWSD does not exist.
 	 */
 	public function getWS($id) {
 		$db =& wfGetDB( DB_SLAVE );
@@ -156,21 +156,21 @@ class WSStorageSQL {
 		          "JOIN page p ON wwsd.web_service_id = p.page_id ".
 		          "WHERE web_service_id = ".$id.";";
 		$ws = null;
-		
+
 		$res = $db->query($sql);
-		
+
 		if ($db->numRows($res) == 1) {
 			$row = $db->fetchObject($res);
 			$ws = new WebService($row->page_title, $row->uri, $row->protocol,
-			                     $row->method, $row->parameters, $row->result,
-			                     $row->display_policy, $row->query_policy,
-			                     $row->updateDelay, $row->span_of_life, 
-			                     $rwow->expires_after_update == 'true',
-			                     $row->confirmed == 'true');
+			$row->method, $row->parameters, $row->result,
+			$row->display_policy, $row->query_policy,
+			$row->updateDelay, $row->span_of_life,
+			$rwow->expires_after_update == 'true',
+			$row->confirmed == 'true');
 		}
 		$db->freeResult($res);
 		return $ws;
-		
+
 	}
 
 	/**
@@ -188,17 +188,16 @@ class WSStorageSQL {
 	 * 		<false>, otherwise
 	 */
 	public function addWSArticle($wsPageID, $paramSetID, $pageID) {
- 		$db =& wfGetDB( DB_MASTER );
- 		try {
+		$db =& wfGetDB( DB_MASTER );
+		try {
 			$db->insert($db->tableName('smw_ws_articles'), array(
 					  'web_service_id' => $wsPageID,
 					  'param_set_id'   => $paramSetID,
 					  'page_id'        => $pageID));
- 		} catch (Exception $e) {
- 			echo $e->getMessage();
- 			return false;
- 		}
-		return true;
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false;
+		}
 	}
 
 	/**
@@ -206,9 +205,9 @@ class WSStorageSQL {
 	 *
 	 * @param int $wsPageID
 	 * 		The unique page ID of the web service's WWSD.
-	 * 
+	 *
 	 * @return array:
-	 * 		An array of page ids of articles that use the web service. 
+	 * 		An array of page ids of articles that use the web service.
 	 */
 	public function getWSArticles($wsPageID, SMWRequestOptions $options) {
 		$db =& wfGetDB( DB_SLAVE );
@@ -218,11 +217,11 @@ class WSStorageSQL {
 		          "WHERE art.web_service_id='".$wsPageID."' ";
 		$cond = DBHelper::getSQLConditions($options, 'p.page_title');
 		$opt = DBHelper::getSQLOptionsAsString($options, 'p.page_title');
-		
+
 		$articles = array();
-		
+
 		$res = $db->query($sql.$cond.' '.$opt);
-		
+
 		if ($db->numRows($res) > 0) {
 			while ($row = $db->fetchObject($res)) {
 				$articles[] = $row->page_id;
@@ -230,7 +229,7 @@ class WSStorageSQL {
 		}
 		$db->freeResult($res);
 		return $articles;
-		
+
 	}
 
 	/**
@@ -250,29 +249,29 @@ class WSStorageSQL {
 	 * 		<false>, otherwise
 	 */
 	public function addWSProperty($propertyID, $wsPageID, $paramSetID, $pageID) {
- 		$db =& wfGetDB( DB_MASTER );
- 		try {
+		$db =& wfGetDB( DB_MASTER );
+		try {
 			$db->insert($db->tableName('smw_ws_properties'), array(
 					  'property_id'    => $propertyID,
 					  'web_service_id' => $wsPageID,
 					  'param_set_id'   => $paramSetID,
 					  'page_id'        => $pageID));
- 		} catch (Exception $e) {
- 			echo $e->getMessage();
- 			return false;
- 		}
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false;
+		}
 		return true;
 	}
 
 	/**
-	 * Retrieves the page IDs of properties that get their value from the given 
+	 * Retrieves the page IDs of properties that get their value from the given
 	 * web service. The IDs can be sorted for the names of the properties.
 	 *
 	 * @param string $wsPageID
 	 * 		The unique page ID of the web service's WWSD.
-	 * 
+	 *
 	 * @return array:
-	 * 		An array of property page ids. 
+	 * 		An array of property page ids.
 	 */
 	public function getWSProperties($wsPageID, SMWRequestOptions $options) {
 		$db =& wfGetDB( DB_SLAVE );
@@ -282,11 +281,11 @@ class WSStorageSQL {
 		          "WHERE prop.web_service_id='".$wsPageID."' ";
 		$cond = DBHelper::getSQLConditions($options, 'p.page_title');
 		$opt = DBHelper::getSQLOptionsAsString($options, 'p.page_title');
-		
+
 		$properties = array();
-		
+
 		$res = $db->query($sql.$cond.' '.$opt);
-		
+
 		if ($db->numRows($res) > 0) {
 			while ($row = $db->fetchObject($res)) {
 				$properties[] = $row->page_id;
@@ -294,8 +293,211 @@ class WSStorageSQL {
 		}
 		$db->freeResult($res);
 		return $properties;
+
+	}
+
+	/**
+	 * This function stores a new parameter set for the given
+	 * parameters if no such parameter set allready exists.
+	 *
+	 * @param array $parameters
+	 * 		the parameters for which a new set has to be stored
+	 * @return string
+	 * 		the parameter set id of the new or an
+	 * 		appropriate parameter set which allready exists
+	 */
+	
+	public function storeParameterset($parameters){
+		$db =& wfGetDB( DB_SLAVE );
+		$ptbl = $db->tableName('smw_ws_parameters');
+
+		// check if an appropriate parameter set exists
+		$whereConstruct="";
+		$i=0;
+		foreach($parameters as $name => $value){
+			if($i != 0){
+				$whereConstruct.= " OR ";
+			} else {
+				$whereConstruct.= "(";
+			}
+			$whereConstruct.= "(parameters.name='".$name."' AND parameters.value='".$value."')";
+			$i++;
+		}
+		$whereConstruct.= ") AND parameters.param_set_id in ".
+		"(SELECT p.param_set_id FROM ".$ptbl." p ".
+		"GROUP BY p.param_set_id HAVING count(p.name) = ".$i.")";
+
+		$sql = "SELECT parameters.param_set_id, count(parameters.name)".
+		 "FROM ".$ptbl." parameters ".
+		"WHERE ".$whereConstruct.
+		"GROUP BY parameters.param_set_id HAVING count(parameters.name) =".$i.";";
+
+		$res = $db->query($sql);
+
+		$parameterSetId = " an error occured";
+
+		if($db->numRows($res) > 1){
+			// an error occured;
+		} else if ($db->numRows($res) == 1){
+			// an appropriate parameter set exists
+			$row = $db->fetchObject($res);
+			$parameterSetId = $row->param_set_id;
+		} else {
+			// a new parameter set has to be created
+			$parameterSetId = self::generateParameterSetId();
+			$db =& wfGetDB( DB_MASTER );
+			foreach($parameters as $name => $value){
+				try {
+					$db->insert($db->tableName('smw_ws_parameters'), array(
+					  'name'    => $name,
+					  'param_set_id' => $parameterSetId,
+					  'value'   => $value));
+				} catch (Exception $e) {
+					echo $e->getMessage();
+					return false;
+				}
+			}
+		}
+		return $parameterSetId;
+	}
+
+	/**
+	 * this function generates a new parameter set id
+	 *
+	 * @return string
+	 * 		a new parameter set id
+	 */
+	public function generateParameterSetId(){
+		$parameterSetId = "";
+		$done = false;
+
+		$db =& wfGetDB( DB_SLAVE );
+		$ptb = $db->tableName('smw_ws_parameters');
+
+		while(!$done){
+			$parameterSetId = rand(0, 9999999);
+			$sql = "SELECT parameters.name FROM ".$ptb." parameters ".
+			"WHERE parameters.param_set_id ='".$parameterSetId."'";
+
+			$res = $db->query($sql);
+			if($db->numRows($res) == 0){
+				$done = true;
+			}
+			$db->freeResult($res);
+		}
+
+		return $parameterSetId;
+	}
+
+	/**
+	 * The database stores which web services with which parameter set IDs are
+	 * used in which article. This method removes such usage from the DB.
+	 *
+	 * @param int $wsPageID
+	 * 		The unique page ID of the web service's WWSD.
+	 * @param int $paramSetID
+	 * 		The unique parameter set ID.
+	 * @param int $pageID
+	 * 		The ID of the article that uses the web service.
+	 * @return bool
+	 * 		<true>, if the DB entry was successfully added
+	 * 		<false>, otherwise
+	 */
+	public function removeWSArticle($wsPageID, $paramSetID, $pageID) {
+		$db =& wfGetDB( DB_MASTER );
+		try {
+			$db->delete($db->tableName('smw_ws_articles'), array(
+					  'web_service_id' => $wsPageID,
+					  'param_set_id'   => $paramSetID,
+					  'page_id'        => $pageID));
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false.$e;
+		}
+		return true;
+	}
+
+	/**
+	 * This method deletes the given parameter set from the DB.
+	 *
+	 * @param string
+	 * 		id of the parameter set to delete
+	 * @return boolean
+	 * 		true: e.th. ok
+	 * 		false: errors occured
+	 */
+	public function removeParameterSet($parameterSetId) {
+		$db =& wfGetDB( DB_MASTER );
 		
+		try {
+			$db->delete($db->tableName('smw_ws_parameters'), array(
+			'param_set_id' => $parameterSetId));
+		} catch (Exception $e) {
+			echo $e->getMessage();
+			return false.$e;
+		}
+		return true;
+	}
+
+	/**
+	 * This function retrieves all articles that use a parameter set with the given id
+	 *
+	 * @param string $parameterSetId
+	 * @return array
+	 * 	an array of parameter set ids
+	 */
+	function getUsedParameterSetIds($parameterSetId){
+		$db =& wfGetDB( DB_SLAVE );
+		$ptb = $db->tableName('smw_ws_articles');
+	
+		$sql = "SELECT wsArticles.param_set_id"
+		." FROM ".$ptb." wsArticles ".
+		"WHERE wsArticles.param_set_id = ".$parameterSetId."";
+
+		$webServices = array();
+		$res = $db->query($sql);
+
+		if ($db->numRows($res) > 0) {
+			while ($row = $db->fetchObject($res)) {
+				array_push(&$webServices, array($row->param_set_id));
+			}
+		} 
+		$db->freeResult($res);
+		return $webServices;
 	}
 	
+	/**
+	 * this method retrieves all webservices and the parameter sets used
+	 * by those, which are used on the given article
+	 *
+	 * @param string articleid
+	 * @return array
+	 * 		an array of the webservices and the parameter sets used by them
+	 */
+	function getWSsUsedInArticle($articleId){
+		$db =& wfGetDB( DB_SLAVE );
+		$ptb = $db->tableName('smw_ws_articles');
+	
+		$sql = "SELECT wsArticles.web_service_id, wsArticles.param_set_id"
+		." FROM ".$ptb." wsArticles ".
+		"WHERE wsArticles.page_id =".$articleId."";
+
+		$webServices = array();
+		$res = $db->query($sql);
+
+		if ($db->numRows($res) > 0) {
+			while ($row = $db->fetchObject($res)) {
+				array_push(&$webServices, array($row->web_service_id, $row->param_set_id));
+			}
+		} 
+		$db->freeResult($res);
+		return $webServices;
+	}
+
+
+
+
+
+
 }
 ?>
