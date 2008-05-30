73929136a77b5adfac5f8279558d9a684db81644
ingo <ingo@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-05-30 14:16:09 +0000

    changes so that modified and deleted wwsd and related cache entries are removed from the db
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@1109 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/specials/SMWWebService/SMW_WebServiceManager.php
+++ extensions/SMWHalo/specials/SMWWebService/SMW_WebServiceManager.php
@@ -1,26 +1,26 @@
 <?php
 /*  Copyright 2008, ontoprise GmbH
-*  This file is part of the halo-Extension.
-*
-*   The halo-Extension is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   The halo-Extension is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *  This file is part of the halo-Extension.
+ *
+ *   The halo-Extension is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The halo-Extension is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * This is the main entry file for the semantic Wiki Web Service extension.
- * 
+ *
  * @author Thomas Schweitzer
- * 
+ *
  */
 
 // Include the settings file for the configuration of the Web Service extension.
@@ -65,7 +65,8 @@ WebServiceManager::registerWWSNamespaces();
 class WebServiceManager {
 
 	private static $mNewWebService = null;
-	
+	private static $mOldWebservice = null;
+
 	/**
 	 * Register the WebServicePage for articles in the namespace 'WebService'.
 	 *
@@ -80,27 +81,27 @@ class WebServiceManager {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Initializes the namespaces that are used by the Wiki Web Service extension.
 	 * Normally the base index starts at 200. It must be an even number greater than
 	 * than 100. However, by default Semantic MediaWiki uses the namespace indexes
-	 * from 100 upwards. 
-	 * 
+	 * from 100 upwards.
+	 *
 	 * @param int $baseIndex
-	 * 		Optional base index for all Wiki Web Service namespaces. The default is 200. 
+	 * 		Optional base index for all Wiki Web Service namespaces. The default is 200.
 	 */
 	static function initWWSNamespaces($baseIndex = 200) {
 		global $smwgWWSNamespaceIndex;
-	
+
 		if (!isset($smwgWWSNamespaceIndex)) {
 			$smwgWWSNamespaceIndex = $baseIndex;
 		}
-	
+
 		define('SMW_NS_WEB_SERVICE',       $smwgWWSNamespaceIndex);
 		define('SMW_NS_WEB_SERVICE_TALK',  $smwgWWSNamespaceIndex+1);
 	}
-	
+
 	/**
 	 * Registers the new namespaces. Must be called after the language dependent
 	 * messages have been installed.
@@ -108,26 +109,26 @@ class WebServiceManager {
 	 */
 	static function registerWWSNamespaces() {
 		global $wgExtraNamespaces, $wgNamespaceAliases, $smwgHaloContLang;
-		
+
 		// Register namespace identifiers
-		if (!is_array($wgExtraNamespaces)) { 
-			$wgExtraNamespaces = array(); 
+		if (!is_array($wgExtraNamespaces)) {
+			$wgExtraNamespaces = array();
 		}
 		$wgExtraNamespaces = $wgExtraNamespaces + $smwgHaloContLang->getNamespaces();
 		$wgNamespaceAliases = $wgNamespaceAliases + $smwgHaloContLang->getNamespaceAliases();
-		
+
 	}
-	
+
 	/**
 	 * Initialized the wiki web service extension:
-	 * - installs the extended representation of articles in the namespace 
+	 * - installs the extended representation of articles in the namespace
 	 *   'WebService'.
 	 *
 	 */
 	static function initWikiWebServiceExtension() {
 		global $wgRequest, $wgHooks, $wgParser;
 		$action = $wgRequest->getVal('action');
-	
+
 		if ($action == 'ajax') {
 			// Do not install the extension for ajax calls
 			return;
@@ -135,66 +136,66 @@ class WebServiceManager {
 			
 		// Install the extended representation of articles in the namespace 'WebService'.
 		$wgHooks['ArticleFromTitle'][] = 'WebServiceManager::showWebServicePage';
-		
+
 		$wgParser->setHook('WebService', 'WebServiceManager::wwsdParserHook');
 		$wgHooks['ArticleSaveComplete'][] = 'WebServiceManager::articleSavedHook';
-		
-		
-		
-		//--- TEST
-				
-/*		
-		global $smwgHaloIP;
-		require_once("$smwgHaloIP/specials/SMWWebService/SMW_WebService.php");
-		
-		$wwsd = '<webservice name="Weather">'.
-				'    <uri name="http://weather.example.com/weather.wsdl" />'.
-				'    <protocol>SOAP</protocol>'.
-				'    <method name="getTemperature" />'.
-				'    <parameter name="zipCode" defaultValue="98101" optional="false" path="cityWeather.zipCode" />'.
-				'    <parameter name="zipCode" defaultValue="98100" optional="false" path="cityWeather.zipCode" />'.
-				'    <result name="temp">'.
-				'        <part name="celsius" path="cityWeather.temperature.celsius" />'.
-				'        <part name="fahrenheit" path="cityWeather.temperature.fahrenheit" />'.
-				'    </result>'.
-				'    <result name="temperature">'.
-				'        <part name="c" path="cityWeather.temperature.celsius" />'.
-				'        <part name="f" path="cityWeather.temperature.fahrenheit" />'.
-				'    </result>'.
-				'    <displayPolicy>'.
-				'        <once />'.
-				'    </displayPolicy>'.
-				'    <queryPolicy>'.
-				'        <maxAge value="1440"/>'.
-				'        <delay value="1"/>'.
-				'    </queryPolicy>'.
-				'    <spanOfLife value="180" expiresAfterUpdate="true"/>'.
-				'</webservice>';
+		$wgHooks['ArticleDelete'][] = 'WebServiceManager::articleDeleteHook';
 
-		$ws = WebService::newFromWWSD($wwsd);
-		
-		$ws = new WebService('MySecondWebService', 'http://some.uri.com', 'SOAP', 'getValue',
-		                     '<parameters> </parameters>', '<result> </result>',
-							 1000, 2000, 0, 90, false, true);
-		$ws->store();
-		$ws = WebService::newFromName('MySecondWebService');
-		$id = $ws->getArticleID();
-		$ws = WebService::newFromID($id);
-		
-		for ($i = 1900; $i < 2000; ++$i) {
-			WSStorage::getDatabase()->addWSArticle($id, 142, $i);
-		}
-		for ($i = 2000; $i < 2100; ++$i) {
-			WSStorage::getDatabase()->addWSProperty($i ,$id, 142, $i);
-		}
-*/
+
 		//--- TEST
-		
+
+		/*
+		 global $smwgHaloIP;
+		 require_once("$smwgHaloIP/specials/SMWWebService/SMW_WebService.php");
+
+		 $wwsd = '<webservice name="Weather">'.
+		 '    <uri name="http://weather.example.com/weather.wsdl" />'.
+		 '    <protocol>SOAP</protocol>'.
+		 '    <method name="getTemperature" />'.
+		 '    <parameter name="zipCode" defaultValue="98101" optional="false" path="cityWeather.zipCode" />'.
+		 '    <parameter name="zipCode" defaultValue="98100" optional="false" path="cityWeather.zipCode" />'.
+		 '    <result name="temp">'.
+		 '        <part name="celsius" path="cityWeather.temperature.celsius" />'.
+		 '        <part name="fahrenheit" path="cityWeather.temperature.fahrenheit" />'.
+		 '    </result>'.
+		 '    <result name="temperature">'.
+		 '        <part name="c" path="cityWeather.temperature.celsius" />'.
+		 '        <part name="f" path="cityWeather.temperature.fahrenheit" />'.
+		 '    </result>'.
+		 '    <displayPolicy>'.
+		 '        <once />'.
+		 '    </displayPolicy>'.
+		 '    <queryPolicy>'.
+		 '        <maxAge value="1440"/>'.
+		 '        <delay value="1"/>'.
+		 '    </queryPolicy>'.
+		 '    <spanOfLife value="180" expiresAfterUpdate="true"/>'.
+		 '</webservice>';
+
+		 $ws = WebService::newFromWWSD($wwsd);
+
+		 $ws = new WebService('MySecondWebService', 'http://some.uri.com', 'SOAP', 'getValue',
+		 '<parameters> </parameters>', '<result> </result>',
+		 1000, 2000, 0, 90, false, true);
+		 $ws->store();
+		 $ws = WebService::newFromName('MySecondWebService');
+		 $id = $ws->getArticleID();
+		 $ws = WebService::newFromID($id);
+
+		 for ($i = 1900; $i < 2000; ++$i) {
+			WSStorage::getDatabase()->addWSArticle($id, 142, $i);
+			}
+			for ($i = 2000; $i < 2100; ++$i) {
+			WSStorage::getDatabase()->addWSProperty($i ,$id, 142, $i);
+			}
+			*/
+		//--- TEST
+
 	}
 
-	
+
 	/**
-	 * This function is called, when a <WebService>-tag for a WWSD has been 
+	 * This function is called, when a <WebService>-tag for a WWSD has been
 	 * found in an article. If the content of the definition is correct, and
 	 * if the namespace of the article is the WebService namespace, the WWSD
 	 * will be stored in the database.
@@ -211,14 +212,14 @@ class WebServiceManager {
 	public static function wwsdParserHook($input, $args, $parser) {
 		global $smwgHaloIP;
 		require_once("$smwgHaloIP/specials/SMWWebService/SMW_WebService.php");
-		
+
 		$attr = "";
 		foreach ($args as $k => $v) {
 			$attr .= " ". $k . '="' . $v . '"';
 		}
 		$completeWWSD =
 			"<WebService$attr>".
-			$input.
+		$input.
 			"</WebService>\n";
 			
 		$notice = '';
@@ -253,10 +254,13 @@ class WebServiceManager {
 		}
 		return  "<pre>\n".htmlspecialchars($completeWWSD)."\n</pre>".$notice;
 	}
-	
+
 	/**
 	 * Stores the previously parsed WWSD in the database. This function is a hook for
 	 * 'ArticleSaveComplete.'
+	 * 
+	 * This function also deletes wwsd and cache entries from the db
+	 * that are no longer needed.
 	 *
 	 * @param Article $article
 	 * @param User $user
@@ -264,12 +268,49 @@ class WebServiceManager {
 	 * @return boolean true
 	 */
 	public static function articleSavedHook(&$article, &$user, &$text) {
+		if(self::$mOldWebservice){
+			$remove = true;
+			if(!self::$mNewWebService){
+				WebServiceCache::removeWS(self::$mOldWebservice->getArticleID());
+				self::$mOldWebservice->removeFromDB();
+				return true;
+			}
+			if(self::$mOldWebservice->getArticleId() != self::$mNewWebService->getArticleId()){
+				$remove = false;
+			} else if(self::$mOldWebservice->getMethod() == self::$mNewWebService->getMethod()){
+				$remove = false;
+			} else if(self::$mOldWebservice->getName() == self::$mNewWebService->getName()){
+				$remove = false;
+			} else if(self::$mOldWebservice->getParameters() == self::$mNewWebService->getParameters()){
+				$remove = false;
+			} else if(self::$mOldWebservice->getProtocol() == self::$mNewWebService->getProtocol()){
+				$remove = false;
+			} else if(self::$mOldWebservice->getResult() == self::$mNewWebService->getResult()){
+				$remove = false;
+			} else if(self::$mOldWebservice->getURI() == self::$mNewWebService->getURI()){
+				$remove = false;
+			} else if(remove){
+				WebServiceCache::removeWS(self::$mOldWebservice->getArticleID());
+				self::$mOldWebservice->removeFromDB();
+			}
+		}
 		if (self::$mNewWebService) {
 			self::$mNewWebService->store();
 		}
 		return true;
 	}
-	
+
+	/*
+	 * Removes a deleted wwsd and its related cache entries from the database.
+	 * This function is a hook for 'ArticleDElete'
+	 */
+	function articleDeleteHook(&$article, &$user, $reason){
+		WebServiceCache::removeWS($article->getID());
+		$ws = WebService::newFromID($article->getID());
+		$ws->removeFromDB();
+		return true;
+	}
+
 	/**
 	 * Creates the database tables that are used by the web service extension.
 	 *
@@ -277,7 +318,7 @@ class WebServiceManager {
 	public static function initDatabaseTables() {
 		global $smwgHaloIP;
 		require_once("$smwgHaloIP/specials/SMWWebService/SMW_WSStorage.php");
-		WSStorage::getDatabase()->initDatabaseTables();	
+		WSStorage::getDatabase()->initDatabaseTables();
 	}
 }
 
