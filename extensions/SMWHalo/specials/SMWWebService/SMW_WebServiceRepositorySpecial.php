96cee583d49bb3a7a13d4c929388289dd46cea93
ingo <ingo@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-06-24 15:46:21 +0000

    s. req-eds 12
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@1533 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/specials/SMWWebService/SMW_WebServiceRepositorySpecial.php
+++ extensions/SMWHalo/specials/SMWWebService/SMW_WebServiceRepositorySpecial.php
@@ -11,7 +11,7 @@ class SMWWebServiceRepositorySpecial extends SpecialPage {
 	public function __construct() {
 		parent::__construct('WebServicerepository');
 	}
-	
+
 	public function execute() {
 		global $wgRequest, $wgOut;
 
@@ -23,30 +23,44 @@ class SMWWebServiceRepositorySpecial extends SpecialPage {
 		require_once($smwgHaloIP . '/specials/SMWWebService/SMW_WSStorage.php');
 
 		$webServices = WSStorage::getDatabase()->getWebServices();
-		
+
 		$html .= "<h2><span class=\"mw-headline\">Available Wiki Web Service Definitions</span></h2>";
-		
+
 		$html .= "<table width=\"100%\" class=\"smwtable\"><tr><th>Name</th><th>Last Updates</th><th>Update</th><th>Confirm</th></tr>";
 		foreach($webServices as $ws){
 			$wsUrl = Title::newFromID($ws->getArticleID())->getInternalURL();
-			$wsName = substr($ws->getName(), 11, strlen($ws->getName())); 
+			$wsName = substr($ws->getName(), 11, strlen($ws->getName()));
 			$html .= "<tr><td><a href=\"".$wsUrl."\">".$wsName."</a></td>";
-			
+
 			$cacheResults = WSStorage::getDatabase()->getResultsFromCache($ws->getArticleID());
-			$oldestUpdate = wfTimestamp(TS_DB, $cacheResults[0]["lastUpdate"]);			
-			$latestUpdate = wfTimestamp(TS_DB, $cacheResults[(sizeof($cacheResults)-1)]["lastUpdate"]);
-			$html .= "<td>".$oldestUpdate." - ".$latestUpdate."</td>";
-			
+			$oldestUpdate = $cacheResults[0]["lastUpdate"];
+			if(strlen($oldestUpdate) > 0){
+				$oldestUpdate = wfTimestamp(TS_DB, $oldestUpdate);
+			}
+
+			$latestUpdate = "";
+			if(sizeof($cacheResults) > 1){
+				$latestUpdate = $cacheResults[(sizeof($cacheResults)-1)]["lastUpdate"];
+				if(strlen($latestUpdate) > 0){
+					$latestUpdate = wfTimestamp(TS_DB, $cacheResults[(sizeof($cacheResults)-1)]["lastUpdate"]);
+					if(strlen($oldestUpdate) > 0){
+						$latestUpdate = " - ".$latestUpdate;
+					}
+				}
+			}
+			$html .= "<td>".$oldestUpdate.$latestUpdate."</td>";
+
 			$html .= "<td><button type=\"button\" name=\"update\" onclick=\"webServiceSpecial.updateCache(".$ws->getArticleID().")\">Update</button></td>";
-			
+
 			if($ws->getConfirmationStatus() != "true"){
 				$html .= "<td id=\"confirmText\">  <button type=\"button\" id=\"confirmButton\" onclick=\"webServiceSpecial.confirmWWSD(".$ws->getArticleID().")\">Confirm</button></td></tr>";
 			} else {
 				$html .= "<td>confirmed</td></tr>";
 			}
 		}
+
 		$html .= "</table>";
-		
+
 		$wgOut->addHTML($html);
 	}
 }
