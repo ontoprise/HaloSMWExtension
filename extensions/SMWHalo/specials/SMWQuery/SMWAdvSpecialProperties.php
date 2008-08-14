11789ac1047fc5da161371dd9f9540e25e535cbe
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-08-14 12:54:54 +0000

    migrate SMWHaloStore2
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2017 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/specials/SMWQuery/SMWAdvSpecialProperties.php
+++ extensions/SMWHalo/specials/SMWQuery/SMWAdvSpecialProperties.php
@@ -83,94 +83,16 @@ class SMWPropertiesPage extends SMWQueryPage {
 		$sort = $wgRequest->getVal("sort") == NULL ? 0 : $wgRequest->getVal("sort") + 0;
 		$type = $wgRequest->getVal("type") == NULL ? 0 : $wgRequest->getVal("type") + 0;
 		
+		$advps_storage = AdvPropertySearchStorage::getAdvPropertySearchStorage();
 		switch($type) {
-			case 0: return $this->getDatatypeProperties($sort);
-			case 1: return $this->getWikiPageProperties($sort);
-			case 2: return $this->getNaryProperties($sort);
+			case 0: return $advps_storage->getDatatypeProperties($sort);
+			case 1: return $advps_storage->getWikiPageProperties($sort);
+			case 2: return $advps_storage->getNaryProperties($sort);
 		}
 		
 	}
 	
-	function getDatatypeProperties($sort) {
-		$NSatt = SMW_NS_PROPERTY;
-		$dbr =& wfGetDB( DB_SLAVE );
-		$attributes = $dbr->tableName( 'smw_attributes' );
-		$specialprops = $dbr->tableName( 'smw_specialprops' );
-		$pages = $dbr->tableName( 'page' );
-		return "SELECT 'Attributes' as type, {$NSatt} as namespace, s.value_string as value, 
-		                a.attribute_title as title, COUNT(*) as count, '-1' as obns
-		        FROM $specialprops s JOIN $pages p ON p.page_id=s.subject_id 
-		        JOIN $attributes a ON a.attribute_title=p.page_title AND s.property_id=".SMW_SP_HAS_TYPE."
-		        GROUP BY a.attribute_title, s.value_string";
-	}
 	
-	function getWikiPageProperties($sort) {
-		$NSrel = SMW_NS_PROPERTY;
-		$dbr =& wfGetDB( DB_SLAVE );
-		global $smwgHaloContLang;
-		$sspa = $smwgHaloContLang->getSpecialSchemaPropertyArray();
-		$nary_relations = $dbr->tableName( 'smw_nary_relations' );
-		$nary = $dbr->tableName( 'smw_nary' );
-		$relations = $dbr->tableName( 'smw_relations' );
-		$pages = $dbr->tableName( 'page' );
-		switch($sort) {
-			
-			case 0:	// fall through
-					
-			case 1: //fall through
-					
-			case 2: return "(SELECT 'Relations' as type,
-								{$NSrel} as namespace,
-					 			n.subject_title as title,
-								r.object_title as value,
-								r.object_namespace as obns,		
-								COUNT(*) as count,
-								p.page_touched as touch
-							FROM $pages p JOIN $relations rel ON p.page_title = rel.relation_title JOIN $nary n ON rel.relation_title = n.subject_title JOIN $nary_relations r ON r.subject_id = n.subject_id 
-							WHERE  r.nary_pos = 1 AND n.attribute_title = ".$dbr->addQuotes(str_replace(" ","_",$sspa[SMW_SSP_HAS_DOMAIN_AND_RANGE_HINT]))."
-							GROUP BY title) 
-						UNION
-							(SELECT 'Relations' as type,
-								{$NSrel} as namespace,
-								relation_title as title,
-								NULL as value, 
-								'-1' as obns, 
-								COUNT(*) as count ,
-								p.page_touched as touch
-							FROM $pages p JOIN $relations r ON p.page_title = r.relation_title LEFT JOIN $nary n ON r.relation_title = n.subject_title WHERE n.subject_title IS NULL GROUP BY title) ";
-		}
-	}
-	
-	function getNaryProperties($sort) {
-		$NSrel = SMW_NS_PROPERTY;
-		$dbr =& wfGetDB( DB_SLAVE );
-		$nary = $dbr->tableName( 'smw_nary' );
-		$pages = $dbr->tableName( 'page' );
-		switch($sort) {
-			case 2: // fall through cause filtering for type makes no sense here
-			case 0: return "SELECT 'Nary' as type,
-					{$NSrel} as namespace,
-					attribute_title as title,
-					
-					COUNT(*) as count,
-					'-1' as obns,
-					'' as value					
-					FROM $nary
-					GROUP BY attribute_title";
-					
-			case 1: return "SELECT 'Nary' as type,
-					{$NSrel} as namespace,
-					attribute_title as title,
-					
-					COUNT(*) as count,
-					'-1' as obns,
-					'' as value	
-					FROM $nary,$pages WHERE attribute_title = page_title
-					GROUP BY attribute_title";
-					
-			
-		}
-	}
 	
 	function getOrder() {
 		global $wgRequest;
@@ -286,5 +208,256 @@ class SMWPropertiesPage extends SMWQueryPage {
 		$db->freeResult($res);
 		return $result;
 	}
+}
+
+abstract class AdvPropertySearchStorage {
+    private static $INSTANCE = NULL;
+    
+    public static function getAdvPropertySearchStorage() {
+        global $smwgDefaultStore;
+        if (self::$INSTANCE == NULL) {
+            switch ($smwgDefaultStore) {
+                case (SMW_STORE_TESTING):
+                    self::$INSTANCE = NULL; // not implemented yet
+                    trigger_error('Testing stores not implemented for HALO extension.');
+                break;
+                case ('SMWHaloStore2'): default:
+                    self::$INSTANCE = new AdvPropertySearchStorageSQL2();
+                break;
+                case ('SMWHaloStore'): default:
+                    self::$INSTANCE = new AdvPropertySearchStorageSQL();
+                break;
+            }
+        }
+        return self::$INSTANCE;
+    }
+    
+	public abstract function getDatatypeProperties($sort);
+	public abstract function getWikiPageProperties($sort);
+	public abstract function getNaryProperties($sort);
+}
+
+class AdvPropertySearchStorageSQL extends AdvPropertySearchStorage {
+	
+    public function getDatatypeProperties($sort) {
+        $NSatt = SMW_NS_PROPERTY;
+        $dbr =& wfGetDB( DB_SLAVE );
+        $attributes = $dbr->tableName( 'smw_attributes' );
+        $specialprops = $dbr->tableName( 'smw_specialprops' );
+        $pages = $dbr->tableName( 'page' );
+        return "SELECT 'Attributes' as type, {$NSatt} as namespace, s.value_string as value, 
+                        a.attribute_title as title, COUNT(*) as count, '-1' as obns
+                FROM $specialprops s JOIN $pages p ON p.page_id=s.subject_id 
+                JOIN $attributes a ON a.attribute_title=p.page_title AND s.property_id=".SMW_SP_HAS_TYPE."
+                GROUP BY a.attribute_title, s.value_string";
+    }
+    
+    public function getWikiPageProperties($sort) {
+        $NSrel = SMW_NS_PROPERTY;
+        $dbr =& wfGetDB( DB_SLAVE );
+        global $smwgHaloContLang;
+        $sspa = $smwgHaloContLang->getSpecialSchemaPropertyArray();
+        $nary_relations = $dbr->tableName( 'smw_nary_relations' );
+        $nary = $dbr->tableName( 'smw_nary' );
+        $relations = $dbr->tableName( 'smw_relations' );
+        $pages = $dbr->tableName( 'page' );
+        switch($sort) {
+            
+            case 0: // fall through
+                    
+            case 1: //fall through
+                    
+            case 2: return "(SELECT 'Relations' as type,
+                                {$NSrel} as namespace,
+                                n.subject_title as title,
+                                r.object_title as value,
+                                r.object_namespace as obns,     
+                                COUNT(*) as count,
+                                p.page_touched as touch
+                            FROM $pages p JOIN $relations rel ON p.page_title = rel.relation_title JOIN $nary n ON rel.relation_title = n.subject_title JOIN $nary_relations r ON r.subject_id = n.subject_id 
+                            WHERE  r.nary_pos = 1 AND n.attribute_title = ".$dbr->addQuotes(str_replace(" ","_",$sspa[SMW_SSP_HAS_DOMAIN_AND_RANGE_HINT]))."
+                            GROUP BY title) 
+                        UNION
+                            (SELECT 'Relations' as type,
+                                {$NSrel} as namespace,
+                                relation_title as title,
+                                NULL as value, 
+                                '-1' as obns, 
+                                COUNT(*) as count ,
+                                p.page_touched as touch
+                            FROM $pages p JOIN $relations r ON p.page_title = r.relation_title LEFT JOIN $nary n ON r.relation_title = n.subject_title WHERE n.subject_title IS NULL GROUP BY title) ";
+        }
+    }
+    
+    public function getNaryProperties($sort) {
+        $NSrel = SMW_NS_PROPERTY;
+        $dbr =& wfGetDB( DB_SLAVE );
+        $nary = $dbr->tableName( 'smw_nary' );
+        $pages = $dbr->tableName( 'page' );
+        switch($sort) {
+            case 2: // fall through cause filtering for type makes no sense here
+            case 0: return "SELECT 'Nary' as type,
+                    {$NSrel} as namespace,
+                    attribute_title as title,
+                    
+                    COUNT(*) as count,
+                    '-1' as obns,
+                    '' as value                 
+                    FROM $nary
+                    GROUP BY attribute_title";
+                    
+            case 1: return "SELECT 'Nary' as type,
+                    {$NSrel} as namespace,
+                    attribute_title as title,
+                    
+                    COUNT(*) as count,
+                    '-1' as obns,
+                    '' as value 
+                    FROM $nary,$pages WHERE attribute_title = page_title
+                    GROUP BY attribute_title";
+                    
+            
+        }
+    }
+}
+
+class AdvPropertySearchStorageSQL2 extends AdvPropertySearchStorageSQL {
+	
+    public function getDatatypeProperties($sort) {
+        $NSatt = SMW_NS_PROPERTY;
+        $db =& wfGetDB( DB_SLAVE );
+        
+        $smw_ids = $db->tableName('smw_ids');
+        $smw_spec2 = $db->tableName('smw_spec2');
+        $smw_atts2 = $db->tableName('smw_atts2');     
+        $page = $db->tableName('page');
+   
+        return "SELECT 'Attributes' as type, {$NSatt} as namespace, s.value_string as value, 
+                        i.smw_title as title, COUNT(*) as count, '-1' as obns FROM $smw_atts2 a 
+                        JOIN $smw_spec2 s ON s.s_id = a.s_id AND s.sp_id=".SMW_SP_HAS_TYPE." 
+                        JOIN $smw_ids i ON i.smw_id = a.p_id
+                        JOIN $page p ON page_title = i.smw_title AND page_namespace = i.smw_namespace  
+                GROUP BY i.smw_title, s.value_string";
+    }
+    
+    public function getWikiPageProperties($sort) {
+        global $smwgHaloContLang;
+        $NSrel = SMW_NS_PROPERTY;
+        $db =& wfGetDB( DB_SLAVE );
+        $sspa = $smwgHaloContLang->getSpecialSchemaPropertyArray();
+        
+        $smw_ids = $db->tableName('smw_ids');
+        $smw_spec2 = $db->tableName('smw_spec2');
+        $smw_atts2 = $db->tableName('smw_atts2');     
+        $smw_rels2 = $db->tableName('smw_rels2');     
+        $page = $db->tableName('page');
+        
+        $domainAndRange = $db->selectRow($db->tableName('smw_ids'), array('smw_id'), array('smw_title' => smwfGetSemanticStore()->domainRangeHintRelation->getDBkey()) );
+        if ($domainAndRange == NULL) {
+            $domainAndRangeID = -1; // does never exist
+        } else {
+            $domainAndRangeID = $domainAndRange->smw_id;
+        }
+        
+        switch($sort) {
+            
+            case 0: // fall through
+                    
+            case 1: //fall through
+                    
+            case 2: return "(SELECT 'Relations' as type,
+                                {$NSrel} as namespace,
+                               q.smw_title as title,
+                                 s.smw_title as value,
+                                s.smw_namespace as obns,
+                                COUNT(q.smw_title) as count,
+                                p.page_touched as touch
+                              FROM $smw_ids q
+                            JOIN $smw_rels2 n ON q.smw_id = n.p_id
+                            LEFT JOIN $page p ON n.p_id = p.page_id
+                            LEFT JOIN $smw_rels2 m ON n.p_id = m.s_id AND m.p_id = $domainAndRangeID
+                            LEFT JOIN $smw_ids r ON m.o_id = r.smw_id
+                            LEFT JOIN $smw_rels2 o ON m.o_id = o.s_id AND m.p_id = $domainAndRangeID
+                            LEFT JOIN $smw_ids s ON o.o_id = s.smw_id
+                            LEFT JOIN $smw_ids t ON o.p_id = t.smw_id
+                            WHERE q.smw_namespace = 102 AND q.smw_iw != \":smw\" AND t.smw_sortkey = 1
+                            GROUP BY title) 
+                        UNION DISTINCT
+                            (SELECT 'Relations' as type,
+                                {$NSrel} as namespace,
+                                q.smw_title as title,
+                                NULL as value, 
+                                '-1' as obns, 
+                                COUNT(*) as count ,
+                                p.page_touched as touch
+                              FROM $smw_ids q 
+                            JOIN $smw_rels2 n ON q.smw_id = n.p_id
+                            LEFT JOIN $page p ON n.s_id = p.page_id
+                            LEFT JOIN $smw_rels2 m ON n.p_id = m.s_id AND m.p_id = $domainAndRangeID
+                            WHERE q.smw_namespace = 102 AND q.smw_iw != \":smw\" AND m.s_id IS NULL
+                            GROUP BY title)";
+        }
+        
+                
+    }
+    
+    public function getNaryProperties($sort) {
+        global $smwgHaloContLang;
+        $NSrel = SMW_NS_PROPERTY;
+        $db =& wfGetDB( DB_SLAVE );
+        $sspa = $smwgHaloContLang->getSpecialSchemaPropertyArray();
+        
+        $smw_ids = $db->tableName('smw_ids');
+        $smw_spec2 = $db->tableName('smw_spec2');
+        $smw_atts2 = $db->tableName('smw_atts2');     
+        $smw_rels2 = $db->tableName('smw_rels2');     
+        $page = $db->tableName('page');
+        
+        $domainAndRange = $db->selectRow($db->tableName('smw_ids'), array('smw_id'), array('smw_title' => smwfGetSemanticStore()->domainRangeHintRelation->getDBkey()) );
+        if ($domainAndRange == NULL) {
+            $domainAndRangeID = -1; // does never exist
+        } else {
+            $domainAndRangeID = $domainAndRange->smw_id;
+        }
+        
+        switch($sort) {
+            
+            case 0: // fall through
+                    
+            case 1: //fall through
+                    
+            case 2: return "(SELECT 'Relations' as type,
+                                {$NSrel} as namespace,
+                               q.smw_title as title,
+                                 s.smw_title as value,
+                                s.smw_namespace as obns,
+                                COUNT(q.smw_title) as count,
+                                p.page_touched as touch
+                              FROM $smw_ids q
+                            JOIN $smw_rels2 n ON q.smw_id = n.p_id
+                            LEFT JOIN $page p ON n.p_id = p.page_id
+                            LEFT JOIN $smw_rels2 m ON n.p_id = m.s_id AND m.p_id = $domainAndRangeID
+                            LEFT JOIN $smw_ids r ON m.o_id = r.smw_id
+                            LEFT JOIN $smw_rels2 o ON m.o_id = o.s_id AND m.p_id = $domainAndRangeID
+                            LEFT JOIN $smw_ids s ON o.o_id = s.smw_id
+                            LEFT JOIN $smw_ids t ON o.p_id = t.smw_id
+                            WHERE q.smw_namespace = 102 AND q.smw_iw != \":smw\" AND t.smw_sortkey = 1
+                            GROUP BY title) 
+                        UNION DISTINCT
+                            (SELECT 'Relations' as type,
+                                {$NSrel} as namespace,
+                                q.smw_title as title,
+                                NULL as value, 
+                                '-1' as obns, 
+                                COUNT(*) as count ,
+                                p.page_touched as touch
+                              FROM $smw_ids q 
+                            JOIN $smw_rels2 n ON q.smw_id = n.p_id
+                            LEFT JOIN $page p ON n.s_id = p.page_id
+                            LEFT JOIN $smw_rels2 m ON n.p_id = m.s_id AND m.p_id = $domainAndRangeID
+                            WHERE q.smw_namespace = 102 AND q.smw_iw != \":smw\" AND m.s_id IS NULL
+                            GROUP BY title)";
+        }
+    }
 }
 ?>
