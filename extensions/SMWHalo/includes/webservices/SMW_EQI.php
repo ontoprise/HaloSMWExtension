b351341026f9e12af06d6d20f7caa3e034d66079
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-08-22 07:36:20 +0000

    fix: support new ask syntax
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2090 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/webservices/SMW_EQI.php
+++ extensions/SMWHalo/includes/webservices/SMW_EQI.php
@@ -6,131 +6,159 @@
  * @param string $queryString in ASK or SPARQL syntax
  * @return XML string
  */
-function query($queryString, $format = "xml") { 
-  $mediaWikiLocation = dirname(__FILE__) . '/../../..';
- 
-  //TODO: import a triple store config file
-  require_once "$mediaWikiLocation/SemanticMediaWiki/includes/SMW_QueryProcessor.php";
-  require_once "$mediaWikiLocation/SMWHalo/includes/SMW_QP_XML.php";
-  
-  global $smwgSPARQLEndpoint;
-  $eqi = new ExternalQueryInterface();
-  // use heuristic to optimize parsing order
-  if (stripos($queryString, "SELECT") !== false) {
-    // parse possible SPARQL query text before ASK
-    $smwgRAPPath = $mediaWikiLocation . "/SemanticMediaWiki/libs/rdfapi-php";
-    $Rdfapi_includes= $smwgRAPPath . '/api/';
-    define("RDFAPI_INCLUDE_DIR", $Rdfapi_includes); // not sure if the constant is needed within RAP
-    include(RDFAPI_INCLUDE_DIR . "RdfAPI.php");
-    include( RDFAPI_INCLUDE_DIR . 'vocabulary/RDF_C.php');
-    include( RDFAPI_INCLUDE_DIR . 'vocabulary/OWL_C.php');
-    include( RDFAPI_INCLUDE_DIR . 'vocabulary/RDFS_C.php');
-    include( RDFAPI_INCLUDE_DIR . 'sparql/SparqlParser.php');
-    
-    $parser = new SparqlParser();
-    try {
-        $query = $parser->parse($queryString);
-        if (isset($smwgSPARQLEndpoint)) {
-            return $eqi->answerSPARQL($queryString);
-        } else {
-            // try to convert to ASK
-            try {
-            	
-                $ask = $eqi->transformSPARQLToASK($query);
-                return $ask;
-                //return $eqi->answerASK($ask);
-            } catch(Exception $e) {
-                return new SoapFault("error_mf_query","Malformed Query","SMWPlus",$e->getMessage());
-            }
-        }
-    } catch(SparqlParserException $e) {
-        $query = SMWQueryProcessor::createQuery($queryString, false, "xml");
-        if (count($query->getErrors()) > 0) {
-            // probably SPARQL query, so return SPARQL parser error
-           return new SoapFault("error_mf_query","Malformed Query","SMWPlus",$e->getMessage());
-        } else {
-            return $eqi->answerASK($queryString, $format);
-        }
-    }
-       
-  } else {
-    // the other way round
-    $query = SMWQueryProcessor::createQuery($queryString, array(), false);
-    if (count($query->getErrors()) > 0) {
-        $smwgRAPPath = $mediaWikiLocation . "/SemanticMediaWiki/libs/rdfapi-php";
-        $Rdfapi_includes= $smwgRAPPath . '/api/';
-        define("RDFAPI_INCLUDE_DIR", $Rdfapi_includes); // not sure if the constant is needed within RAP
-        include(RDFAPI_INCLUDE_DIR . "RdfAPI.php");
-        include( RDFAPI_INCLUDE_DIR . 'vocabulary/RDF_C.php');
-        include( RDFAPI_INCLUDE_DIR . 'vocabulary/OWL_C.php');
-        include( RDFAPI_INCLUDE_DIR . 'vocabulary/RDFS_C.php');
-        include( RDFAPI_INCLUDE_DIR . 'sparql/SparqlParser.php');
-        
-        $parser = new SparqlParser();
-        try {
-           $query = $parser->parse($queryString);
-           return $eqi->answerSPARQL($queryString);
-        } catch(SparqlParserException $e) {
-             $errors = implode(",",$query->getErrors());
-             // probably ASK query, so return SMWProcessor error
-             return new SoapFault("error_mf_query","Malformed Query","SMWPlus",$errors);
-        }
-      
-    } else {
-        return $eqi->answerASK($queryString, $format);
-    }
-  }
-} 
+function query($rawQuery, $format = "xml") {
+	$mediaWikiLocation = dirname(__FILE__) . '/../../..';
+
+	//TODO: import a triple store config file
+	require_once "$mediaWikiLocation/SemanticMediaWiki/includes/SMW_QueryProcessor.php";
+	require_once "$mediaWikiLocation/SMWHalo/includes/SMW_QP_XML.php";
+
+	global $smwgSPARQLEndpoint;
+	$eqi = new ExternalQueryInterface();
+	// use heuristic to optimize parsing order
+	if (stripos($rawQuery, "SELECT") !== false) {
+		// parse possible SPARQL query text before ASK
+		$smwgRAPPath = $mediaWikiLocation . "/SemanticMediaWiki/libs/rdfapi-php";
+		$Rdfapi_includes= $smwgRAPPath . '/api/';
+		define("RDFAPI_INCLUDE_DIR", $Rdfapi_includes); // not sure if the constant is needed within RAP
+		include(RDFAPI_INCLUDE_DIR . "RdfAPI.php");
+		include( RDFAPI_INCLUDE_DIR . 'vocabulary/RDF_C.php');
+		include( RDFAPI_INCLUDE_DIR . 'vocabulary/OWL_C.php');
+		include( RDFAPI_INCLUDE_DIR . 'vocabulary/RDFS_C.php');
+		include( RDFAPI_INCLUDE_DIR . 'sparql/SparqlParser.php');
+
+		$parser = new SparqlParser();
+		try {
+			$query = $parser->parse($rawQuery);
+			if (isset($smwgSPARQLEndpoint)) {
+				return $eqi->answerSPARQL($rawQuery);
+			} else {
+				// try to convert to ASK
+				try {
+
+					$ask = $eqi->transformSPARQLToASK($query);
+					return $ask;
+					//return $eqi->answerASK($ask);
+				} catch(Exception $e) {
+					return new SoapFault("error_mf_query","Malformed Query","SMWPlus",$e->getMessage());
+				}
+			}
+		} catch(SparqlParserException $e) {
+			$query = SMWQueryProcessor::createQuery($rawQuery, false, "xml");
+			if (count($query->getErrors()) > 0) {
+				// probably SPARQL query, so return SPARQL parser error
+				return new SoapFault("error_mf_query","Malformed Query","SMWPlus",$e->getMessage());
+			} else {
+				return $eqi->answerASK($rawQuery, $format);
+			}
+		}
+			
+	} else {
+		// the other way round
+
+		// truncate any parameters or printouts, before parsing
+		$paramPos = strpos($rawQuery, "|");
+		if ($paramPos === false) {
+			$queryString = $rawQuery;
+		} else {
+			$queryString = substr($rawQuery, 0, $paramPos);
+		}
+			
+		$query = SMWQueryProcessor::createQuery($queryString, array(), false);
+		if (count($query->getErrors()) > 0) {
+			$smwgRAPPath = $mediaWikiLocation . "/SemanticMediaWiki/libs/rdfapi-php";
+			$Rdfapi_includes= $smwgRAPPath . '/api/';
+			define("RDFAPI_INCLUDE_DIR", $Rdfapi_includes); // not sure if the constant is needed within RAP
+			include(RDFAPI_INCLUDE_DIR . "RdfAPI.php");
+			include( RDFAPI_INCLUDE_DIR . 'vocabulary/RDF_C.php');
+			include( RDFAPI_INCLUDE_DIR . 'vocabulary/OWL_C.php');
+			include( RDFAPI_INCLUDE_DIR . 'vocabulary/RDFS_C.php');
+			include( RDFAPI_INCLUDE_DIR . 'sparql/SparqlParser.php');
+
+			$parser = new SparqlParser();
+			try {
+				$query = $parser->parse($rawQuery);
+				return $eqi->answerSPARQL($rawQuery);
+			} catch(SparqlParserException $e) {
+				$errors = implode(",",$query->getErrors());
+				// probably ASK query, so return SMWProcessor error
+				return new SoapFault("error_mf_query","Malformed Query","SMWPlus",$errors);
+			}
+
+		} else {
+			return $eqi->answerASK($rawQuery, $format);
+		}
+	}
+}
 
 class ExternalQueryInterface {
-    
-    /**
-     * Answers a ASK query.
-     *
-     * @param string $queryString
-     * @return SPARQL XML string
-     */
-    function answerASK($queryString, $format = "xml") {
-      SMWQueryProcessor::$formats['xml'] = 'SMWXMLResultPrinter';
-      $result =  SMWQueryProcessor::getResultFromHookParams($queryString,array('format' => $format),SMW_OUTPUT_HTML);
-      return $result;
-    }
-    
-    /**
-     * Answers a SPARQL query.
-     *
-     * @param string $queryString
-     * @return SPARQL XML string
-     */
-    function answerSPARQL($queryString) {
-    	global $wgServer, $wgScript;
-        $client = new SoapClient("$wgServer$wgScript?action=get_sparql");
-                
-        try {
-             global $smwgNamespace;
-             $response = $client->query($queryString, $smwgNamespace);
-             return $response;
 
-         } catch(Exception $e) {
-             return ""; // What to return here?
-         }
-    }
-    
-    /**
-     * Converts a SPARQL query to ASK syntax
-     *
-     * @param Query $query (object from SPARQLParser)
-     */
-    function transformSPARQLToASK($query) {
-    	require_once 'SMW_SPARQLTransformer.php';
-        $st = new SMWSPARQLTransformer($query);
-        return $st->transform();
+	/**
+	 * Answers a ASK query.
+	 *
+	 * @param string $rawQuery
+	 * @return SPARQL XML string
+	 */
+	function answerASK($rawquery, $format = "xml") {
         
-    }
+		// add desired query printer (SPARQL-XML)
+		SMWQueryProcessor::$formats['xml'] = 'SMWXMLResultPrinter';
+
+		// add query as first rawparam
+		$paramPos = strpos($rawquery, "|");
+		$rawparams[] = $paramPos === false ? $rawquery : substr($rawquery, 0, $paramPos);
+		if ($paramPos !== false) {
+			// add other params
+			$ps = explode("|", substr($rawquery, $paramPos + 1));
+			foreach ($ps as $param) {
+				$param = trim($param);
+				$rawparams[] = $param;
+			}
+		}
+		// parse params and answer query
+		SMWQueryProcessor::processFunctionParams($rawparams,$querystring,$params,$printouts);
+		$params['format'] = $format;
+		return SMWQueryProcessor::getResultFromQueryString($querystring,$params,$printouts, SMW_OUTPUT_HTML, SMWQueryProcessor::SPECIAL_PAGE);
+
+
+			
+	}
+
+	/**
+	 * Answers a SPARQL query.
+	 *
+	 * @param string $rawQuery
+	 * @return SPARQL XML string
+	 */
+	function answerSPARQL($rawQuery) {
+		global $wgServer, $wgScript;
+		$client = new SoapClient("$wgServer$wgScript?action=get_sparql");
+
+		try {
+			global $smwgNamespace;
+			$response = $client->query($rawQuery, $smwgNamespace);
+			return $response;
+
+		} catch(Exception $e) {
+			return ""; // What to return here?
+		}
+	}
+
+	/**
+	 * Converts a SPARQL query to ASK syntax
+	 *
+	 * @param Query $query (object from SPARQLParser)
+	 */
+	function transformSPARQLToASK($query) {
+		require_once 'SMW_SPARQLTransformer.php';
+		$st = new SMWSPARQLTransformer($query);
+		return $st->transform();
+
+	}
 }
 
 class MalformedQueryException extends Exception {
-	
+
 }
 
 ?>
