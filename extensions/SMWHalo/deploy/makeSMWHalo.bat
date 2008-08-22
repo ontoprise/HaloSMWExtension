08fba9e5c3405ea6cb1dd31a04a6c4a618ba5077
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-08-22 14:49:07 +0000

    SMW patches for Halar
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2095 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/deploy/makeSMWHalo.bat
+++ extensions/SMWHalo/deploy/makeSMWHalo.bat
@@ -17,12 +17,17 @@ mkdir %OUTPUT_DIR%\skins\ontoskin
 mkdir %OUTPUT_DIR%\includes
 
 mkdir %OUTPUT_DIR%\extensions\SMWHalo
+mkdir %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes
+mkdir %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes\storage
 
 REM copy files
 
 xcopy ..\* %OUTPUT_DIR%\extensions\SMWHalo /S /EXCLUDE:excludeForHalo.dat /Y
 
-REM Patches for SMW (NONE!)
+REM Patches for SMW 
+xcopy ..\..\SemanticMediaWiki\includes\SMW_FactBox.php %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes /Y
+xcopy ..\..\SemanticMediaWiki\includes\storage\SMW_SQLStore.php %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes\storage /Y
+xcopy ..\..\SemanticMediaWiki\includes\storage\SMW_SQLStore2.php %OUTPUT_DIR%\extensions\SemanticMediaWiki\includes\storage /Y
 
 REM Patches for MW
 xcopy ..\..\..\includes\User.php %OUTPUT_DIR%\includes /Y
