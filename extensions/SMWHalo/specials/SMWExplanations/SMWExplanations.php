936ac00dbe40a2904b85e6586dc55f82ee11cc29
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-08-28 14:02:11 +0000

    Added explanations for simple rules
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2136 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/SMWHalo/specials/SMWExplanations/SMWExplanations.php
@@ -0,0 +1,205 @@
+<?php
+
+ if (!defined('MEDIAWIKI')) die();
+
+global $IP;
+require_once( $IP . "/includes/SpecialPage.php" );
+
+/*
+ * Standard class that is resopnsible for the creation of the Special Page
+ */
+class SMWExplanations extends SpecialPage {
+	
+	public function __construct() {
+		parent::__construct('Explanations');
+		$this->explanation = "";
+	}
+/*
+ * Overloaded function that is resopnsible for the creation of the Special Page
+ */
+	public function execute() {
+
+		global $wgRequest, $wgOut, $smwgHaloScriptPath;
+		$wgOut->setPageTitle(wfMsg('smw_explanations'));
+		
+		//get request parameters
+		$instance = $wgRequest->getVal( 'i' );
+		$property = $wgRequest->getVal( 'p' );
+		$value = $wgRequest->getVal( 'v' );
+		$category = $wgRequest->getVal( 'c' );
+		$mode = $wgRequest->getVal('mode');
+		$html = "";
+		
+		if($mode == "category"){
+			$html = $this->getGUIHeader($mode, $instance, "", "", $category);
+			if ($instance == "" || $category == ""){
+				$html .= wfMsg('smw_expl_not_all_inputs');
+			} else {
+				$query = $this->getCategoryExplanationQuery($instance, $category);
+				$explanation = $this->getExplanationForQuery($query);
+				$html .= $this->formatExplanation($explanation);
+			}
+		} else {
+			$html = $this->getGUIHeader($mode, $instance, $property, $value, "");
+			if ($instance == "" || $property == "" || $value == ""){
+				$html .= wfMsg('smw_expl_not_all_inputs');
+			} else {
+				$query = $this->getPropertyExplanationQuery($instance, $property, $value);
+				$explanation = $this->getExplanationForQuery($query);
+				$html .= $this->formatExplanation($explanation);
+			}
+		}
+		
+		$wgOut->addHTML($html);
+	}
+	/*
+	 * This method is called whenever the Special Page is opened and
+	 * either one of the necessary parameters is missing.
+	 * Available parameters will be filled into the input fields.
+	 */
+	
+	function getGUIHeader($mode, $i, $p, $v, $c){
+		global $smwgHaloScriptPath;
+		$imagepath = $smwgHaloScriptPath . '/skins/Explanations';
+		$article = wfMsg('article');
+		$property = Namespace::getCanonicalName(SMW_NS_PROPERTY);
+		$value = wfMsg('smw_expl_value');
+		$category = Namespace::getCanonicalName(NS_CATEGORY);
+		$explainProp = wfMsg('smw_expl_explain_property');
+		$explainCat = wfMsg('smw_expl_explain_category');
+		$imgtitle = wfMsg('smw_expl_img');
+		$imgstring = <<<IMG
+		<image src="$imagepath/cog_go.png" alt="$imgtitle" title="$imgtitle" style="cursor:pointer" onclick="explanation.trigger()"/>
+IMG;
+		if($mode == "category"){
+			$cchecked = "checked";
+			$pchecked = "";
+			$cdisplay = "";
+			$pdisplay = "none";
+		} else {
+			$cchecked = "";
+			$pchecked = "checked";
+			$cdisplay = "none";
+			$pdisplay = "";
+		}
+		$html = <<<END
+		<div style="background:#F1F7AE; padding:5px"><form>
+		<table style="background-color:#F1F7AE;"><tr><td>
+		<input type="radio" name="SelectExplanationType" value="Explain property assignment" onclick="explanation.radioChecked()" id="pcheck" $pchecked /></td><td>$explainProp&nbsp;&nbsp;&nbsp;</td>
+		<td style="display:$pdisplay" id="propertyrow">
+		$article: <input id="instance" class="wickEnabled" type="text" value="$i" typehint="0"/>&nbsp;&nbsp;
+		$property: <input id="property" class="wickEnabled" type="text" value="$p" typehint="102"/>&nbsp;&nbsp;
+		$value: <input id="value" class="wickEnabled" type="text" value="$v" $pdisabled/>&nbsp;&nbsp;
+		$imgstring<br/></td></tr><tr><td>
+		<input type="radio" name="SelectExplanationType" value="Explain category assignment" onclick="explanation.radioChecked()" id="ccheck" $cchecked/></td><td>$explainCat&nbsp;&nbsp;&nbsp;</td>
+		<td style="display:$cdisplay" id="categoryrow">
+		$article: <input id="instance1" class="wickEnabled" type="text" value="$i" typehint="0"/>&nbsp;&nbsp;
+		$category: <input id="category" class="wickEnabled" type="text" value="$c" typehint="14"/>&nbsp;&nbsp;
+		$imgstring</td></tr></table></form></div><hr/><br/>
+END;
+		return $html;	
+	}
+	
+	function getPropertyExplanationQuery($i, $p, $v){
+		global $smwgNamespace;
+		$propertyTitle = Title::newFromText($p, SMW_NS_PROPERTY);
+
+		$isRelation = true;
+		// Check if its a page property (relation) or attribute
+		if($propertyTitle->exists()){
+			$p = $propertyTitle->getDBkey();
+			$dbr =& wfGetDB( DB_SLAVE );
+			$smw_specialprops = $dbr->tableName('smw_specialprops');
+			$res = $dbr->query('SELECT * FROM ' . $smw_specialprops . ' WHERE subject_title = "'. $p .'" AND property_id = 1');
+			while ( $row = $dbr->fetchObject( $res ) ) {
+				if ($row->value_string != "_wpg")
+					$isRelation = false;
+			}
+			$dbr->freeResult( $res );
+		}
+
+		$p = str_replace(" ", "_", $p);
+		$p = '"' . $smwgNamespace . '/property#"#' . $p;
+		
+		if($isRelation){
+			$valueTitle = Title::newFromText($v);
+			if($valueTitle->exists()){
+				$ns = $valueTitle->getNamespace();
+				switch($ns){
+					case NS_CATEGORY:
+						$v = '"' . $smwgNamespace . '/category#"#' . $valueTitle->getDBkey();
+						break;
+					case SMW_NS_PROPERTY:
+						$v = '"' . $smwgNamespace . '/property#"#' . $valueTitle->getDBkey();
+						break;
+					default:
+						$v = '"' . $smwgNamespace . '/a#"#' . $valueTitle->getDBkey();
+				}
+			} else {
+				$v = '"' . $smwgNamespace . '/a#"#' . $v;
+			}
+		} else {
+			if(!is_numeric($v))
+				$v = '"' . $v . '"';
+		}
+		
+		$instanceTitle = Title::newFromText($i);
+		if($instanceTitle->exists()){
+			$ns = $instanceTitle->getNamespace();
+			switch($ns){
+				case NS_CATEGORY:
+					$i = '"' . $smwgNamespace . '/category#"#' . $instanceTitle->getDBkey();
+					break;
+				case SMW_NS_PROPERTY:
+					$i = '"' . $smwgNamespace . '/property#"#' . $instanceTitle->getDBkey();
+					break;
+				default:
+					$i = '"' . $smwgNamespace . '/a#"#' . $instanceTitle->getDBkey();
+			}
+		} else {
+			$i = '"' . $smwgNamespace . '/a#"#' . $i;
+		}
+	
+		$query = "FORALL X,Y <- " . ($isRelation?("attr_(X, $p, Y) "):("attl_(X, $p, Y, \"\") ")) .  "AND equal(X,$i) AND equal(Y,$v).;explain";
+		return $query;
+	}
+	
+	function getCategoryExplanationQuery($i, $c){
+		global $smwgNamespace;
+		$category = Title::newFromText($c, NS_CATEGORY);
+		$instance = Title::newFromText($i);
+		if ($category->exists())
+			$category = $category->getDBkey();
+		if ($instance->exists())
+			$instance = $instance->getDBkey();
+		$category = str_replace(" ", "_", $category);
+		$instance = str_replace(" ", "_", $instance);
+	
+		$query = 'FORALL X <- isa_(X, "' . $smwgNamespace . '/category#"#' . $c .') AND equal(X,"' . $smwgNamespace . '/a#"#' . $instance . ').;explain';
+		return $query;
+	}
+	
+	function getExplanationForQuery($query){
+		try {
+			global $wgServer, $wgScript, $smwgNamespace;
+			ini_set("soap.wsdl_cache_enabled", "0");  //set for debugging
+			$_client = new SoapClient("$wgServer$wgScript?action=get_explanation");          
+	 		$explanation = $_client->explainFloQuery($smwgNamespace, $query);
+			
+	 		if($expanation == "-1"){
+	 			$explanation = "No explanation could be provided. Please check if all inputs are correct.";
+	 		}
+			return $explanation;
+		} catch (Exception $e){
+			return wfMsg('smw_expl_error') . "<br/><br/><i>" . $e . "</i>";
+		}
+	}
+	
+	function formatExplanation($exp){
+		$exp = str_replace("BECAUSE", wfMsg('smw_expl_because') . "<br/>", $exp);
+		$exp = str_replace("AND", wfMsg('smw_expl_and') . "<br/>", $exp);
+		return $exp;
+	}
+
+}
+?>
