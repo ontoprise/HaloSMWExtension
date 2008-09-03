7407b795497edd7c54fcc6fc9cb3e243e88c5349
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-09-03 12:11:18 +0000

    ALTER SMW tables instead of create
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2223 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/storage/SMW_HaloStore.php
+++ extensions/SMWHalo/includes/storage/SMW_HaloStore.php
@@ -12,118 +12,66 @@
  */
 class SMWHaloStore extends SMWSQLStore {
 
-	/**
-	 * Modified to store ratings.
-	 */
-	function updateData(SMWSemanticData $data, $newpage) {
-		wfProfileIn("SMWHaloStore::updateData (SMW)");
-		$dbkey = $data->getSubject()->getDBkey();
-		$annotations = smwfGetSemanticStore()->getRatedAnnotations($dbkey);
-		parent::updateData($data, $newpage);
-		if ($annotations !== NULL) {
-			foreach($annotations as $pa) {
-				smwfGetSemanticStore()->rateAnnotation($dbkey, $pa[0], $pa[1], $pa[2] );
-			}
-		}
-		wfProfileOut("SMWHaloStore::updateData (SMW)");
-	}
+    /**
+     * Modified to store ratings.
+     */
+    function updateData(SMWSemanticData $data, $newpage) {
+        wfProfileIn("SMWHaloStore::updateData (SMW)");
+        $dbkey = $data->getSubject()->getDBkey();
+        $annotations = smwfGetSemanticStore()->getRatedAnnotations($dbkey);
+        parent::updateData($data, $newpage);
+        if ($annotations !== NULL) {
+            foreach($annotations as $pa) {
+                smwfGetSemanticStore()->rateAnnotation($dbkey, $pa[0], $pa[1], $pa[2] );
+            }
+        }
+        wfProfileOut("SMWHaloStore::updateData (SMW)");
+    }
 
-	/**
-	 * Modified to include "rating" column in relations and attributes table.
-	 */
-	function setup($verbose = true) {
-		global $wgDBtype;
-		$this->reportProgress("Setting up standard database configuration for SMW+Halo ...\n\n",$verbose);
-
-		if ($wgDBtype === 'postgres') {
-			$this->reportProgress("For Postgres, please import the file SMW_Postgres_Schema.sql manually\n",$verbose);
-			return;
-		}
-
-		$db =& wfGetDB( DB_MASTER );
-
-		extract( $db->tableNames('smw_relations', 'smw_attributes', 'smw_longstrings', 'smw_specialprops', 'smw_subprops', 'smw_nary', 'smw_nary_attributes', 'smw_nary_longstrings', 'smw_nary_relations') );
-
-		// create relation table
-		$this->setupTable($smw_relations,
-		              array('subject_id'        => 'INT(8) UNSIGNED NOT NULL',
-		                    'subject_namespace' => 'INT(11) NOT NULL',
-		                    'subject_title'     => 'VARCHAR(255) binary NOT NULL',
-		                    'relation_title'    => 'VARCHAR(255) binary NOT NULL',
-		                    'object_namespace'  => 'INT(11) NOT NULL',
-		                    'object_title'      => 'VARCHAR(255) binary NOT NULL',
-		                    'object_id'         => 'INT(8) UNSIGNED',
-		                    'rating'            => 'INT(8)'), $db, $verbose);
-		$this->setupIndex($smw_relations, array('subject_id','relation_title','object_title,object_namespace','object_id'), $db);
-
-		// create attribute table
-		$this->setupTable($smw_attributes,
-		              array('subject_id'        => 'INT(8) UNSIGNED NOT NULL',
-		                    'subject_namespace' => 'INT(11) NOT NULL',
-		                    'subject_title'     => 'VARCHAR(255) binary NOT NULL',
-		                    'attribute_title'   => 'VARCHAR(255) binary NOT NULL',
-		                    'value_unit'        => 'VARCHAR(63) binary',
-		                    'value_xsd'         => 'VARCHAR(255) binary NOT NULL',
-		                    'value_num'         => 'DOUBLE',
-		                    'rating'            => 'INT(8)'), $db, $verbose);
-		$this->setupIndex($smw_attributes, array('subject_id','attribute_title','value_num','value_xsd'), $db);
-
-		// create table for long string attributes
-		$this->setupTable($smw_longstrings,
-		              array('subject_id'        => 'INT(8) UNSIGNED NOT NULL',
-		                    'subject_namespace' => 'INT(11) NOT NULL',
-		                    'subject_title'     => 'VARCHAR(255) binary NOT NULL',
-		                    'attribute_title'   => 'VARCHAR(255) binary NOT NULL',
-		                    'value_blob'        => 'MEDIUMBLOB'), $db, $verbose);
-		$this->setupIndex($smw_longstrings, array('subject_id','attribute_title'), $db);
-
-		// set up according tables for nary properties
-		$this->setupTable($smw_nary,
-		              array('subject_id'        => 'INT(8) UNSIGNED NOT NULL',
-		                    'subject_namespace' => 'INT(11) NOT NULL',
-		                    'subject_title'     => 'VARCHAR(255) binary NOT NULL',
-		                    'attribute_title'   => 'VARCHAR(255) binary NOT NULL',
-		                    'nary_key'          => 'INT(8) UNSIGNED NOT NULL'), $db, $verbose);
-		$this->setupIndex($smw_nary, array('subject_id','attribute_title','subject_id,nary_key'), $db);
-		$this->setupTable($smw_nary_relations,
-		              array('subject_id'        => 'INT(8) UNSIGNED NOT NULL',
-		                    'nary_key'          => 'INT(8) UNSIGNED NOT NULL',
-		                    'nary_pos'          => 'INT(8) UNSIGNED NOT NULL',
-		                    'object_namespace'  => 'INT(11) NOT NULL',
-		                    'object_title'      => 'VARCHAR(255) binary NOT NULL',
-		                    'object_id'         => 'INT(8) UNSIGNED'), $db, $verbose);
-		$this->setupIndex($smw_nary_relations, array('subject_id,nary_key','object_title,object_namespace','object_id'), $db);
-		$this->setupTable($smw_nary_attributes,
-		              array('subject_id'        => 'INT(8) UNSIGNED NOT NULL',
-		                    'nary_key'          => 'INT(8) UNSIGNED NOT NULL',
-		                    'nary_pos'          => 'INT(8) UNSIGNED NOT NULL',
-		                    'value_unit'        => 'VARCHAR(63) binary',
-		                    'value_xsd'         => 'VARCHAR(255) binary NOT NULL',
-		                    'value_num'         => 'DOUBLE'), $db, $verbose);
-		$this->setupIndex($smw_nary_attributes, array('subject_id,nary_key','value_num','value_xsd'), $db);
-		$this->setupTable($smw_nary_longstrings,
-		              array('subject_id'        => 'INT(8) UNSIGNED NOT NULL',
-		                    'nary_key'          => 'INT(8) UNSIGNED NOT NULL',
-		                    'nary_pos'          => 'INT(8) UNSIGNED NOT NULL',
-		                    'value_blob'        => 'MEDIUMBLOB'), $db, $verbose);
-		$this->setupIndex($smw_nary_longstrings, array('subject_id,nary_key'), $db);
-
-		// create table for special properties
-		$this->setupTable($smw_specialprops,
-		              array('subject_id'        => 'INT(8) UNSIGNED NOT NULL',
-		                    'property_id'       => 'SMALLINT(6) NOT NULL',
-		                    'value_string'      => 'VARCHAR(255) binary NOT NULL'), $db, $verbose);
-		$this->setupIndex($smw_specialprops, array('subject_id', 'property_id', 'subject_id,property_id'), $db);
-
-		// create table for subproperty relationships
-		$this->setupTable($smw_subprops,
-		              array('subject_title'     => 'VARCHAR(255) binary NOT NULL',
-		                    'object_title'      => 'VARCHAR(255) binary NOT NULL'), $db, $verbose);
-		$this->setupIndex($smw_subprops, array('subject_title', 'object_title'), $db);
-
-		$this->reportProgress("Database initialised successfully.\n",$verbose);
-		return true;
-	}
+    function setup($verbose = true) {
+        parent::setup($verbose);
+        global $wgDBtype;
+        $this->reportProgress("Altering tables for SMW+ if necessary ...\n\n",$verbose);
+        if ($wgDBtype === 'postgres') {
+            $this->reportProgress("Postgres is not supported by SMW+",$verbose);
+            return;
+        }
+        $db =& wfGetDB( DB_MASTER );
+        extract( $db->tableNames('smw_rels2', 'smw_atts2') );
+        
+        if (!$this->isColumnPresent($smw_rels2, 'rating')) {    
+            $this->reportProgress("Altering $smw_rels2...\n",$verbose);
+            $this->reportProgress("\t... adding column rating\n",$verbose);
+            $db->query("ALTER TABLE $smw_rels2 ADD `rating` INT(8) FIRST", 'SMWHaloStore::setupTable');
+            $this->reportProgress("done \n",$verbose);
+        }
+        
+        if (!$this->isColumnPresent($smw_atts2, 'rating')) {  
+            $this->reportProgress("Altering $smw_atts2...\n",$verbose);
+            $this->reportProgress("\t... adding column rating\n",$verbose);
+            $db->query("ALTER TABLE $smw_atts2 ADD `rating` INT(8) FIRST", 'SMWHaloStore::setupTable');
+            $this->reportProgress("done \n",$verbose);
+        }
+        
+        $this->reportProgress("Database initialised for SMW+ successfully.\n",$verbose);
+        return true;
+    }
+    
+    private function isColumnPresent($table, $column) {
+       $db =& wfGetDB( DB_MASTER );
+        
+       $ratingPresent = false; 
+       $res = $db->query( 'DESCRIBE ' . $table, 'SMWSQLStore::setupTable' );
+      
+       while ($row = $db->fetchObject($res)) {
+           $field = $row->Field;
+           if ($field == $column) {
+                $ratingPresent = true;
+           }
+        }
+        $db->freeResult($res);
+        return $ratingPresent;
+    }
 
 
 }
--- extensions/SMWHalo/includes/storage/SMW_HaloStore2.php
+++ extensions/SMWHalo/includes/storage/SMW_HaloStore2.php
@@ -23,78 +23,48 @@ class SMWHaloStore2 extends SMWSQLStore2 {
     }
     
     function setup($verbose = true) {
+        parent::setup($verbose);
         global $wgDBtype;
-        $this->reportProgress("Setting up standard database configuration for SMW ...\n\n",$verbose);
+        $this->reportProgress("Altering tables for SMW+ if necessary ...\n\n",$verbose);
         if ($wgDBtype === 'postgres') {
-            $this->reportProgress("For Postgres, please import the file SMW_Postgres_Schema_2.sql manually\n",$verbose);
+            $this->reportProgress("Postgres is not supported by SMW+",$verbose);
             return;
         }
         $db =& wfGetDB( DB_MASTER );
-        extract( $db->tableNames('smw_ids','smw_rels2','smw_atts2','smw_text2',
-                                 'smw_spec2','smw_subs2','smw_redi2','smw_inst2',
-                                 'smw_conc2') );
-
-        $this->setupTable($smw_ids, // internal IDs used in this store
-                      array('smw_id'        => 'INT(8) UNSIGNED NOT NULL KEY AUTO_INCREMENT',
-                            'smw_namespace' => 'INT(11) NOT NULL',
-                            'smw_title'     => 'VARCHAR(255) binary NOT NULL',
-                            'smw_iw'        => 'CHAR(32)',
-                            'smw_sortkey'   => 'VARCHAR(255) binary NOT NULL'
-                            ), $db, $verbose);
-        $this->setupIndex($smw_ids, array('smw_id','smw_title,smw_namespace,smw_iw', 'smw_sortkey'), $db);
-
-        $this->setupTable($smw_redi2, // fast redirect resolution
-                      array('s_title'     => 'VARCHAR(255) binary NOT NULL',
-                            's_namespace' => 'INT(11) NOT NULL',
-                            'o_id'        => 'INT(8) UNSIGNED NOT NULL'), $db, $verbose);
-        $this->setupIndex($smw_redi2, array('s_title,s_namespace','o_id'), $db);
-
-        $this->setupTable($smw_rels2, // properties with other pages as values ("relations")
-                      array('s_id' => 'INT(8) UNSIGNED NOT NULL',
-                            'p_id' => 'INT(8) UNSIGNED NOT NULL',
-                            'o_id' => 'INT(8) UNSIGNED NOT NULL',
-                            'rating' => 'INT(8)'), $db, $verbose);
-        $this->setupIndex($smw_rels2, array('s_id','p_id','o_id'), $db);
-
-        $this->setupTable($smw_atts2, // most standard properties ("attributes")
-                      array('s_id'        => 'INT(8) UNSIGNED NOT NULL',
-                            'p_id'        => 'INT(8) UNSIGNED NOT NULL',
-                            'value_unit'        => 'VARCHAR(63) binary',
-                            'value_xsd'         => 'VARCHAR(255) binary NOT NULL',
-                            'value_num'         => 'DOUBLE',
-                            'rating'            => 'INT(8)'), $db, $verbose);
-        $this->setupIndex($smw_atts2, array('s_id','p_id','value_num','value_xsd'), $db);
-
-        $this->setupTable($smw_text2, // properties with long strings as values
-                      array('s_id'        => 'INT(8) UNSIGNED NOT NULL',
-                            'p_id'        => 'INT(8) UNSIGNED NOT NULL',
-                            'value_blob'        => 'MEDIUMBLOB'), $db, $verbose);
-        $this->setupIndex($smw_text2, array('s_id','p_id'), $db);
-
-        $this->setupTable($smw_spec2, // (generic builtin) special properties
-                      array('s_id'        => 'INT(8) UNSIGNED NOT NULL',
-                            'sp_id'       => 'SMALLINT(6) NOT NULL',
-                            'value_string'      => 'VARCHAR(255) binary NOT NULL'), $db, $verbose);
-        $this->setupIndex($smw_spec2, array('s_id', 'sp_id', 's_id,sp_id'), $db);
-
-        $this->setupTable($smw_subs2, // subproperty/subclass relationships
-                      array('s_id'        => 'INT(8) UNSIGNED NOT NULL',
-                            'o_id'        => 'INT(8) UNSIGNED NOT NULL',), $db, $verbose);
-        $this->setupIndex($smw_subs2, array('s_id', 'o_id'), $db);
-
-        $this->setupTable($smw_inst2, // class instances (s_id the element, o_id the class)
-                      array('s_id'        => 'INT(8) UNSIGNED NOT NULL',
-                            'o_id'        => 'INT(8) UNSIGNED NOT NULL',), $db, $verbose);
-        $this->setupIndex($smw_inst2, array('s_id', 'o_id'), $db);
-
-        $this->setupTable($smw_conc2, // concept descriptions
-                      array('s_id'        => 'INT(8) UNSIGNED NOT NULL KEY',
-                            'concept_txt' => 'MEDIUMBLOB',
-                            'concept_docu'=> 'MEDIUMBLOB'), $db, $verbose);
-        $this->setupIndex($smw_conc2, array('s_id'), $db);
-
-        $this->reportProgress("Database initialised successfully.\n",$verbose);
+        extract( $db->tableNames('smw_rels2', 'smw_atts2') );
+        
+        if (!$this->isColumnPresent($smw_rels2, 'rating')) {    
+            $this->reportProgress("Altering $smw_rels2...\n",$verbose);
+            $this->reportProgress("\t... adding column rating\n",$verbose);
+            $db->query("ALTER TABLE $smw_rels2 ADD `rating` INT(8) FIRST", 'SMWHaloStore::setupTable');
+            $this->reportProgress("done \n",$verbose);
+        }
+        
+        if (!$this->isColumnPresent($smw_atts2, 'rating')) {  
+            $this->reportProgress("Altering $smw_atts2...\n",$verbose);
+            $this->reportProgress("\t... adding column rating\n",$verbose);
+            $db->query("ALTER TABLE $smw_atts2 ADD `rating` INT(8) FIRST", 'SMWHaloStore::setupTable');
+            $this->reportProgress("done \n",$verbose);
+        }
+        
+        $this->reportProgress("Database initialised for SMW+ successfully.\n",$verbose);
         return true;
+    }
+    
+    private function isColumnPresent($table, $column) {
+       $db =& wfGetDB( DB_MASTER );
+        
+       $ratingPresent = false; 
+       $res = $db->query( 'DESCRIBE ' . $table, 'SMWSQLStore::setupTable' );
+      
+       while ($row = $db->fetchObject($res)) {
+           $field = $row->Field;
+           if ($field == $column) {
+                $ratingPresent = true;
+           }
+        }
+        $db->freeResult($res);
+        return $ratingPresent;
     }
 }
 ?>
