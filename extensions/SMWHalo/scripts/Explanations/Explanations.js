1475f775817c5bd242d67374f5791eb483d8ecde
joerg <joerg@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-09-05 14:25:56 +0000

    added missing explanation JS
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2251 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/SMWHalo/scripts/Explanations/Explanations.js
@@ -0,0 +1,59 @@
+var explanation = null;
+
+var Explanations = Class.create();
+Explanations.prototype = {
+
+initialize:function(){
+
+},
+//TODO: i18n
+
+trigger:function(){
+	// Check inputs
+	if($("pcheck").checked){
+		var ins = escape($("instance").value.strip());
+		var prop = escape($("property").value.strip());
+		var val = escape($("value").value.strip());
+		if (ins == "" || prop == "" || val == ""){
+			alert("Please fill out all three input fields");
+			return;
+		}
+		// If all inputs set: trigger explanation
+		var path = wgArticlePath.replace(/\$1/g, wgPageName);
+		var url = wgServer + path + "?mode=property&i=" + ins + "&p=" + prop + "&v=" + val;
+		window.location.href=url;
+	} else {
+		var ins = escape($("instance1").value.strip());
+		var cat = escape($("category").value.strip());
+		if (ins == "" || cat == ""){
+			alert("Please fill out all two input fields");
+			return;
+		}
+		// If all inputs set: trigger explanation
+		var path = wgArticlePath.replace(/\$1/g, wgPageName);
+		var url = wgServer + path + "?mode=category&i=" + ins + "&c=" + cat;
+		window.location.href=url;
+	}
+	
+},
+
+radioChecked:function(){
+	if($("pcheck").checked){
+		$("propertyrow").style.display="";
+		$("categoryrow").style.display="none";
+	} else {
+		$("propertyrow").style.display="none";
+		$("categoryrow").style.display="";
+	}
+	//;
+	
+}
+	
+}
+
+
+Event.observe(window, 'load', initialize_exp);
+
+function initialize_exp(){
+	explanation = new Explanations();
+}
