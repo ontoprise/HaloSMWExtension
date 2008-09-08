b38958adba1b7d56546f4988891fcddbe4afdfa5
joerg <joerg@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-09-08 12:28:19 +0000

    added missing webservice calls
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2263 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- index.php
+++ index.php
@@ -104,8 +104,34 @@ if ($action == 'get_eqi') {
     
     echo str_replace("{{wiki-path}}", $_SERVER['SERVER_ADDR'].":".$_SERVER['SERVER_PORT'].$_SERVER['SCRIPT_NAME'], $contents);
     exit;
+} else if ($action == 'get_sparql') {
+    $wsdl = "extensions/SMWHalo/includes/webservices/sparql.wsdl";
+    $handle = fopen($wsdl, "rb");
+    $contents = fread ($handle, filesize ($wsdl));
+    fclose($handle);
+    global $smwgSPARQLEndpoint;
+    if (isset($smwgSPARQLEndpoint)) echo str_replace("{{sparql-endpoint}}", $smwgSPARQLEndpoint, $contents); 
+        else echo "No SPARQL endpoint defined! Set \$smwgSPARQLEndpoint in your LocalSettings.php. E.g.: \$smwgSPARQLEndpoint = \"localhost:8080\"";
+    exit;
+} else if ($action == 'get_flogic') {
+    $wsdl = "extensions/SMWHalo/includes/webservices/flogic.wsdl";
+    $handle = fopen($wsdl, "rb");
+    $contents = fread ($handle, filesize ($wsdl));
+    fclose($handle);
+    global $smwgFlogicEndpoint;
+    if (isset($smwgFlogicEndpoint)) echo str_replace("{{flogic-endpoint}}", $smwgFlogicEndpoint, $contents); 
+        else echo "No FLogic endpoint defined! Set \$smwgFlogicEndpoint in your LocalSettings.php. E.g.: \$smwgFlogicEndpoint = \"localhost:8080\"";
+    exit;
+} else if ($action == 'get_explanation') {
+    $wsdl = "extensions/SMWHalo/includes/webservices/explanation.wsdl";
+    $handle = fopen($wsdl, "rb");
+    $contents = fread ($handle, filesize ($wsdl));
+    fclose($handle);
+    global $smwgExplanationEndpoint;
+    if (isset($smwgExplanationEndpoint)) echo str_replace("{{explanation-endpoint}}", $smwgExplanationEndpoint, $contents); 
+        else echo "No FLogic endpoint defined! Set \$smwgExplanationEndpoint in your LocalSettings.php. E.g.: \$smwgExplanationEndpoint = \"localhost:8080\"";
+    exit;
 }
-
 
 wfProfileOut( 'main-misc-setup' );
 
