720caa1b84797935684a285452b427832b90a036
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-09-30 08:26:27 +0000

    delete log only if it exists
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2332 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests/gardening/runTests.bat
+++ extensions/SMWHalo/tests/gardening/runTests.bat
@@ -9,5 +9,5 @@ php SMW_startBot.php -b smw_consistencybot
 REM compare results with saved logs
 cd..
 cd tests\gardening
-del %1
+IF EXIST %1 del %1
 php SMW_compareLog.php -l logs -b smw_consistencybot >> %1
