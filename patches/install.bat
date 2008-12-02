c61113b7c362a1f53ec06dbb61601b6e3992d79a
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-12-02 11:12:57 +0000

    added mime.types file
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2529 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- patches/install.bat
+++ patches/install.bat
@@ -27,6 +27,7 @@ xcopy includes\Linker.php ..\includes /Y
 xcopy includes\Namespace.php ..\includes /Y
 xcopy includes\Title.php ..\includes /Y
 xcopy includes\Wiki.php ..\includes /Y
+xcopy includes\mime.types ..\includes /Y
 xcopy includes\specials\SpecialFileDuplicateSearch.php ..\includes\specials /Y
 xcopy includes\specials\SpecialFilePath.php ..\includes\specials /Y
 xcopy includes\specials\SpecialImagelist.php ..\includes\specials /Y
