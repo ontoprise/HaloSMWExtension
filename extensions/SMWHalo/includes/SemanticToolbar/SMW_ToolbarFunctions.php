2ac93b9173ff0d2b091cf15aa0c2d0bf1e45421e
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2008-12-25 19:14:58 +0000

    added smwf_tb_getTripleStoreStatus
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2581 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/SemanticToolbar/SMW_ToolbarFunctions.php
+++ extensions/SMWHalo/includes/SemanticToolbar/SMW_ToolbarFunctions.php
@@ -12,6 +12,7 @@ $wgAjaxExportList[] = 'smwf_tb_getLinks';
 $wgAjaxExportList[] = 'smwf_tb_GetBuiltinDatatypes';
 $wgAjaxExportList[] = 'smwf_tb_GetUserDatatypes';
 $wgAjaxExportList[] = 'smwf_tb_AskQuestion';
+$wgAjaxExportList[] = 'smwf_tb_getTripleStoreStatus';
 
 /**
  * This function will load context sensitive help from the language help files
@@ -24,19 +25,19 @@ function smwf_tb_GetHelp($namespace, $action){
 	global $wgScriptPath, $smwgHaloScriptPath, $smwgAllowNewHelpQuestions;
 	$html = '';
 	$results = false;
-	
+
 	$tb_store = SMWToolbarStorage::getToolbarStorage();
 	$helppages = $tb_store->getHelppages($namespace, $action);
 	$discourseState = $namespace . ":" . $action;
-	
+
 	foreach($helppages as $id){
 		$questions = $tb_store->getQuestions($id);
 		//get questions
-		
+
 		if(is_array($questions)){
 			$results = true;
 			$wikiTitle = Title::newFromText($questions[0], NS_HELP);
-	
+
 			if($questions[2] == wfMsg('smw_csh_newquestion')){
 				$html .= '<a href="' . $wikiTitle->getFullURL();
 				$html .= '?action=edit" class="new';
@@ -57,7 +58,7 @@ function smwf_tb_GetHelp($namespace, $action){
 	}
 	$specialTitle = Title::newFromText('ContextSensitiveHelp', NS_SPECIAL);
 	$html .= '<div id="morehelp"><a href="' . $specialTitle->getFullURL() . '?restriction=all&ds=' . $discourseState .'" target="_new">(more)</a></div><br/>';
-	
+
 	if ($smwgAllowNewHelpQuestions){
 		$html .= '<a href="javascript:void(0)" onclick="$(\'askHelp\').show()">Ask your own question</a><br/>';
 		$html .= '<div id="askHelp" style="display:none"><input id="question" name="question" type="text" size="20" onKeyPress="return submitenter(this,event)"/>';
@@ -92,11 +93,11 @@ function smwf_tb_AskQuestion($namespace, $action, $question){
 	if($wgTitle->exists()){
 		return wfMsg('smw_help_pageexists');
 	}
-	
+
 
 	/*STARTLOG*/
 	$logmsg = "Added question '$question'";
-    smwLog($logmsg, "CSH", "help_addednew", wfTimestampNow());
+	smwLog($logmsg, "CSH", "help_addednew", wfTimestampNow());
 	/*ENDLOG*/
 
 
@@ -137,7 +138,7 @@ function smwf_tb_getLinks($articleId){
 	$url = preg_replace('/\/$/', '', $url);
 	$res = $dbr->select( $dbr->tableName('pagelinks'),
 		'*',
-		array('pl_from=' . $articleId, 'pl_namespace=0'));
+	array('pl_from=' . $articleId, 'pl_namespace=0'));
 
 	if ($dbr->numRows($res) > 0){
 		$linksExist = true;
@@ -170,7 +171,7 @@ function smwf_tb_GetBuiltinDatatypes(){
 
 	$types = SMWDataValueFactory::getKnownTypeLabels();
 	asort($types);
-	
+
 	// Ignore all special properties
 	$sp = $smwgHaloContLang->getSpecialPropertyLabels();
 	$types = array_diff($types, $sp);
@@ -192,12 +193,12 @@ function smwf_tb_GetUserDatatypes(){
 	$NStype = SMW_NS_TYPE;
 	$page = $db->tableName( 'page' );
 	$sql = "SELECT 'Types' as type,
-				{$NStype} as namespace,
-				page_title as title,
-				page_title as value,
-				1 as count
-				FROM $page
-				WHERE page_namespace = $NStype";
+	{$NStype} as namespace,
+	page_title as title,
+	page_title as value,
+	1 as count
+	FROM $page
+	WHERE page_namespace = $NStype";
 
 	$res = $db->query($sql);
 
@@ -223,34 +224,48 @@ function smwf_tb_GetUserDatatypes(){
 	return $result;
 }
 
-abstract class SMWToolbarStorage {
-	
-	private static $INSTANCE = NULL;
-	
-	public static function getToolbarStorage() {
-	    global $smwgBaseStore;
-	    if (self::$INSTANCE == NULL) {
-	        switch ($smwgBaseStore) {
-	            case (SMW_STORE_TESTING):
-	                self::$INSTANCE = NULL; // not implemented yet
-	                trigger_error('Testing stores not implemented for HALO extension.');
-	            break;
-	            case ('SMWHaloStore2'): default:
-	                self::$INSTANCE = new SMWToolbarStorageSQL2();
-	            break;
-	            case ('SMWHaloStore'): default:
-	                self::$INSTANCE = new SMWToolbarStorageSQL();
-	            break;
-	        }
-	    }
-        return self::$INSTANCE;
+function smwf_tb_getTripleStoreStatus() {
+	global $wgServer,$wgScript,$smwgNamespace, $smwgWebserviceUser, $smwgWebServicePassword;
+	//ini_set("soap.wsdl_cache_enabled", "0");  //set for debugging
+	$client = new SoapClient("$wgServer$wgScript?action=ajax&rs=smwf_ws_getWSDL&rsargs[]=get_manage", array('connection_timeout' => 4, 'login'=>$smwgWebserviceUser, 'password'=>$smwgWebServicePassword));
+	try {
+		global $smwgNamespace;
+		$statusJSON = $client->getTripleStoreStatus($smwgNamespace);
+		
+	} catch(Exception $e) {
+		return "false";
 	}
-	
+	return $statusJSON;
+}
+
+abstract class SMWToolbarStorage {
+
+	private static $INSTANCE = NULL;
+
+	public static function getToolbarStorage() {
+		global $smwgBaseStore;
+		if (self::$INSTANCE == NULL) {
+			switch ($smwgBaseStore) {
+				case (SMW_STORE_TESTING):
+					self::$INSTANCE = NULL; // not implemented yet
+					trigger_error('Testing stores not implemented for HALO extension.');
+					break;
+				case ('SMWHaloStore2'): default:
+					self::$INSTANCE = new SMWToolbarStorageSQL2();
+					break;
+				case ('SMWHaloStore'): default:
+					self::$INSTANCE = new SMWToolbarStorageSQL();
+					break;
+			}
+		}
+		return self::$INSTANCE;
+	}
+
 	/**
 	 * TODO: Write documentation
 	 */
 	public abstract function  getHelppages($namespace, $action);
-	
+
 	public abstract function getQuestions($id);
 }
 
@@ -258,35 +273,35 @@ class SMWToolbarStorageSQL extends SMWToolbarStorage {
 	public function getHelppages($namespace, $action) {
 		$helppages = array();
 		$discourseState = mysql_real_escape_string($namespace) . ":" . mysql_real_escape_string($action);
-	    $dbr =& wfGetDB( DB_SLAVE );
-	    $smw_attributes = $dbr->tableName('smw_attributes');
-	    $res = $dbr->query('SELECT * FROM '.$smw_attributes.' WHERE attribute_title = "DiscourseState" AND value_xsd= "' . $discourseState . '" AND subject_namespace = "' . NS_HELP . '" ORDER BY RAND() LIMIT 5');
-	
-	    while ($row = $dbr->fetchObject( $res )) {
-	        $helppages[] = $row->subject_id;
-	    }
-	    $dbr->freeResult( $res );
+		$dbr =& wfGetDB( DB_SLAVE );
+		$smw_attributes = $dbr->tableName('smw_attributes');
+		$res = $dbr->query('SELECT * FROM '.$smw_attributes.' WHERE attribute_title = "DiscourseState" AND value_xsd= "' . $discourseState . '" AND subject_namespace = "' . NS_HELP . '" ORDER BY RAND() LIMIT 5');
+
+		while ($row = $dbr->fetchObject( $res )) {
+			$helppages[] = $row->subject_id;
+		}
+		$dbr->freeResult( $res );
 		return $helppages;
 	}
-	
+
 	public function getQuestions($id){
 		$dbr =& wfGetDB( DB_SLAVE );
 		$res = $dbr->select( $dbr->tableName('smw_attributes'),
 			'*',
-			array('subject_id =' . $id, 'attribute_title="Question"'));
+		array('subject_id =' . $id, 'attribute_title="Question"'));
 		if ($dbr->numRows($res) > 0){
 			$results = true;
 			$row = $dbr->fetchObject( $res );
 			$title = $row->subject_title;
 			$question = htmlspecialchars($row->value_xsd);
 			$dbr->freeResult( $res );
-			
+				
 			$res = $dbr->select( $dbr->tableName('smw_longstrings'),
 				'*',
-				array('subject_id =' . $id, 'attribute_title="Description"'));
-			
+			array('subject_id =' . $id, 'attribute_title="Description"'));
+				
 			$description = '';
-			
+				
 			if ($dbr->numRows($res) > 0){
 				$row = $dbr->fetchObject( $res );
 				$description = htmlspecialchars($row->value_blob);
@@ -300,68 +315,68 @@ class SMWToolbarStorageSQL extends SMWToolbarStorage {
 }
 
 class SMWToolbarStorageSQL2 extends SMWToolbarStorageSQL {
-    public function getHelppages($namespace, $action) {
-        $helppages = array();
-        $discourseState = mysql_real_escape_string($namespace) . ":" . mysql_real_escape_string($action);
-        $db =& wfGetDB( DB_SLAVE );
-        $smw_ids = $db->tableName('smw_ids');
-        $smw_atts2 = $db->tableName('smw_atts2');     
-        $page = $db->tableName('page');
-        
-        $discourseStateID = $db->selectRow($smw_ids, array('smw_id'), array('smw_title'=>'DiscourseState', 'smw_namespace' => SMW_NS_PROPERTY));
-        if ($discourseStateID != null) {
-        	$discourseStateID = $discourseStateID->smw_id;
-        } else return array();
-        
-        $res = $db->query('SELECT page_id FROM '.$smw_atts2.
+	public function getHelppages($namespace, $action) {
+		$helppages = array();
+		$discourseState = mysql_real_escape_string($namespace) . ":" . mysql_real_escape_string($action);
+		$db =& wfGetDB( DB_SLAVE );
+		$smw_ids = $db->tableName('smw_ids');
+		$smw_atts2 = $db->tableName('smw_atts2');
+		$page = $db->tableName('page');
+
+		$discourseStateID = $db->selectRow($smw_ids, array('smw_id'), array('smw_title'=>'DiscourseState', 'smw_namespace' => SMW_NS_PROPERTY));
+		if ($discourseStateID != null) {
+			$discourseStateID = $discourseStateID->smw_id;
+		} else return array();
+
+		$res = $db->query('SELECT page_id FROM '.$smw_atts2.
         ' JOIN '.$smw_ids.' ON smw_id = s_id '.
         ' JOIN '.$page.' ON page_title = smw_title AND page_namespace = smw_namespace '.
         ' WHERE p_id = '.$discourseStateID. ' AND value_xsd = ' . $db->addQuotes($discourseState) . ' AND page_namespace = ' . NS_HELP . ' ORDER BY RAND() LIMIT 5');
-    
-        while ($row = $db->fetchObject( $res )) {
-            $helppages[] = $row->page_id;
-        }
-        $db->freeResult( $res );
-        return $helppages;
-    }
-   
-    public function getQuestions($id){
-    	$db =& wfGetDB( DB_SLAVE );
-    	
-    	$smw_ids = $db->tableName('smw_ids');
-        $smw_atts2 = $db->tableName('smw_atts2');     
-        $smw_text2 = $db->tableName('smw_text2');
-        $page = $db->tableName('page');
-        
-         $res = $db->query('SELECT i.smw_title AS subject_title, value_xsd FROM '.$smw_ids.' i '.
+
+		while ($row = $db->fetchObject( $res )) {
+			$helppages[] = $row->page_id;
+		}
+		$db->freeResult( $res );
+		return $helppages;
+	}
+	 
+	public function getQuestions($id){
+		$db =& wfGetDB( DB_SLAVE );
+		 
+		$smw_ids = $db->tableName('smw_ids');
+		$smw_atts2 = $db->tableName('smw_atts2');
+		$smw_text2 = $db->tableName('smw_text2');
+		$page = $db->tableName('page');
+
+		$res = $db->query('SELECT i.smw_title AS subject_title, value_xsd FROM '.$smw_ids.' i '.
                             ' JOIN '.$smw_atts2.' ON i.smw_id = s_id '.
                             ' JOIN '.$smw_ids.' i2 ON i2.smw_id = p_id '.
                             ' JOIN '.$page.' p ON p.page_title = i.smw_title AND p.page_namespace = i.smw_namespace'.
                             ' WHERE p.page_id = '.$id.' AND i2.smw_title = "Question" AND i2.smw_namespace = '.SMW_NS_PROPERTY);
-        
-        if ($db->numRows($res) > 0){
-            $results = true;
-            $row = $db->fetchObject( $res );
-            $title = $row->subject_title;
-            $question = htmlspecialchars($row->value_xsd);
-            $db->freeResult( $res );
-            
-            $res = $db->query('SELECT value_blob FROM '.$smw_text2.' JOIN '.$smw_ids.' ON smw_id = p_id '.
+
+		if ($db->numRows($res) > 0){
+			$results = true;
+			$row = $db->fetchObject( $res );
+			$title = $row->subject_title;
+			$question = htmlspecialchars($row->value_xsd);
+			$db->freeResult( $res );
+
+			$res = $db->query('SELECT value_blob FROM '.$smw_text2.' JOIN '.$smw_ids.' ON smw_id = p_id '.
                                ' JOIN '.$page.' ON page_title = smw_title AND page_namespace = smw_namespace'.
                                ' WHERE page_id = '.$id.' AND smw_title = "Description" AND smw_namespace = '.SMW_NS_PROPERTY);
-            
-            $description = '';
-            
-            if ($db->numRows($res) > 0){
-                $row = $db->fetchObject( $res );
-                $description = htmlspecialchars($row->value_blob);
-            }
-            $db->freeResult( $res );
-            return array($title, $question, $description);
-        }
-        $db->freeResult( $res );
-        return 0;
-    }
+
+			$description = '';
+
+			if ($db->numRows($res) > 0){
+				$row = $db->fetchObject( $res );
+				$description = htmlspecialchars($row->value_blob);
+			}
+			$db->freeResult( $res );
+			return array($title, $question, $description);
+		}
+		$db->freeResult( $res );
+		return 0;
+	}
 }
 
 ?>
