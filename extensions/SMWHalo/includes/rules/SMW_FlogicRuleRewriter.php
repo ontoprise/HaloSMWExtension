97f15f71d9833d2f17aaf4f3a48763a6b4777775
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-02-12 06:35:41 +0000

    Changed namespaces for rules
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2770 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/rules/SMW_FlogicRuleRewriter.php
+++ extensions/SMWHalo/includes/rules/SMW_FlogicRuleRewriter.php
@@ -44,7 +44,13 @@ class FlogicRuleRewriter extends RuleRewriter {
 		// replace placeholder for namespace
 		$ruletext = str_replace("{{wiki-name}}", $smwgNamespace, $ruletext);
 		$ruletext = str_replace("|", "", $ruletext);
-		
+
+		$ruletext = str_replace('"xsd:string"', '"http://www.w3.org/2001/XMLSchema#"#string', $ruletext);
+		$ruletext = str_replace('"xsd:float"', '"http://www.w3.org/2001/XMLSchema#"#float', $ruletext);
+		$ruletext = str_replace('"xsd:boolean"', '"http://www.w3.org/2001/XMLSchema#"#boolean', $ruletext);
+		$ruletext = str_replace('"xsd:dateTime"', '"http://www.w3.org/2001/XMLSchema#"#string', $ruletext);
+		$ruletext = str_replace('"xsd:unit"', '"http://www.w3.org/2001/XMLSchema#"#string', $ruletext);
+
 		return $ruletext;
 	}
     
