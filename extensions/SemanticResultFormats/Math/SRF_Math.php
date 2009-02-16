0bdc18c4d574baf9bb1320a18c19195cc33bc6af
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-02-16 09:47:03 +0000

    semantic results formats: initial commit
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@2792 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/SemanticResultFormats/Math/SRF_Math.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Various mathematical functions - sum, average, min and max.
+ *
+ * @file
+ * @ingroup SemanticResultFormats
+ * @author Yaron Koren
+ * @author Nathan Yergler
+ */
+
+if (!defined('MEDIAWIKI')) die();
+
+class SRFMath extends SMWResultPrinter {
+
+	public function getResult($results, $params, $outputmode) {
+		$this->readParameters($params, $outputmode);
+		return $this->getResultText($results, SMW_OUTPUT_HTML);
+	}
+
+	protected function getResultText($res, $outputmode) {
+		global $smwgIQRunningNumber, $wgUser;
+		$skin = $wgUser->getSkin();
+
+		// initialize all necessary variables
+		$sum = 0;
+		$count = 0;
+		$min = '';
+		$max = '';
+
+		while ( $row = $res->getNext() ) {
+			$last_col = array_pop($row);
+			foreach ( $last_col->getContent() as $value ) {
+				// handle each value only if it's of type Number or NAry
+				if ( $value instanceof SMWNumberValue ) {
+					$num = $value->getNumericValue();
+				} elseif ( $value instanceof SMWNAryValue ) {
+					$inner_values = $value->getDVs();
+					// find the first inner value that's of
+					// type Number, and use that; if none
+					// are found, ignore this row
+					$num = null;
+					foreach ( $inner_values as $inner_value ) {
+						if ( $inner_value instanceof SMWNumberValue ) {
+							$num = $inner_value->getNumericValue();
+							break;
+						}
+					}
+					if ( is_null( $num ) )
+						continue;
+				} else {
+					continue;
+				}
+				$count++;
+				if ($this->mFormat == 'sum' || $this->mFormat == 'average') {
+					$sum += $num;
+				} elseif ($this->mFormat == 'min') {
+					if ($min == '' || $num < $min)
+						$min = $num;
+				} elseif ($this->mFormat == 'max') {
+					if ($max == '' || $num > $max)
+						$max = $num;
+				}
+			}
+		}
+		// if there were no results, display a blank
+		if ($count == 0) {
+			$result = '';
+		} elseif ($this->mFormat == 'sum') {
+			$result = $sum;
+		} elseif ($this->mFormat == 'average') {
+			$result = $sum / $count;
+		} elseif ($this->mFormat == 'min') {
+			$result = $min;
+		} elseif ($this->mFormat == 'max') {
+			$result = $max;
+		} else {
+			$result = '';
+		}
+
+		return $result;
+	}
+
+}
---
+++ extensions/SemanticResultFormats/SRF_Settings.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Initializing file for the Semantic Result Formats extension.
+ *
+ * @file
+ * @ingroup SemanticResultFormats
+ */
+if( !defined( 'MEDIAWIKI' ) ) {
+	die( 'Not an entry point.' );
+}
+
+define('SRF_VERSION', '1.4.2');
+
+$srfgScriptPath = $wgScriptPath . '/extensions/SemanticResultFormats';
+$srfgIP = $IP . '/extensions/SemanticResultFormats';
+$wgExtensionMessagesFiles['SemanticResultFormats'] = $srfgIP . '/SRF_Messages.php';
+$wgExtensionFunctions[] = 'srffSetup';
+
+$srfgFormats = array('icalendar', 'vcard', 'calendar', 'eventline', 'timeline', 'sum', 'average', 'min', 'max');
+
+function srffSetup() {
+	global $srfgFormats, $wgExtensionCredits;
+
+	foreach($srfgFormats as $fn) srffInitFormat($fn);
+	$formats_list = implode(', ', $srfgFormats);
+	$wgExtensionCredits['other'][]= array(
+		'name' => 'Semantic Result Formats',
+		'version' => SRF_VERSION,
+		'author' => "Frank Dengler, Fabian Howahl, Joel Natividad, Yaron Koren, [http://korrekt.org Markus Krötzsch], [http://simia.net Denny&nbsp;Vrandecic], Nathan Yergler",
+		'url' => 'http://semantic-mediawiki.org/wiki/Help:Semantic_Result_Formats',
+		'description' => 'Additional formats for Semantic MediaWiki inline queries. Available formats: ' . $formats_list
+	);
+}
+
+function srffInitFormat( $format ) {
+	global $smwgResultFormats, $wgAutoloadClasses, $srfgIP;
+
+	$class = '';
+	$file = '';
+	switch ($format) {
+		case 'timeline': case 'eventline':
+			$class = 'SRFTimeline';
+			$file = $srfgIP . '/Timeline/SRF_Timeline.php';
+		break;
+		case 'vcard':
+			$class = 'SRFvCard';
+			$file = $srfgIP . '/vCard/SRF_vCard.php';
+		break;
+		case 'icalendar':
+			$class = 'SRFiCalendar';
+			$file = $srfgIP . '/iCalendar/SRF_iCalendar.php';
+		break;
+		case 'calendar':
+			$class = 'SRFCalendar';
+			$file = $srfgIP . '/Calendar/SRF_Calendar.php';
+		breaK;
+		case  'sum': case 'average': case 'min': case 'max':
+			$class = 'SRFMath';
+			$file = $srfgIP . '/Math/SRF_Math.php';
+		break;
+		case 'exhibit':
+			$class = 'SRFExhibit';
+			$file = $srfgIP . '/Exhibit/SRF_Exhibit.php';
+		break;
+		case 'googlebar':
+			$class = 'SRFGoogleBar';
+			$file = $srfgIP . '/GoogleCharts/SRF_GoogleBar.php';
+		break;
+		case 'googlepie':
+			$class = 'SRFGooglePie';
+			$file = $srfgIP . '/GoogleCharts/SRF_GooglePie.php';
+		break;
+		case 'ploticus':
+			$class = 'SRFPloticus';
+			$file = $srfgIP . '/Ploticus/SRF_Ploticus.php';
+		break;
+		case 'graph':
+			$class = 'SRFGraph';
+			$file = $srfgIP . '/GraphViz/SRF_Graph.php';
+		break;
+	}
+	if (($class) && ($file)) {
+		$smwgResultFormats[$format] = $class;
+		$wgAutoloadClasses[$class] = $file;
+	}
+}
+
