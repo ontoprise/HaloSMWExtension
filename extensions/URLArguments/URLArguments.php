913ede7b72142e768f4e43f43977b5d4eb2610bc
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-03-11 09:17:24 +0000

    Initial commit
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3129 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/URLArguments/URLArguments.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ URLArguments.php
+ Defines a new parser function:
+
+ {{#arg:name}} Returns the value of the given URL argument.  Can also be called
+               with a default value, which is returned if the given argument is
+               undefined or blank: {{#arg:name|default}}
+ 
+ Author: Thomas Schweitzer
+         based on: Algorithm [http://meta.wikimedia.org/wiki/User:Algorithm]
+ Version 1.0 (10/3/09)
+*/
+ 
+$wgExtensionFunctions[] = 'wfURLArguments';
+$wgExtensionCredits['parserhook'][] = array(
+	'name' => 'URL Arguments',
+	'version' => '1.0',
+	'url' => '',
+	'author' => 'Thomas Schweitzer',   
+	'description' => 'Defines a new parser function that retrieves arguments from the URL.'
+);
+ 
+$wgHooks['LanguageGetMagic'][] = 'wfURLArgumentsLanguageGetMagic';
+ 
+function wfURLArguments() {
+	global $wgParser, $wgExtURLArguments;
+ 
+	$wgExtURLArguments = new ExtURLArguments();
+ 
+	$wgParser->setFunctionHook( 'arg', array( &$wgExtURLArguments, 'arg' ) );
+}
+ 
+function wfURLArgumentsLanguageGetMagic( &$magicWords, $langCode ) {
+	switch ( $langCode ) {
+	default:
+		$magicWords['arg']    = array( 0, 'arg' );
+	}
+	return true;
+}
+ 
+class ExtURLArguments {
+ 
+	function arg( &$parser, $name = '', $default = '' ) {
+		global $wgRequest;
+		$parser->disableCache();
+		return $wgRequest->getVal($name, $default);
+	}
+}
