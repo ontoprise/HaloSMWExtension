6651602939006d87f7d142053401e842d4f06c51
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-03-13 13:01:01 +0000

    Jaro(-Winkler) metric implementations
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3185 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/libs/mysqlUDFs/src/halowiki_udf.cpp
+++ extensions/SMWHalo/libs/mysqlUDFs/src/halowiki_udf.cpp
@@ -1,5 +1,6 @@
-/*  Copyright 2007, ontoprise GmbH
+/*  Copyright 2009, ontoprise GmbH
 *   Author: Kai Kühn
+
 *   This file is part of the halo-Extension.
 *
 *   The halo-Extension is free software; you can redistribute it and/or modify
@@ -16,7 +17,7 @@
 *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *
 * 	
-*	How to use Levensthein edit distance function as MySQL UDF (user defined function):
+*	How to use for example the Levensthein edit distance function as MySQL UDF (user defined function):
 * 
 * 	I. Copy halowiki.dll where the system can find it (e.g. /mysql/bin)
 * 	and enter in the MySQL admin console:
@@ -48,7 +49,7 @@ typedef long long longlong;
 #include <mysql.h>
 #include <m_ctype.h>
 #include <m_string.h>		// To get strmov()
-
+#include "algorithms.h"
 static pthread_mutex_t LOCK_hostname;
 
 #ifdef HAVE_DLOPEN
@@ -65,7 +66,16 @@ my_bool editdistance_dl_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
 longlong editdistance_dl(UDF_INIT *initid, UDF_ARGS *args,
   		 char *is_null, char *error);
 
+my_bool jarowinkler_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+double jarowinkler(UDF_INIT *initid, UDF_ARGS *args,
+  		 char *is_null, char *error);
+
+my_bool jaro_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+double jaro(UDF_INIT *initid, UDF_ARGS *args,
+  		 char *is_null, char *error);
 }
+
+
 
 
 /***************************************************************************
@@ -123,105 +133,45 @@ my_bool editdistance_dl_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
   
   return 0;
 }
-/*
-* Helper function: Calc minimum of two values.
-*/
-unsigned long minimum(unsigned long m1, unsigned long m2) {
-	return m1 < m2 ? m1 : m2;
+
+my_bool jarowinkler_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
+{
+  uint i;
+
+  if (!args->arg_count)
+  {
+    strcpy(message,"jarowinkler must have exactly two arguments");
+    return 1;
+  }
+  /*
+  ** As this function wants to have everything as strings, force all arguments
+  ** to strings.
+  */
+  for (i=0 ; i < args->arg_count; i++)
+    args->arg_type[i]=STRING_RESULT;
+  
+  return 0;
 }
 
-/*
-* Calculates Levenshtein distance
-* s: term1
-* t: term2
-* ls: length of term1
-* lt: length of term2
-*/
-longlong levenshteinDistance(char *s, char *t, unsigned long ls, unsigned long lt) {
-   unsigned long **d = new unsigned long*[ls+1];
-   unsigned long i, j, cost;
+my_bool jaro_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
+{
+  uint i;
 
-   // initialize
-   for (i=0; i < ls+1;i++) {
-	   d[i] = new unsigned long[lt+1];
-       d[i][0] = i;
-   }
-   for (j=0; j < lt+1;j++) {
-       d[0][j] = j;
-   }
-
-   // calc LS-Distance
-   for (i=1; i < ls+1;i++) {
-       for (j=1; j < lt+1;j++) {
-          if (s[i-1] == t[j-1]) { 
-          	cost = 0; 
-          }
-          else { 
-          	cost = 1; 
-          }
-          d[i][j] = minimum(minimum(d[i-1][j] + 1, 
-                             d[i][j-1] + 1),
-                             d[i-1][j-1] + cost);
-	   }
-   }
-   unsigned long lst_distance = d[ls][lt];
-   
-   // de-allocate memory
-   for (i=0; i < ls+1;i++) {
-	   delete[] d[i];
-   }
-   delete[] d;
-
-   return lst_distance;
+  if (!args->arg_count)
+  {
+    strcpy(message,"jarowinkler must have exactly two arguments");
+    return 1;
+  }
+  /*
+  ** As this function wants to have everything as strings, force all arguments
+  ** to strings.
+  */
+  for (i=0 ; i < args->arg_count; i++)
+    args->arg_type[i]=STRING_RESULT;
+  
+  return 0;
 }
 
-/*
-* Calculates Damerau-Levenshtein distance
-* s: term1
-* t: term2
-* ls: length of term1
-* lt: length of term2
-*/
-longlong damerauLevenshteinDistance(char *s, char *t, unsigned long ls, unsigned long lt) {
-   unsigned long **d = new unsigned long*[ls+1];
-   unsigned long i, j, cost;
-
-   // initialize
-   for (i=0; i < ls+1;i++) {
-	   d[i] = new unsigned long[lt+1];
-       d[i][0] = i;
-   }
-   for (j=0; j < lt+1;j++) {
-       d[0][j] = j;
-   }
-
-   // calc LS-Distance
-   for (i=1; i < ls+1;i++) {
-       for (j=1; j < lt+1;j++) {
-          if (s[i-1] == t[j-1]) { 
-          	cost = 0; 
-          }
-          else { 
-          	cost = 1; 
-          }
-          d[i][j] = minimum(minimum(d[i-1][j] + 1, 
-                             d[i][j-1] + 1),
-                             d[i-1][j-1] + cost);
-		  if ((i > 1) && (j > 1) && (s[i - 1] == t[j - 2]) && (s[i - 2] == t[j - 1])) {
-			d[i][j] = minimum(d[i][j], d[i - 2][j - 2] + cost);
-          }
-	   }
-   }
-   unsigned long lst_distance = d[ls][lt];
-   
-   // de-allocate memory
-   for (i=0; i < ls+1;i++) {
-	   delete[] d[i];
-   }
-   delete[] d;
-
-   return lst_distance;
-}
 
 /*
 * main function called from MySQL
@@ -259,6 +209,41 @@ longlong editdistance_dl(UDF_INIT *initid __attribute__((unused)), UDF_ARGS *arg
 }
 
 
+
+
+double jarowinkler(UDF_INIT *initid __attribute__((unused)), UDF_ARGS *args,
+                     char *is_null, char *error __attribute__((unused)))
+{
+ 
+  uint i;
+
+  for (i = 0; i < args->arg_count; i++) {
+	  if (args->args[i] == NULL) {
+		// NULL values are not allowed here, if so indicate via is_null
+		*is_null = 1;
+		return 0; 
+	  }
+  }
+ 
+  return jarowinkler_distance(args->args[0], args->args[1], args->lengths[0], args->lengths[1]);
+}
+
+double jaro(UDF_INIT *initid __attribute__((unused)), UDF_ARGS *args,
+                     char *is_null, char *error __attribute__((unused)))
+{
+ 
+  uint i;
+
+  for (i = 0; i < args->arg_count; i++) {
+	  if (args->args[i] == NULL) {
+		// NULL values are not allowed here, if so indicate via is_null
+		*is_null = 1;
+		return 0; 
+	  }
+  }
+ 
+  return jaro_distance(args->args[0], args->args[1], args->lengths[0], args->lengths[1]);
+}
 
 
 
