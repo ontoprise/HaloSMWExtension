c9e802df9f7ec4296eb04c8820475b6b71cb3dcb
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-04-03 08:35:57 +0000

    all deployables get copied to a temp directory
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3338 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/makeAll.bat
+++ extensions/makeAll.bat
@@ -1,28 +1,48 @@
 REM Builds all extensions
 
 SET CURRENT=%CD%
+SET OUT=c:\temp\alldeploys
+
+IF NOT EXIST %OUT% goto CREATEDIRS
+
+del /S /Q %OUT%\*
+rmdir /S /Q %OUT%
+
+REM Create directories
+
+:CREATEDIRS
+mkdir %OUT%
+
+cd SemanticGardening\deploy
+CALL make
+xcopy bin\*.zip %OUT%
+cd %CURRENT%
 
 cd DataImport\deploy
 CALL make
+xcopy bin\*.zip %OUT%
 cd %CURRENT%
 
 cd RichMedia\deploy
 CALL make
-cd %CURRENT%
-
-cd SemanticGardening\deploy
-CALL make
+xcopy bin\*.zip %OUT%
 cd %CURRENT%
 
 cd SemanticNotifications\deploy
 CALL make
+xcopy bin\*.zip %OUT%
 cd %CURRENT%
 
 cd SMWHalo\deploy
 CALL makeSMWHalo
 CALL makeExt
+xcopy bin\*.zip %OUT%
 cd %CURRENT%
 
 cd UnifiedSearch\deploy
 CALL make gpl
+xcopy bin\*.zip %OUT%
 cd %CURRENT%
+
+echo All deployable zips are located in %OUT%
+
