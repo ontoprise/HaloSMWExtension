a215e9549efe3c7ddf0a2793dd2cf80fa94968ea
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-04-24 14:02:25 +0000

    add includes
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3561 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests/HaloTests.php
+++ extensions/SMWHalo/tests/HaloTests.php
@@ -2,6 +2,10 @@
 require_once 'PHPUnit/Framework.php';
 
 require_once 'testcases/TestSemanticStore.php';
+require_once 'testcases/TestAutocompletionStore.php';
+require_once 'testcases/TestRuleRewriter.php';
+require_once 'testcases/TestWikiJobs.php';
+require_once 'testcases/TestQueryPrinters.php';
 
 
 class HaloTests
