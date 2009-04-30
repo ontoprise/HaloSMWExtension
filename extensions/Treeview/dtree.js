6ba119e1c2443873f12afa0ed1ce9eb2c7fa013d
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-04-30 16:19:08 +0000

    have histogram excluded from function URLEncode and URLDecode to have the list once only.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3615 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/Treeview/dtree.js
+++ extensions/Treeview/dtree.js
@@ -1112,7 +1112,7 @@ function URLEncode( str ) {
     // version: 904.1412
     // discuss at: http://phpjs.org/functions/urlencode
 			      
-    var histogram = {}, tmp_arr = [];
+    var tmp_arr = [];
     var ret = (str+'').toString();
      
     var replacer = function(search, replace, str) {
@@ -1122,52 +1122,7 @@ function URLEncode( str ) {
     };
      
     // The histogram is identical to the one in urldecode.
-    histogram["'"]   = '%27';
-    histogram['(']   = '%28';
-    histogram[')']   = '%29';
-    histogram['*']   = '%2A';
-    histogram['~']   = '%7E';
-    histogram['!']   = '%21';
-    histogram['%20'] = '+';
-    histogram['\u00DC'] = '%DC';
-    histogram['\u00FC'] = '%FC';
-    histogram['\u00C4'] = '%D4';
-    histogram['\u00E4'] = '%E4';
-    histogram['\u00D6'] = '%D6';
-    histogram['\u00F6'] = '%F6';
-    histogram['\u00DF'] = '%DF';
-    histogram['\u20AC'] = '%80';
-    histogram['\u0081'] = '%81';
-    histogram['\u201A'] = '%82';
-    histogram['\u0192'] = '%83';
-    histogram['\u201E'] = '%84';
-    histogram['\u2026'] = '%85';
-    histogram['\u2020'] = '%86';
-    histogram['\u2021'] = '%87';
-    histogram['\u02C6'] = '%88';
-    histogram['\u2030'] = '%89';
-    histogram['\u0160'] = '%8A';
-    histogram['\u2039'] = '%8B';
-    histogram['\u0152'] = '%8C';
-    histogram['\u008D'] = '%8D';
-    histogram['\u017D'] = '%8E';
-    histogram['\u008F'] = '%8F';
-    histogram['\u0090'] = '%90';
-    histogram['\u2018'] = '%91';
-    histogram['\u2019'] = '%92';
-    histogram['\u201C'] = '%93';
-    histogram['\u201D'] = '%94';
-    histogram['\u2022'] = '%95';
-    histogram['\u2013'] = '%96';
-    histogram['\u2014'] = '%97';
-    histogram['\u02DC'] = '%98';
-    histogram['\u2122'] = '%99';
-    histogram['\u0161'] = '%9A';
-    histogram['\u203A'] = '%9B';
-    histogram['\u0153'] = '%9C';
-    histogram['\u009D'] = '%9D';
-    histogram['\u017E'] = '%9E';
-    histogram['\u0178'] = '%9F';
+    var histogram = URL_Histogram();
      
     // Begin with encodeURIComponent, which most resembles PHP's encoding functions
     ret = encodeURIComponent(ret);
@@ -1188,7 +1143,7 @@ function URLEncode( str ) {
 function URLDecode( str ) {
     // version: 904.1412
     // discuss at: http://phpjs.org/functions/urldecode
-    var histogram = {};
+
     var ret = str.toString();
      
     var replacer = function(search, replace, str) {
@@ -1198,6 +1153,22 @@ function URLDecode( str ) {
     };
      
     // The histogram is identical to the one in urlencode.
+    var histogram = URL_Histogram();
+    
+    for (replace in histogram) {
+	search = histogram[replace]; // Switch order when decoding
+	ret = replacer(search, replace, ret) // Custom replace. No regexing  
+    }
+     
+    // End with decodeURIComponent, which most resembles PHP's encoding functions
+    ret = decodeURIComponent(ret);
+ 
+    return ret;
+}
+
+function URL_Histogram() {
+	var histogram = {};
+	
     histogram["'"]   = '%27';
     histogram['(']   = '%28';
     histogram[')']   = '%29';
@@ -1244,14 +1215,5 @@ function URLDecode( str ) {
     histogram['\u009D'] = '%9D';
     histogram['\u017E'] = '%9E';
     histogram['\u0178'] = '%9F';
- 
-    for (replace in histogram) {
-	search = histogram[replace]; // Switch order when decoding
-	ret = replacer(search, replace, ret) // Custom replace. No regexing  
-    }
-     
-    // End with decodeURIComponent, which most resembles PHP's encoding functions
-    ret = decodeURIComponent(ret);
- 
-    return ret;
+ 	return histogram;
 }
