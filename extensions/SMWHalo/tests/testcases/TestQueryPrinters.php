ed89256f3cc8bba217b67a33f1aaa2452539dea1
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-05-18 15:34:50 +0000

    added query printer tests
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3737 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests/testcases/TestQueryPrinters.php
+++ extensions/SMWHalo/tests/testcases/TestQueryPrinters.php
@@ -2,12 +2,50 @@
 class TestQueryPrinters extends PHPUnit_Framework_TestCase {
 
 
-    function setUp() {
+	function setUp() {
 
-    }
+	}
 
-    function tearDown() {
-            
-    }
+	function tearDown() {
+
+	}
+
+	function testXMLQueryPrinter() {
+		global $smwgResultFormats, $smwgHaloIP;
+		require_once "$smwgHaloIP/includes/SMW_QP_XML.php";
+		$smwgResultFormats['xml'] = 'SMWXMLResultPrinter';
+		$params = array();
+		$context = SMWQueryProcessor::INLINE_QUERY;
+		$format = "xml";
+		$extraprintouts = array();
+		$querystring = "[[Category:Car]]";
+		$query  = SMWQueryProcessor::createQuery($querystring, $params, $context, $format, $extraprintouts);
+		$res = smwfGetStore()->getQueryResult($query);
+		$result = SMWQueryProcessor::getResultFromQuery($query, $params, $extraprintouts, SMW_OUTPUT_FILE, $context, $format);
+		$this->assertFileContentsIgnoringWhitespaces("testcases/resources/xml_qp_result.dat", $result);
+		
+	}
+
+	function testExcelQueryPrinter() {
+	
+		$params = array();
+		$context = SMWQueryProcessor::INLINE_QUERY;
+		$format = "exceltable";
+		$extraprintouts = array();
+		$querystring = "[[Category:Car]]";
+		$query  = SMWQueryProcessor::createQuery($querystring, $params, $context, $format, $extraprintouts);
+		$res = smwfGetStore()->getQueryResult($query);
+		$result = SMWQueryProcessor::getResultFromQuery($query, $params, $extraprintouts, SMW_OUTPUT_FILE, $context, $format);
+		
+		$this->assertFileContentsIgnoringWhitespaces("testcases/resources/excel_qp_result.dat", $result);
+		
+	}
+	
+	function assertFileContentsIgnoringWhitespaces($file, $act_result) {
+		$contents = file_get_contents($file);
+		$contents = preg_replace("/\\s+|\\n+|\\r+/", "", $contents);
+		$act_result = preg_replace("/\\s+|\\n+|\\r+/", "", $act_result);
+		$this->assertEquals($contents, $act_result);
+	}
 }
 ?>
