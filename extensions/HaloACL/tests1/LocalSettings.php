3f048b351175f0f8aa20e253cd6d7a2cfa4f1936
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-02 09:54:18 +0000

    Added webtests
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3827 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/HaloACL/tests1/LocalSettings.php
@@ -0,0 +1,10 @@
+#Import SMW, SMWHalo and the Gardening extension
+include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
+enableSemantics('http://wiki', true);
+ 
+include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
+enableSMWHalo('SMWHaloStore2');
+
+include_once('extensions/HaloACL/includes/HACL_Initialize.php');
+enableHaloACL(); 
+
