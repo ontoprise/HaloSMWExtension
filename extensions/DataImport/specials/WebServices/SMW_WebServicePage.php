d5d3e24f0e48d9652c1d0b949afabc0b0a18d89a
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-02 07:57:57 +0000

    improved web service page layout
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3812 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/DataImport/specials/WebServices/SMW_WebServicePage.php
+++ extensions/DataImport/specials/WebServices/SMW_WebServicePage.php
@@ -1,30 +1,30 @@
 <?php
 /*  Copyright 2008, ontoprise GmbH
-*  This file is part of the Data Import-Extension.
-*
-*   The Data Import-Extension is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   The Data Import-Extension is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *  This file is part of the Data Import-Extension.
+ *
+ *   The Data Import-Extension is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Data Import-Extension is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * Pages in the namespace WebService are enhanced with additional lists:
  *  - An alphabetical list of all articles that use the web service.
- *  - An alphabetical list of all semantic properties that get their value from 
+ *  - An alphabetical list of all semantic properties that get their value from
  *    the web service.
- *  - Services that have the update policy once show a Refresh button that 
- *    starts an update bot when the user clicks it. The time of the last update 
+ *  - Services that have the update policy once show a Refresh button that
+ *    starts an update bot when the user clicks it. The time of the last update
  *    is shown as well.
- *  
+ *
  * Some code based is on CategoryPage.php and SMW_PropertyPage.php
  *
  * @author: Thomas Schweitzer
@@ -39,24 +39,24 @@ require_once("$smwgDIIP/specials/WebServices/SMW_WSStorage.php");
 
 /**
  * Implementation of MediaWiki's Article that shows additional information on
- * WebService pages. 
+ * WebService pages.
  */
 class SMWWebServicePage extends SMWOrderedListPage {
-	
+
 	private $mArticles = array();
 	private $mProperties = array();
 	private $mFromArticle;
 	private $mUntilArticle;
-	
+
 	/**
 	 * Initialize the limits
 	 */
 	protected function initParameters() {
 		global $smwgWWSArticleLimit, $wgRequest;
 		$this->limit = $smwgWWSArticleLimit;
-		
+
 		$this->mFromArticle = $wgRequest->getVal( 'fromarticle' );
-		$this->mUntilArticle = $wgRequest->getVal( 'untilarticle' );		
+		$this->mUntilArticle = $wgRequest->getVal( 'untilarticle' );
 		return true;
 	}
 
@@ -66,7 +66,7 @@ class SMWWebServicePage extends SMWOrderedListPage {
 	 */
 	protected function doQuery() {
 		global $wgContLang;
-		
+
 		// ask for the list of articles that use the web service
 		$options = new SMWRequestOptions();
 		$options->limit = $this->limit + 1;
@@ -84,20 +84,20 @@ class SMWWebServicePage extends SMWOrderedListPage {
 			$reverse = true;
 		}
 		$articleIDs = WSStorage::getDatabase()
-		                ->getWSArticles($this->getTitle()->getArticleID(), $options);
+		->getWSArticles($this->getTitle()->getArticleID(), $options);
 
 		foreach($articleIDs as $articleId){
 			$this->mArticles[] = Title::newFromID($articleId);
 		}
-		
+
 		if ($reverse) {
 			$this->mArticles = array_reverse($this->mArticles);
 		}
-		
+
 		foreach ($this->mArticles as $title) {
 			$this->articles_start_char[] = $wgContLang->convert( $wgContLang->firstChar( $title->getText() ) );
 		}
-		
+
 	}
 
 	/**
@@ -107,31 +107,32 @@ class SMWWebServicePage extends SMWOrderedListPage {
 	protected function getPages() {
 		wfProfileIn( __METHOD__ . ' (SMW)');
 		$r = '';
-		$ti = htmlspecialchars( $this->mTitle->getText() );
-
-		// list articles
-		$nav = $this->getNavigationLinks('WWSArticleResults', $this->mArticles,
-		                                 $this->mFromArticle, $this->mUntilArticle,
-		                                 'fromarticle', 'untilarticle');
-		                                 $r .= '<a name="WWSArticleResults"></a>' . "<div id=\"mw-pages\">\n";
-		$r .= '<h2>' . wfMsg('smw_wws_articles_header',$ti) . "</h2>\n";
-		$r .= wfMsg('smw_wws_articlecount', min($this->limit, count($this->mArticles))) . "\n";
-		$r .= $nav;
-		$r .= $this->shortList( $this->mArticles, $this->articles_start_char, $this->mUntilArticle) . "\n</div>";
-		$r .= $nav;
-		
 
 		global $wgArticlePath;
 		$ws = WebService::newFromID($this->getTitle()->getArticleID());
 		if($ws != null){
 			if(strpos($wgArticlePath, "?") > 0){
-				$url = Title::makeTitleSafe(NS_SPECIAL, "DefineWebService")->getFullURL()."&wwsdId=".$this->getTitle()->getArticleID();		
+				$url = Title::makeTitleSafe(NS_SPECIAL, "DefineWebService")->getFullURL()."&wwsdId=".$this->getTitle()->getArticleID();
 			} else {
 				$url = Title::makeTitleSafe(NS_SPECIAL, "DefineWebService")->getFullURL()."?wwsdId=".$this->getTitle()->getArticleID();
 			}
-			$r .= '<a href="'.$url.'"><br/><h2><b>'.wfMsg('smw_wws_edit_in_gui').'</h2></p></a>';
+			$r .= '<h2><a href="'.$url.'">'.wfMsg('smw_wws_edit_in_gui').'</a></h2>';
 		}
-		
+
+		$ti = htmlspecialchars( $this->mTitle->getText() );
+
+		// list articles
+		$nav = $this->getNavigationLinks('WWSArticleResults', $this->mArticles,
+		$this->mFromArticle, $this->mUntilArticle,
+		                                 'fromarticle', 'untilarticle');
+		$r .= '<a name="WWSArticleResults"></a>' . "<div id=\"mw-pages\">\n";
+		$r .= '<h2>' . wfMsg('smw_wws_articles_header',$ti) . "</h2>\n";
+		$r .= wfMsg('smw_wws_articlecount', min($this->limit, count($this->mArticles))) . "\n";
+		$r .= $nav;
+		$r .= $this->shortList( $this->mArticles, $this->articles_start_char, $this->mUntilArticle) . "\n</div>";
+		$r .= $nav;
+
+
 		wfProfileOut( __METHOD__ . ' (SMW)');
 		return $r;
 	}
@@ -141,11 +142,11 @@ class SMWWebServicePage extends SMWOrderedListPage {
 	 * Generates the prev/next link part to the HTML code of the top and bottom section of the page.
 	 */
 	protected function getNavigationLinks($fragment, &$articles, $from, $until,
-	                                      $fromLabel, $untilLabel) {
+	$fromLabel, $untilLabel) {
 		global $wgUser, $wgLang;
 		$sk =& $this->getSkin();
 		$limitText = $wgLang->formatNum( $this->limit );
-		
+
 		$ac = count($articles);
 		if ($until != '') {
 			if ($ac > $this->limit) { // (we assume that limit is at least 1)
@@ -169,16 +170,16 @@ class SMWWebServicePage extends SMWOrderedListPage {
 		$this->mTitle->setFragment("#$fragment"); // make navigation point to the result list
 		if( $first != '' ) {
 			$prevLink = $sk->makeLinkObj( $this->mTitle, $prevLink,
-				wfArrayToCGI( array( $untilLabel => $first ) ) );
+			wfArrayToCGI( array( $untilLabel => $first ) ) );
 		}
 		$nextLink = htmlspecialchars( wfMsg( 'nextn', $limitText ) );
 		if( $last != '' ) {
 			$nextLink = $sk->makeLinkObj( $this->mTitle, $nextLink,
-				wfArrayToCGI( array( $fromLabel => $last ) ) );
+			wfArrayToCGI( array( $fromLabel => $last ) ) );
 		}
 		return "($prevLink) ($nextLink)";
 	}
-	
+
 	/**
 	 * Format a list of articles chunked by letter in a table that shows subject articles in
 	 * one column and object articles/values in the other one.
@@ -205,15 +206,15 @@ class SMWWebServicePage extends SMWOrderedListPage {
 			include_once($smwgIP . '/includes/SMW_Infolink.php');
 			// Header for index letters
 			if ($startChar[$index] != $prevchar) {
-				$r .= '<tr><th class="smwpropname"><h3>' . 
-				      htmlspecialchars( $startChar[$index] ) . 
+				$r .= '<tr><th class="smwpropname"><h3>' .
+				htmlspecialchars( $startChar[$index] ) .
 				      "</h3></th><th></th></tr>\n";
 				$prevchar = $startChar[$index];
 			}
-			
+				
 			$searchlink = SMWInfolink::newBrowsingLink('+',$articles[$index]->getPrefixedText());
-			$r .= '<tr><td class="smwpropname">' . $this->getSkin()->makeKnownLinkObj( $articles[$index], 
-			$wgContLang->convert( $articles[$index]->getPrefixedText() ) ) . 
+			$r .= '<tr><td class="smwpropname">' . $this->getSkin()->makeKnownLinkObj( $articles[$index],
+			$wgContLang->convert( $articles[$index]->getPrefixedText() ) ) .
 			  '&nbsp;' . $searchlink->getHTML($this->getSkin()) .
 			  '</td><td class="smwprops">';
 			$r .= "</td></tr>\n";
