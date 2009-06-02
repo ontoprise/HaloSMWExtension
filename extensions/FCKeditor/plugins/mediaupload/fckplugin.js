c22d46e65f6d63775b3587aec91beca50147600a
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-02 10:52:44 +0000

    included the floatbox
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3830 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/FCKeditor/plugins/mediaupload/fckplugin.js
+++ extensions/FCKeditor/plugins/mediaupload/fckplugin.js
@@ -3,4 +3,15 @@ var tbButton = new FCKToolbarButton( 'MW_MediaUpload', 'UploadMedia', 'Upload me
 tbButton.IconPath = FCKConfig.PluginsPath + 'mediaupload/images/icon_mediaupload.gif' ;
 FCKToolbarItems.RegisterItem( 'MW_MediaUpload', tbButton );
 
-FCKCommands.RegisterCommand( 'MW_MediaUpload', new FCKDialogCommand( 'MW_MediaUpload', 'UploadMedia', FCKConfig.PluginsPath + 'mediaupload/dialogs/uploadmedia.html', 400, 330 ) ) ;
+//FCKCommands.RegisterCommand( 'MW_MediaUpload', new FCKDialogCommand( 'MW_MediaUpload', 'UploadMedia', FCKConfig.PluginsPath + 'mediaupload/dialogs/uploadmedia.html', 400, 330 ) ) ;
+
+var OpenUploadWindowCommand=function(){};
+OpenUploadWindowCommand.prototype.Execute=function(){ }
+OpenUploadWindowCommand.GetState=function() {
+        return FCK_TRISTATE_OFF; //we dont want the button to be toggled
+}
+OpenUploadWindowCommand.Execute=function() {
+	top.fb.loadAnchor('index.php?title=Special:UploadWindow&sfInputID=myWpDestFile&RMUpload[RelatedArticles]=RichMedia11&wpIgnoreWarning=true', 'width:600 height:660', 'Uploading files');
+	// do stuff here if you want
+}
+FCKCommands.RegisterCommand( 'MW_MediaUpload', OpenUploadWindowCommand) ;
