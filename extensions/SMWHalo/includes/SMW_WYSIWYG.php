5d4ae878a577d661c664768687c6bc2b48ae53f3
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-04 11:26:57 +0000

    ww editor activated by default. only plain edit mode does not load ww
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3855 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/SMW_WYSIWYG.php
+++ extensions/SMWHalo/includes/SMW_WYSIWYG.php
@@ -8,10 +8,11 @@ if ( !defined( 'MEDIAWIKI' ) ) die;
 global $wgHooks;
 $wgHooks[ 'SkinTemplateTabs' ][] = 'smwfAddWYSIWYGTab';
 
-
-if ((array_key_exists('mode', $_REQUEST) && $_REQUEST['mode'] == 'wysiwyg') 
- || (array_key_exists('action', $_REQUEST) && array_key_exists('rs', $_REQUEST) && $_REQUEST['action'] == 'ajax' && stripos($_REQUEST['rs'], 'wfSajax') === 0)) {
-   require_once $IP . "/extensions/FCKeditor/FCKeditor.php";
+// do not load WW if and only if in plain edit mode
+$plainEditmode = (array_key_exists('action', $_REQUEST) && $_REQUEST['action'] == 'edit') &&
+!(array_key_exists('mode', $_REQUEST) && $_REQUEST['mode'] == 'wysiwyg');
+if (!$plainEditmode) {
+	require_once $IP . "/extensions/FCKeditor/FCKeditor.php";
 }
 
 /**
@@ -19,19 +20,21 @@ if ((array_key_exists('mode', $_REQUEST) && $_REQUEST['mode'] == 'wysiwyg')
  * the cache and thus refreshes the inline queries.
  */
 function smwfAddWYSIWYGTab($obj, $content_actions) {
-    global $wgUser, $wgTitle;
-        $wwactive = array_key_exists('action', $_REQUEST) && $_REQUEST['action'] == 'edit' && array_key_exists('mode', $_REQUEST) && $_REQUEST['mode'] == 'wysiwyg' ? 'selected' : false;
-        $content_actions['wysiwyg'] = array(
+	global $wgUser, $wgTitle;
+	
+	if (!$wgUser->isAllowed('wysiwyg')) return true;
+	$wwactive = array_key_exists('action', $_REQUEST) && $_REQUEST['action'] == 'edit' && array_key_exists('mode', $_REQUEST) && $_REQUEST['mode'] == 'wysiwyg' ? 'selected' : false;
+	$content_actions['wysiwyg'] = array(
             'class' => $wwactive,
             'text' => wfMsg('smw_wysiwyg'),
             'href' => $wgTitle->getLocalUrl( 'action=edit&mode=wysiwyg' )
-        );
-        
-        // adjust edit tab
-        $editactive = array_key_exists('action', $_REQUEST) && $_REQUEST['action'] == 'edit' && array_key_exists('mode', $_REQUEST) && $_REQUEST['mode'] != 'wysiwyg' ? 'selected' : false;
-        $content_actions['edit']['class'] = $editactive;
-       
-    
-    return true; // always return true, in order not to stop MW's hook processing!
+	);
+
+	// adjust edit tab
+	$editactive = array_key_exists('action', $_REQUEST) && $_REQUEST['action'] == 'edit' && array_key_exists('mode', $_REQUEST) && $_REQUEST['mode'] != 'wysiwyg' ? 'selected' : false;
+	$content_actions['edit']['class'] = $editactive;
+	 
+
+	return true; // always return true, in order not to stop MW's hook processing!
 }
 ?>
