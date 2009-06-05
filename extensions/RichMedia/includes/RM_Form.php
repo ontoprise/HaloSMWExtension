271592c930597690053a5f182e7f4f9a5709cbe4
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-05 12:41:36 +0000

    added function that creates the link to 'Rich Media Upload Window'
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3891 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/RichMedia/includes/RM_Form.php
+++ extensions/RichMedia/includes/RM_Form.php
@@ -1,10 +1,9 @@
 <?php
 class RMForm {
 
-	static function createRichMediaForm(&$parser) {
+	static function createRichMediaForm(&$parameter) {
 		global $wgOut, $wgParser;
 
-		$size = "35";
 		$html =<<<END
 <form onsubmit="fb.loadAnchor($('link_id'));return false;">
 END;
@@ -22,6 +21,42 @@ END;
 		$html .= "<a id=\"link_id\" href=\"$uploadWindowUrl\" title=\"$uploadLabel\" rel=\"iframe\" rev=\"width:600 height:660\"></a><input type=\"submit\" value=\"$buttonText\"/></form>";
 
 		//return array($html, 'noparse' => true, 'isHTML' => true);
+		return $wgParser->insertStripItem( $html, $wgParser->mStripState );
+	}
+	
+static function createRichMediaLink(&$parameter) {
+		global $wgOut, $wgParser, $wgRequest;
+
+		if ( array_key_exists( 0, $parameter ) && isset( $parameter[0] ) )
+			$link_name = $parameter[0];
+		else
+			$link_name = 'Link';
+
+		if ( array_key_exists( 1, $parameter ) && isset( $parameter[1] ) )
+			$link_title = $parameter[1];
+		else
+			$link_title = wfMsgNoTrans('smw_rm_uploadheadline');
+		
+		if ( array_key_exists( 2, $parameter ) && isset( $parameter[2] ) )
+			$rev_width = 'width:' . $parameter[2];
+		else
+			$rev_width = 'width:600';
+
+		if ( array_key_exists( 3, $parameter ) && isset( $parameter[3] ) )
+			$rev_height = 'height:' . $parameter[3];
+		else
+			$rev_height = 'height:660';
+		
+		$rev = $rev_width . ' ' . $rev_height;
+		
+		global $smwgRMFormByNamespace;
+		$smwgRMUploadFormName = $smwgRMFormByNamespace['RMUpload'];
+		$queryString = "wpIgnoreWarning=true";
+		$uploadWindowPage = SpecialPage::getPage('UploadWindow');
+		$uploadWindowUrl = $uploadWindowPage->getTitle()->getFullURL($queryString);
+
+		$html .= "<a href=\"$uploadWindowUrl\" title=\"$link_title\" rel=\"iframe\" rev=\"$rev\">$link_name</a>";
+
 		return $wgParser->insertStripItem( $html, $wgParser->mStripState );
 	}
 }
