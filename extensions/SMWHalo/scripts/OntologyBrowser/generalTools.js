484d1e6f8d71abfc85b848488927712533552816
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-09 16:52:33 +0000

    add URLEncode functionality to general tools.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3956 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/scripts/OntologyBrowser/generalTools.js
+++ extensions/SMWHalo/scripts/OntologyBrowser/generalTools.js
@@ -1,5 +1,5 @@
 /*  Copyright 2007, ontoprise GmbH
-*   Author: Kai Kühn
+*   Author: Kai Kï¿½hn
 *   This file is part of the halo-Extension.
 *
 *   The halo-Extension is free software; you can redistribute it and/or modify
@@ -465,9 +465,89 @@ GeneralTools.matchArrayOfRegExp = function (term, regexArray) {
     return doesMatch;
 }
   
+GeneralTools.URLEncode = function ( str ) {
+    // version: 904.1412
+    // discuss at: http://phpjs.org/functions/urlencode
+			      
+    var tmp_arr = [];
+    var ret = (str+'').toString();
+     
+    var replacer = function(search, replace, str) {
+	var tmp_arr = [];
+	tmp_arr = str.split(search);
+	return tmp_arr.join(replace);
+    };
+     
+    // The histogram is identical to the one in urldecode.
+    var histogram = this._URL_Histogram();
+     
+    // Begin with encodeURIComponent, which most resembles PHP's encoding functions
+    ret = encodeURIComponent(ret);
+     
+    for (search in histogram) {
+	replace = histogram[search];
+	ret = replacer(search, replace, ret) // Custom replace. No regexing
+    }
+     
+    // Uppercase for full PHP compatibility
+    return ret.replace(/(\%([a-z0-9]{2}))/g, function(full, m1, m2) {
+	return "%"+m2.toUpperCase();
+    });
+     
+    return ret;
+}
 
-
-
+GeneralTools._URL_Histogram = function() {
+	var histogram = {};
+	
+    histogram["'"]   = '%27';
+    histogram['(']   = '%28';
+    histogram[')']   = '%29';
+    histogram['*']   = '%2A';
+    histogram['~']   = '%7E';
+    histogram['!']   = '%21';
+    histogram['%20'] = '+';
+    histogram['\u00DC'] = '%DC';
+    histogram['\u00FC'] = '%FC';
+    histogram['\u00C4'] = '%D4';
+    histogram['\u00E4'] = '%E4';
+    histogram['\u00D6'] = '%D6';
+    histogram['\u00F6'] = '%F6';
+    histogram['\u00DF'] = '%DF';
+    histogram['\u20AC'] = '%80';
+    histogram['\u0081'] = '%81';
+    histogram['\u201A'] = '%82';
+    histogram['\u0192'] = '%83';
+    histogram['\u201E'] = '%84';
+    histogram['\u2026'] = '%85';
+    histogram['\u2020'] = '%86';
+    histogram['\u2021'] = '%87';
+    histogram['\u02C6'] = '%88';
+    histogram['\u2030'] = '%89';
+    histogram['\u0160'] = '%8A';
+    histogram['\u2039'] = '%8B';
+    histogram['\u0152'] = '%8C';
+    histogram['\u008D'] = '%8D';
+    histogram['\u017D'] = '%8E';
+    histogram['\u008F'] = '%8F';
+    histogram['\u0090'] = '%90';
+    histogram['\u2018'] = '%91';
+    histogram['\u2019'] = '%92';
+    histogram['\u201C'] = '%93';
+    histogram['\u201D'] = '%94';
+    histogram['\u2022'] = '%95';
+    histogram['\u2013'] = '%96';
+    histogram['\u2014'] = '%97';
+    histogram['\u02DC'] = '%98';
+    histogram['\u2122'] = '%99';
+    histogram['\u0161'] = '%9A';
+    histogram['\u203A'] = '%9B';
+    histogram['\u0153'] = '%9C';
+    histogram['\u009D'] = '%9D';
+    histogram['\u017E'] = '%9E';
+    histogram['\u0178'] = '%9F';
+ 	return histogram;
+}
 
 var OBPendingIndicator = Class.create();
 OBPendingIndicator.prototype = {
