3a146b889be23f3e28e3feb8eaf794ff4b593409
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-10 10:11:02 +0000

    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@3968 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/HaloACL/tests/HaloACLTests.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'PHPUnit/Framework.php';
+ 
+require_once 'testcases/TestDatabase.php';
+require_once 'testcases/TestParserFunctions.php';
+require_once 'testcases/TestUserCanHook.php';
+require_once 'testcases/TestDefaultSecurityDescriptor.php';
+
+class HaloACLTests
+{ 
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('PHPUnit');
+        
+        $suite->addTestSuite("TestDatabase");
+        $suite->addTestSuite("TestParserFunctions");
+        $suite->addTestSuite("TestUserCanHookSuite");
+        $suite->addTestSuite("TestDefaultSecurityDescriptorSuite");
+        
+        return $suite;
+    }
+}
+?>
