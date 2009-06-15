b7d04257e14c124afbcccc89af8275f79fb7372b
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-15 12:52:14 +0000

    works with target=... and the path variant now
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4055 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/FCKeditor/plugins/mediaupload/fckplugin.js
+++ extensions/FCKeditor/plugins/mediaupload/fckplugin.js
@@ -14,9 +14,19 @@ OpenUploadWindowCommand.Execute=function() {
 	var uri = window.parent.wgServer + window.parent.wgScriptPath + "/index.php?title=Special:UploadWindow";
 	var article = window.parent.wgTitle;
 	if (window.parent.wgPageName == 'Special:AddData') {
-	//obviously we are in Special:AddData and wgTitle is containing what we're loooking for... 
-		var article = window.parent.location.pathname.match( /[^\/]+\/?$/ )[0];
-	}	
+		//obviously we are in Special:AddData and wgTitle is containing what we're loooking for...
+		// try target= ...first
+		var regexS = "[\\?&]target=([^&#]*)";
+		var regex = new RegExp( regexS );
+		var result = regex.exec( window.parent.location.href );
+		if (result == null) {
+			//target not found, it has to be it the path now!
+			article = window.parent.location.pathname.match( /[^\/]+\/?$/ )[0];
+		}
+		else {
+			article = result[1];
+		}
+	}
 	top.fb.loadAnchor(uri+'&sfInputID=myWpDestFile&RMUpload[RelatedArticles]='+article+'&wpIgnoreWarning=true', 'width:600 height:660', 'Uploading files');
 	// do stuff here if you want
 }
