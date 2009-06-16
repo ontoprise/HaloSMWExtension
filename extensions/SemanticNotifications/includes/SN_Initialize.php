d0d3bbb07de4ea874442bcaf92904766897ff389
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-16 13:31:00 +0000

    Changed version to 1.1
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4086 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SemanticNotifications/includes/SN_Initialize.php
+++ extensions/SemanticNotifications/includes/SN_Initialize.php
@@ -37,7 +37,7 @@ if ( !defined( 'SGA_GARDENING_EXTENSION_VERSION' ) ) {
 	     "Please read 'extensions/SemanticNotifications/INSTALL' for further information.\n" );
 }
 
-define('SN_SEMANTIC_NOTIFICATIONS_VERSION', '1.4.4');
+define('SN_SEMANTIC_NOTIFICATIONS_VERSION', '1.1');
 
 // constant for special schema properties
 
