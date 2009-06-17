caa01f4e5e8893e7aad5ccdf89ddf60280d80a21
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-17 11:49:26 +0000

    new web tests for webservicerepository and simple webservicepage
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4097 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/DataImport/specials/WebServices/SMW_WebServiceRepositorySpecial.php
+++ extensions/DataImport/specials/WebServices/SMW_WebServiceRepositorySpecial.php
@@ -74,9 +74,9 @@ class SMWWebServiceRepositorySpecial extends SpecialPage {
 
 		$html .= "<p>".wfMsg('smw_wwsr_rep_intro')."</p>";
 		if($allowed){
-			$html .= "<table width=\"100%\" class=\"smwtable\"><tr><th>".wfMsg('smw_wwsr_name')."</th><th>".wfMsg('smw_wwsr_lastupdate')."</th><th style=\"text-align: center\">".wfMsg('smw_wwsr_update_manual')."</th><th style=\"text-align: center\">".wfMsg('smw_wwsr_rep_edit')."</th><th style=\"text-align: center\">".wfMsg('smw_wwsr_confirm')."</th></tr>";
+			$html .= "<table id=\"webservicetable\" width=\"100%\" class=\"smwtable\"><tr><th>".wfMsg('smw_wwsr_name')."</th><th>".wfMsg('smw_wwsr_lastupdate')."</th><th style=\"text-align: center\">".wfMsg('smw_wwsr_update_manual')."</th><th style=\"text-align: center\">".wfMsg('smw_wwsr_rep_edit')."</th><th style=\"text-align: center\">".wfMsg('smw_wwsr_confirm')."</th></tr>";
 		} else {
-			$html .= "<table width=\"100%\" class=\"smwtable\"><tr><th>".wfMsg('smw_wwsr_name')."</th><th>".wfMsg('smw_wwsr_lastupdate')."</th><th style=\"text-align: center\">".wfMsg('smw_wwsr_rep_edit')."</th></tr>";
+			$html .= "<table id=\"webservicetable\" width=\"100%\" class=\"smwtable\"><tr><th>".wfMsg('smw_wwsr_name')."</th><th>".wfMsg('smw_wwsr_lastupdate')."</th><th style=\"text-align: center\">".wfMsg('smw_wwsr_rep_edit')."</th></tr>";
 		}
 		foreach($webServices as $ws){
 			$wsUrl = Title::newFromID($ws->getArticleID())->getFullURL();
