01de397e6c388896997e78828e49d54a532bf793
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-06-22 10:08:25 +0000

    updated build and make files
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4143 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/RichMedia/deploy/make.bat
+++ extensions/RichMedia/deploy/make.bat
@@ -2,9 +2,9 @@
 
 REM Windows batch file for creating SMW Rich Media extension 
 
-SET VERSION="1.0"
+SET VERSION="1.1"
 
-set OUTPUT_DIR=c:\temp\halodataimport
+set OUTPUT_DIR=c:\temp\halorichmedia
 IF NOT EXIST %OUTPUT_DIR% goto CREATEDIRS
 
 del /S /Q %OUTPUT_DIR%\*
@@ -22,14 +22,6 @@ xcopy ..\* %OUTPUT_DIR%\extensions\RichMedia /S /EXCLUDE:exclude.dat /Y
 xcopy ..\patches\SemanticForms\* %OUTPUT_DIR%\extensions\SemanticForms /S /EXCLUDE:exclude_mime.dat /Y
 xcopy ..\patches\skins\* %OUTPUT_DIR%\skins\* /S /EXCLUDE:exclude_mime.dat /Y
 
-echo Installing patches for MIME-Type extension and WYSIWYG extension
-xcopy ..\..\..\patches\includes\* %OUTPUT_DIR%\includes\ /S /EXCLUDE:exclude_mime.dat /Y
-xcopy ..\..\..\patches\extensions\* %OUTPUT_DIR%\extensions\ /S /EXCLUDE:exclude_mime.dat /Y
-xcopy ..\..\..\patches\skins\* %OUTPUT_DIR%\skins\ /S /EXCLUDE:exclude_mime.dat /Y
-xcopy ..\..\..\patches\MIME-README.txt %OUTPUT_DIR%\ /S /EXCLUDE:exclude_mime.dat/Y
-xcopy ..\..\..\patches\DELETEMOVE-README.txt %OUTPUT_DIR%\ /S /EXCLUDE:exclude_mime.dat /Y
-xcopy ..\..\..\patches\WYSIWYG-README.txt %OUTPUT_DIR%\ /S /EXCLUDE:exclude_mime.dat /Y
-
 REM Pack Rich Media Extension
 
 cd bin
@@ -41,6 +33,3 @@ REM Remove temp files
 
 del /S /Q %OUTPUT_DIR%\*
 rmdir /S /Q %OUTPUT_DIR%
-
-
-
