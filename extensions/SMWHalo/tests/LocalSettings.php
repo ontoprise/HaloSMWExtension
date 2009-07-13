dbc726bd04024104643cea4db6811257e5131ee6
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-07-13 15:26:01 +0000

    added triplestore tests
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4301 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests/LocalSettings.php
+++ extensions/SMWHalo/tests/LocalSettings.php
@@ -7,4 +7,5 @@ include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
 enableSemantics('http://wiki', true);
  
 include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
-enableSMWHalo('SMWHaloStore2');
+enableSMWHalo('SMWHaloStore2', 'SMWTripleStore');
+$smwgWebserviceEndpoint="localhost:8090";
