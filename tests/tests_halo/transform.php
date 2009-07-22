47edc8caf48cf3f989aa63ec033a7cefd36b72fb
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-07-22 08:14:41 +0000

    added package parameter for transform script
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4330 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- tests/tests_halo/transform.php
+++ tests/tests_halo/transform.php
@@ -1,31 +1,38 @@
 <?php
 /**
  * Transforms a PHPUnit XML test result to into a Hudson compatible version.
- * 
- * Usage: 
- * 
+ *
+ * Usage:
+ *
  *  php Transform.php -i <input file> -o <output file>
- * 
- * 
+ *
+ *
  * @author Kai Kühn
- * 
+ *
  */
 $params = array();
 for( $arg = reset( $argv ); $arg !== false; $arg = next( $argv ) ) {
-	//-b => BotID
+	//-i => input file
 	if ($arg == '-i') {
 		$inputFile = next($argv);
 		continue;
 	}
+	// -o => output file
 	if ($arg == '-o') {
 		$outputFile = next($argv);
+		continue;
+	}
+	// -p => package name
+	if ($arg == '-p') {
+		$package = next($argv);
 		continue;
 	}
 	$params[] = $arg;
 }
 
-$xp = new XsltProcessor;
 
+$xp = new XsltProcessor;
+$xp->setParameter("", "package", isset($package) ? $package : "GeneralTests");
 $xsl = new DOMDocument;
 $xsl->load(dirname(__FILE__)."/transform.xslt");
 $xp->importStylesheet($xsl);
--- tests/tests_halo/transform.xslt
+++ tests/tests_halo/transform.xslt
@@ -2,7 +2,7 @@
 <xsl:stylesheet version="1.0"
 	xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 	<xsl:output method="xml" indent="yes" />
-
+    <xsl:param name="package" select="'GeneralTests'"/>
 
 	<xsl:template match="testsuites/testsuite">
 
@@ -29,8 +29,9 @@
 	</xsl:template>
 	<xsl:template match="testcase">
 		<xsl:copy>
+		    <xsl:variable name="parentName" select="parent::testsuite/@name"/>
 			<xsl:attribute name="classname"><xsl:value-of
-					select="parent::testsuite/@name" />
+					select="concat($package,'.',$parentName)" />
             </xsl:attribute>
 			<xsl:attribute name="name"><xsl:value-of select="@name" />
             </xsl:attribute>
