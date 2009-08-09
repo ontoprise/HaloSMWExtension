5a35cef1aee0c70cd6a3539296415d9a66ddd334
schachtler <schachtler@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-08-09 20:42:02 +0000

    Updates im Rahmen Wave 2
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4428 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/HaloACL/includes/HACL_AjaxConnector.php
+++ extensions/HaloACL/includes/HACL_AjaxConnector.php
@@ -1,7 +1,27 @@
 <?php
-/* 
- * B2browse Group
- * patrick.hilsbos@b2browse.com
+/*  Copyright 2009, ontoprise GmbH
+*  This file is part of the HaloACL-Extension.
+*
+*   The HaloACL-Extension is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   The HaloACL-Extension is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * This file contains the class HACLGroup.
+ *
+ * @author B2browse/Patrick Hilsbos, Steffen Schachtler
+ * Date: 03.04.2009
+ *
  */
 
 /**
@@ -792,7 +812,7 @@ function rightPanelSelectDeselectTab($panelid) {
                     <span class="haloacl_rightpanel_selecttab_leftpart_filter_title">
                         Filter in groups:
                     </span>
-                    <input type="text" />
+                    <input id="filterSelectGroup_$panelid" type="text" />
                 </div>
                 <div id="treeDiv_$panelid" class="haloacl_rightpanel_selecttab_leftpart_treeview">&nbsp;</div>
                 <div class="haloacl_rightpanel_selecttab_leftpart_treeview_userlink">
@@ -831,7 +851,10 @@ function rightPanelSelectDeselectTab($panelid) {
     YAHOO.haloacl.treeInstance$panelid.labelClickAction = 'YAHOO.haloacl.datatableInstance$panelid.executeQuery';
     YAHOO.haloacl.buildTreeFirstLevelFromJson(YAHOO.haloacl.treeInstance$panelid);
 
-   
+    refilter = function() {
+        YAHOO.haloacl.filterNodes (YAHOO.haloacl.treeInstance$panelid.getRoot(), document.getElementById("filterSelectGroup_$panelid").value);
+    }
+    YAHOO.util.Event.addListener("filterSelectGroup_$panelid", "keyup", refilter);
 
 
 </script>
