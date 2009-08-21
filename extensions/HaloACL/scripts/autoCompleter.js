fb1f1bbccad0a4f79b304bdd839983ce53bb850a
schachtler <schachtler@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-08-21 21:35:40 +0000

    Updates im Rahmen Wave 2
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4581 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/HaloACL/scripts/autoCompleter.js
@@ -0,0 +1,97 @@
+/*  Copyright 2009, ontoprise GmbH
+ *  This file is part of the HaloACL-Extension.
+ *
+ *   The HaloACL-Extension is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The HaloACL-Extension is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This file contains the AutoCompleter.
+ *
+ * @author B2browse/Patrick Hilsbos, Steffen Schachtler
+ * Date: 03.04.2009
+ *
+ */
+
+
+/**
+ *  @param  target-div-id
+ *
+ */
+
+
+YAHOO.haloacl.AutoCompleter = function(fieldName, containerName) {
+
+    // datasource for this userdatatable
+    var myDataSource = new YAHOO.util.DataSource("?action=ajax");
+    myDataSource.responseType = YAHOO.util.DataSource.TYPE_JSON;
+    myDataSource.connMethodPost = true;
+    myDataSource.responseSchema = {
+        resultsList: "records",
+        fields : ["id", "name"]
+    };
+ 
+
+    // Enable caching
+   /// myDataSource.maxCacheEntries = 5;
+
+    // Instantiate the AutoComplete
+   
+
+    var oAC = new YAHOO.widget.AutoComplete(fieldName, containerName, myDataSource);
+
+    function fnCallback(e, args) {
+        YAHOO.util.Dom.get(fieldName).value = args[2][1];
+
+    }
+    oAC.itemSelectEvent.subscribe(fnCallback);
+
+
+    oAC.generateRequest = function(sQuery) {
+        return "rs=getAutocompleteDocuments&rsargs[]=" + sQuery;
+    };
+          
+  // Custom formatter to highlight the matching letters
+    oAC.formatResult = function(oResultData, sQuery, sResultMatch) {
+
+        var query = sQuery.toLowerCase(),
+            name = oResultData[1],
+            nameMatchIndex = name.toLowerCase().indexOf(query),
+            displayname;
+
+        if(nameMatchIndex > -1) {
+            displayname = highlightMatch(name, query, nameMatchIndex);
+        }
+        else {
+            displayname = name;
+        }
+
+        return displayname;
+
+    };
+
+    // Helper function for the formatter
+    var highlightMatch = function(full, snippet, matchindex) {
+        return full.substring(0, matchindex) +
+                "<span class='match'>" +
+                full.substr(matchindex, snippet.length) +
+                "</span>" +
+                full.substring(matchindex + snippet.length);
+    };
+
+
+    return {
+        oDS: myDataSource,
+        oAC: oAC
+    };
+};
