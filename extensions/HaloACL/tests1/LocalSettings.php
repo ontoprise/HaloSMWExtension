36b9708edac1d03f5b5e17fdc578c63c552d3466
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-08-21 15:34:19 +0000

    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4578 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/HaloACL/tests1/LocalSettings.php
+++ extensions/HaloACL/tests1/LocalSettings.php
@@ -1,5 +1,5 @@
 $smwgMessageBroker='localhost';
-$smwgWebserviceEndpoint='localhost:8060';
+//$smwgWebserviceEndpoint='localhost:8060';
 
 
 #Import SMW, SMWHalo and the Gardening extension
@@ -7,10 +7,12 @@ include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
 enableSemantics('http://mywiki', true);
  
 include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
-//enableSMWHalo('SMWHaloStore2');
-enableSMWHalo('SMWHaloStore2', 'SMWTripleStore', "http://mywiki/ob");
+enableSMWHalo('SMWHaloStore2');
+//enableSMWHalo('SMWHaloStore2', 'SMWTripleStore', "http://mywiki/ob");
 
 include_once('extensions/HaloACL/includes/HACL_Initialize.php');
+global haclgProtectProperties;
+$haclgProtectProperties = true;
 enableHaloACL(); 
 
 include_once('extensions/RichMedia/includes/RM_Initialize.php');
