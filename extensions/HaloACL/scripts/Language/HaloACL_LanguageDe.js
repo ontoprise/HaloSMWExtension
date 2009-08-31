337a9295769938af013675b434228de6fa67fc7e
hilsbos <hilsbos@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-08-31 07:16:33 +0000

    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4670 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/HaloACL/scripts/Language/HaloACL_LanguageDe.js
@@ -0,0 +1,47 @@
+/*  Copyright 2007, ontoprise GmbH
+*  This file is part of the halo-Extension.
+*
+*   The halo-Extension is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   The halo-Extension is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var wgContLanguageStrings = {
+	'NoName'                : '(namenlos)',
+        'groups'                : 'Gruppen',
+        'createACL'             : 'ACL anlegen',
+        'manageACLs'            : 'ACLs verwalten',
+        'manageACLTooltip'      : 'ACLs verwalten, eigenes Standard Template verwalten und ACL Favoriten verwalten',
+        'manageUser'            : 'Nutzer verwalten',
+        'manageUserTooltip'     : 'Hier können ACL Gruppen angelegt, editiert und gelöscht werden',
+        'whitelists'            : 'Whitelists',
+        'manageWhitelistTooltip': 'Hier können Whitelist Einträge angelegt und gelöscht werden ',
+        'manageExistingACLs'    : 'ACLs verwalten',
+        'manageDefaultUserTemplate' : 'Standard Nutzer Template verwalten',
+        'manageQuickAccess'     : 'ACL Favoriten verwalten',
+        'createStandardACL'     : 'ACL anlegen',
+        'createACLTemplate'     : 'ACL Template anlegen',
+        'createDefaultUserTemplate' : 'Standard Nutzer Template anlegen',
+        'selectDeselect'        : 'Auswählen',
+        'assigned'              : 'Zugeordnet',
+        'newSubgroup'           : 'Neue Untergruppe',
+        'clickEditToCreate'     : 'Zum anlegen Edit anklicken',
+        'ok'                    : 'OK',
+        'cancel'                : 'Abbrechen',
+        'name'                  : 'Name',
+        'selected'              : 'Ausgewähl',
+        'delete'                : 'Löschen',
+        'to'                    : 'bis',
+        'from'                  : 'von',
+        'remove'                : 'Löschen',
+	
+}
---
+++ extensions/HaloACL/scripts/Language/HaloACL_LanguageEn.js
@@ -0,0 +1,48 @@
+/*  Copyright 2007, ontoprise GmbH
+*  This file is part of the halo-Extension.
+*
+*   The halo-Extension is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   The halo-Extension is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var wgContLanguageStrings = {
+	'NoName'                : '(no name)',
+        'groups'                : 'Groups',
+        'createACL'             : 'Create ACL',
+        'manageACLs'            : 'Manage ACLs',
+        'manageACLTooltip'      : 'Manage existing ACLs, Manage own default user template and Manage quick access ACLs',
+        'manageUser'            : 'Manage User',
+        'manageUserTooltip'     : 'In this tab you can create, edit and delete ACL groups',
+        'whitelists'            : 'Whitelists',
+        'manageWhitelistTooltip': 'In this tab you can create and delete whitelist entries ',
+        'manageExistingACLs'    : 'Manage existing ACLs',
+        'manageDefaultUserTemplate' : 'Manage own default user template',
+        'manageQuickAccess'     : 'Manage quick access ACLs',
+        'createStandardACL'     : 'Create standard ACL',
+        'createACLTemplate'     : 'Create ACL template',
+        'createDefaultUserTemplate' : 'Create ACL default user template',
+        'selectDeselect'        : 'Select / Deselect',
+        'assigned'              : 'Assigned',
+        'newSubgroup'           : 'new subgroup',
+        'clickEditToCreate'     : 'click edit to create',
+        'ok'                    : 'OK',
+        'cancel'                : 'Cancel',
+        'name'                  : 'Name',
+        'selected'              : 'Selected',
+        'delete'                : 'Delete',
+        'to'                    : 'to',
+        'from'                  : 'from',
+        'remove'                : 'Remove',
+
+	
+}
