6b3a26d45b9f482c881fa3b3e3104a73a0f11006
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-09-03 10:41:47 +0000

    namespace definitions for pop3 import
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4734 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/DataImport/specials/TermImport/SMW_ImportedTermsNamespaces.php
+++ extensions/DataImport/specials/TermImport/SMW_ImportedTermsNamespaces.php
@@ -4,8 +4,6 @@ global $smwgWWSNamespaceIndex, $wgContLang;
 
 if (!defined('NS_TI_EMAIL')) define('NS_TI_EMAIL', $smwgWWSNamespaceIndex+20);
 if (!defined('NS_TI_EMAIL_TALK')) define('NS_TI_EMAIL_TALK', $smwgWWSNamespaceIndex+21);
-if (!defined('NS_TI_ICALENDAR')) define('NS_TI_ICALENDAR', $smwgWWSNamespaceIndex+24);
-if (!defined('NS_TI_ICALENDAR_TALK')) define('NS_TI_ICALENDAR_TALK', $smwgWWSNamespaceIndex+25);
 
 
 global $wgExtraNamespaces;
