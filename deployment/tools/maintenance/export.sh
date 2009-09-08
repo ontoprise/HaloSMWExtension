7144bc898b66cbb4bb3f7c4dd0efcd6c09c28295
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-09-08 10:47:11 +0000

    shell script for linux
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@4838 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/maintenance/export.sh
+++ deployment/tools/maintenance/export.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-//TODO: write script
+if [! -e "$2" ]
+then
+ mkdir $2
+fi
+:dump
+php export.php --current --output=file:$2/dump.xml -b $1 $3 $4 $5
+php exportDesc.php -o $2/deploy.xml -b $1 -d dump.xml $3 $4 $5
