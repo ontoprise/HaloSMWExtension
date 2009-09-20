051872a20e0914871c1b28f431b4d6161c78a0d1
hilsbos <hilsbos@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-09-20 13:15:27 +0000

    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@5144 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/HaloACL/includes/HACL_GlobalFunctions.php
+++ extensions/HaloACL/includes/HACL_GlobalFunctions.php
@@ -289,7 +289,10 @@ function haclAddHTMLHeader(&$out) {
         $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/yuiloader-min.js"></script>');
         $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/event-min.js"></script>');
         $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/dom-min.js"></script>');
-        $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/treeview-min.js"></script>');
+
+        $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/treeview-debug.js"></script>');
+        $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/logger-min.js"></script>');
+
         $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/element-min.js"></script>');
         $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/button-min.js"></script>');
         $out->addScript('<script type="text/javascript" src="'. $haclgHaloScriptPath .  '/yui/connection-min.js"></script>');
