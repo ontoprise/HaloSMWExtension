739d94fa883f81292530f845c9f7658037e5b914
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-10-06 15:15:55 +0000

    bugfix: QPs were moved to SRF.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@5484 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/queryprinters/SMW_QP_Halo.php
+++ extensions/SMWHalo/includes/queryprinters/SMW_QP_Halo.php
@@ -1,12 +1,11 @@
 <?php
 global $smwgIP;
 require_once "$smwgIP/includes/SMW_QP_CSV.php";
-require_once "$smwgIP/includes/SMW_QP_iCalendar.php";
 require_once "$smwgIP/includes/SMW_QP_Embedded.php";
 require_once "$smwgIP/includes/SMW_QP_List.php";
 require_once "$smwgIP/includes/SMW_QP_RSSlink.php";
 require_once "$smwgIP/includes/SMW_QP_Template.php";
-require_once "$smwgIP/includes/SMW_QP_vCard.php";
+
 
 
 /**
@@ -17,167 +16,151 @@ require_once "$smwgIP/includes/SMW_QP_vCard.php";
  */
 class SMWQPParameter {
 
-	// actual parameter used by QP
-	public $mParam;
+    // actual parameter used by QP
+    public $mParam;
 
-	// parameter name to show
-	public $mParamName;
+    // parameter name to show
+    public $mParamName;
 
-	// possible values of the parameter. Can be:
+    // possible values of the parameter. Can be:
 
-	// 1. enums (array of string)
-	// 2. string (<string>)
-	// 3. number (<number>)
-	// 4. boolean (<boolean>)
-	public $mValues;
+    // 1. enums (array of string)
+    // 2. string (<string>)
+    // 3. number (<number>)
+    // 4. boolean (<boolean>)
+    public $mValues;
 
-	// default value (optional)
-	public $mDefault;
+    // default value (optional)
+    public $mDefault;
 
-	// more verbose description than $mParamName (optional)
-	public $mParamDescription;
-	
-	// AC constraints
-	public $mConstraints = NULL;
+    // more verbose description than $mParamName (optional)
+    public $mParamDescription;
+    
+    // AC constraints
+    public $mConstraints = NULL;
 
-	public function __construct($param, $paramName, $values, $default = NULL, $paramDescription = NULL) {
-		$this->mParam = $param;
-		$this->mParamName = $paramName;
-		$this->mValues = $values;
-		$this->mDefault = $default;
-		$this->mParamDescription = $paramDescription;
-	}
+    public function __construct($param, $paramName, $values, $default = NULL, $paramDescription = NULL) {
+        $this->mParam = $param;
+        $this->mParamName = $paramName;
+        $this->mValues = $values;
+        $this->mDefault = $default;
+        $this->mParamDescription = $paramDescription;
+    }
 
-	public function getParameter() {
-		return $this->mParam;
-	}
+    public function getParameter() {
+        return $this->mParam;
+    }
 
-	public function getParameterName() {
-		return $this->mParamName;
-	}
+    public function getParameterName() {
+        return $this->mParamName;
+    }
 
-	public function getDefaultValue() {
-		return $this->mDefault;
-	}
+    public function getDefaultValue() {
+        return $this->mDefault;
+    }
 
-	public function getValues() {
-		return $this->mValues;
-	}
+    public function getValues() {
+        return $this->mValues;
+    }
 
-	public function getParameterDescription() {
-		return $this->mParamDescription;
-	}
-	
-	public function setConstraints($constraints) {
-		$this->mConstraints = $constraints;
-	}
+    public function getParameterDescription() {
+        return $this->mParamDescription;
+    }
+    
+    public function setConstraints($constraints) {
+        $this->mConstraints = $constraints;
+    }
 }
 
 function smwfhCreateDefaultParameters() {
-	$order = new SMWQPParameter('order', 'Order', array('ascending','descending'), NULL, "Sort order");
-	$link = new SMWQPParameter('link', 'Link', array('all','subject', 'none'), NULL, "Show everything as link, only subjects or nothing at all.");
-	$limit = new SMWQPParameter('limit', 'Limit', '<number>', NULL, "Instance display limit");
-	$headers = new SMWQPParameter('headers', 'Headers', array('show', 'hide'), NULL, "Show headers or not.");
-	$intro = new SMWQPParameter('intro', 'Intro', '<string>', NULL, "Intro text");
-	$mainlabel = new SMWQPParameter('mainlabel', 'Mainlabel', '<string>', NULL, "Name of main column");
-	$default = new SMWQPParameter('default', 'Order', '<string>', NULL, "Displayed when there are no results at all.");
-	return array($order, $link, $limit, $headers, $intro, $mainlabel, $default);;
+    $order = new SMWQPParameter('order', 'Order', array('ascending','descending'), NULL, "Sort order");
+    $link = new SMWQPParameter('link', 'Link', array('all','subject', 'none'), NULL, "Show everything as link, only subjects or nothing at all.");
+    $limit = new SMWQPParameter('limit', 'Limit', '<number>', NULL, "Instance display limit");
+    $headers = new SMWQPParameter('headers', 'Headers', array('show', 'hide'), NULL, "Show headers or not.");
+    $intro = new SMWQPParameter('intro', 'Intro', '<string>', NULL, "Intro text");
+    $mainlabel = new SMWQPParameter('mainlabel', 'Mainlabel', '<string>', NULL, "Name of main column");
+    $default = new SMWQPParameter('default', 'Order', '<string>', NULL, "Displayed when there are no results at all.");
+    return array($order, $link, $limit, $headers, $intro, $mainlabel, $default);;
 }
 
 class SMWHaloTableResultPrinter extends SMWTableResultPrinter {
-	// supported parameters
-	protected $mParameters;
+    // supported parameters
+    protected $mParameters;
 
 
-	public function __construct($format, $inline) {
-		parent::__construct($format, $inline);
-		$this->mParameters = smwfhCreateDefaultParameters();
-	}
+    public function __construct($format, $inline) {
+        parent::__construct($format, $inline);
+        $this->mParameters = smwfhCreateDefaultParameters();
+    }
 
-	function getSupportedParameters() {
-		return $this->mParameters;
-	}
+    function getSupportedParameters() {
+        return $this->mParameters;
+    }
 }
 
 
 class SMWHaloCsvResultPrinter extends SMWCsvResultPrinter{
-	protected $mParameters;
+    protected $mParameters;
 
-	public function __construct($format, $inline) {
-		parent::__construct($format, $inline);
-		$this->mParameters = smwfhCreateDefaultParameters();
-		$sep = new SMWQPParameter('sep', 'Separator', '<string>', ',', "Separator used");
-		$this->mParameters[] = $sep;
-	}
+    public function __construct($format, $inline) {
+        parent::__construct($format, $inline);
+        $this->mParameters = smwfhCreateDefaultParameters();
+        $sep = new SMWQPParameter('sep', 'Separator', '<string>', ',', "Separator used");
+        $this->mParameters[] = $sep;
+    }
 
-	function getSupportedParameters() {
-		return $this->mParameters;
-	}
+    function getSupportedParameters() {
+        return $this->mParameters;
+    }
 
 }
 
 class SMWHaloEmbeddedResultPrinter extends SMWEmbeddedResultPrinter{
-	protected $mParameters;
+    protected $mParameters;
 
-	public function __construct($format, $inline) {
-		parent::__construct($format, $inline);
-		$this->mParameters = smwfhCreateDefaultParameters();
-		$embeddOnly = new SMWQPParameter('embedonly', 'Embedd only', '<boolean>', NULL, "Show header or not");
-		$embedformat = new SMWQPParameter('embedformat', 'Embedd format', '<string>', NULL, "Embedded format");
-		$this->mParameters[] = $embeddOnly;
-		$this->mParameters[] = $embedformat;
-	}
+    public function __construct($format, $inline) {
+        parent::__construct($format, $inline);
+        $this->mParameters = smwfhCreateDefaultParameters();
+        $embeddOnly = new SMWQPParameter('embedonly', 'Embedd only', '<boolean>', NULL, "Show header or not");
+        $embedformat = new SMWQPParameter('embedformat', 'Embedd format', '<string>', NULL, "Embedded format");
+        $this->mParameters[] = $embeddOnly;
+        $this->mParameters[] = $embedformat;
+    }
 
-	function getSupportedParameters() {
-		return $this->mParameters;
-	}
-
-
-}
-
-class SMWHaloiCalendarResultPrinter extends SMWiCalendarResultPrinter {
-	protected $mParameters;
-
-	public function __construct($format, $inline) {
-		parent::__construct($format, $inline);
-		$iCalendarTitle = new SMWQPParameter('icalendartitle', 'iCalendar title', '<string>', NULL, "iCalendar title");
-		$iCalendarDescription = new SMWQPParameter('icalendardescription', 'iCalendar description', '<string>', NULL, "iCalendar description");
-		$this->mParameters[] = $iCalendarTitle;
-		$this->mParameters[] = $iCalendarDescription;
-	}
-
-	function getSupportedParameters() {
-		return $this->mParameters;
-	}
+    function getSupportedParameters() {
+        return $this->mParameters;
+    }
 
 
 }
+
+
 
 
 class SMWHaloListResultPrinter extends SMWListResultPrinter {
-	protected $mParameters;
+    protected $mParameters;
 
-	public function __construct($format, $inline) {
-		parent::__construct($format, $inline);
-		$order = new SMWQPParameter('order', 'Order', array('ascending','descending'), NULL, "Sort order");
-		$link = new SMWQPParameter('link', 'Link', array('all','subject', 'none'), NULL, "Show everything as link, only subjects or nothing at all.");
-		$limit = new SMWQPParameter('limit', 'Limit', '<number>', NULL, "Instance display limit");
-		$default = new SMWQPParameter('default', 'Default text', '<string>', NULL, "Displayed when there are no results at all.");
-		$sep = new SMWQPParameter('sep', 'Separator', '<string>', NULL, "Separator used");
-		$template = new SMWQPParameter('template', 'Template', '<string>', NULL, "Template used to display");
-		$userparam = new SMWQPParameter('userparam', 'User param', '<string>', NULL, "User param");
-		$this->mParameters[] = $order;
-		$this->mParameters[] = $link;
-		$this->mParameters[] = $limit;
-		$this->mParameters[] = $default;
-		$this->mParameters[] = $sep;
-		$this->mParameters[] = $template;
-		$this->mParameters[] = $userparam;
-	}
+    public function __construct($format, $inline) {
+        parent::__construct($format, $inline);
+        $order = new SMWQPParameter('order', 'Order', array('ascending','descending'), NULL, "Sort order");
+        $link = new SMWQPParameter('link', 'Link', array('all','subject', 'none'), NULL, "Show everything as link, only subjects or nothing at all.");
+        $limit = new SMWQPParameter('limit', 'Limit', '<number>', NULL, "Instance display limit");
+        $default = new SMWQPParameter('default', 'Default text', '<string>', NULL, "Displayed when there are no results at all.");
+        $sep = new SMWQPParameter('sep', 'Separator', '<string>', NULL, "Separator used");
+        $template = new SMWQPParameter('template', 'Template', '<string>', NULL, "Template used to display");
+        $userparam = new SMWQPParameter('userparam', 'User param', '<string>', NULL, "User param");
+        $this->mParameters[] = $order;
+        $this->mParameters[] = $link;
+        $this->mParameters[] = $limit;
+        $this->mParameters[] = $default;
+        $this->mParameters[] = $sep;
+        $this->mParameters[] = $template;
+        $this->mParameters[] = $userparam;
+    }
 
-	function getSupportedParameters() {
-		return $this->mParameters;
-	}
+    function getSupportedParameters() {
+        return $this->mParameters;
+    }
 
 
 }
@@ -205,51 +188,40 @@ class SMWHaloCountResultPrinter extends SMWListResultPrinter {
 }
 
 class SMWHaloRSSResultPrinter extends SMWRSSResultPrinter {
-	protected $mParameters;
+    protected $mParameters;
 
-	public function __construct($format, $inline) {
-		parent::__construct($format, $inline);
-		$title = new SMWQPParameter('title', 'Title', '<string>', NULL, "Title");
-		$template = new SMWQPParameter('description', 'Description', '<string>', NULL, "Description of RSS feed");
+    public function __construct($format, $inline) {
+        parent::__construct($format, $inline);
+        $title = new SMWQPParameter('title', 'Title', '<string>', NULL, "Title");
+        $template = new SMWQPParameter('description', 'Description', '<string>', NULL, "Description of RSS feed");
 
-		$this->mParameters[] = $title;
-		$this->mParameters[] = $template;
-	}
+        $this->mParameters[] = $title;
+        $this->mParameters[] = $template;
+    }
 
-	function getSupportedParameters() {
-		return $this->mParameters;
-	}
+    function getSupportedParameters() {
+        return $this->mParameters;
+    }
 
 }
 
 class SMWHaloTemplateResultPrinter extends SMWTemplateResultPrinter {
-	protected $mParameters;
+    protected $mParameters;
 
-	public function __construct($format, $inline) {
-		parent::__construct($format, $inline);
-		$template = new SMWQPParameter('template', 'Template', '<string>', NULL, "Template used to display");
-		$userparam = new SMWQPParameter('userparam', 'User param', '<string>', NULL, "User param");
+    public function __construct($format, $inline) {
+        parent::__construct($format, $inline);
+        $template = new SMWQPParameter('template', 'Template', '<string>', NULL, "Template used to display");
+        $userparam = new SMWQPParameter('userparam', 'User param', '<string>', NULL, "User param");
 
-		$this->mParameters[] = $template;
-		$this->mParameters[] = $userparam;
-	}
+        $this->mParameters[] = $template;
+        $this->mParameters[] = $userparam;
+    }
 
-	function getSupportedParameters() {
-		return $this->mParameters;
-	}
+    function getSupportedParameters() {
+        return $this->mParameters;
+    }
 
 
 }
 
-class SMWHalovCardResultPrinter extends SMWvCardResultPrinter {
-	protected $mParameters;
 
-	public function __construct($format, $inline) {
-		parent::__construct($format, $inline);
-
-	}
-
-	function getSupportedParameters() {
-		return $this->mParameters;
-	}
-}
