285fe53212c44172600844838930efe98c81a391
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-10-16 14:10:38 +0000

    Adapted LocalSettings to updated tests
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@5618 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/HaloACL/tests1/LocalSettings.php
+++ extensions/HaloACL/tests1/LocalSettings.php
@@ -1,26 +1,27 @@
 $smwgMessageBroker='localhost';
-//$smwgWebserviceEndpoint='localhost:8060';
+$smwgWebserviceEndpoint='localhost:8060';
 
 
 #Import SMW, SMWHalo and the Gardening extension
 include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
 enableSemantics('http://mywiki', true);
- 
+
 include_once('extensions/SemanticForms/includes/SF_Settings.php');
 
 include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
-enableSMWHalo('SMWHaloStore2');
-//enableSMWHalo('SMWHaloStore2', 'SMWTripleStore', "http://mywiki/ob");
+//enableSMWHalo('SMWHaloStore2');
+enableSMWHalo('SMWHaloStore2', 'SMWTripleStore', "http://mywiki/ob");
 
 include_once('extensions/HaloACL/includes/HACL_Initialize.php');
-global $haclgProtectProperties;
 $haclgProtectProperties = true;
+$haclgNewUserTemplate = "ACL:Template/NewUserTemplate";
 enableHaloACL(); 
 
-include_once('extensions/RichMedia/includes/RM_Initialize.php');
+//include_once('extensions/RichMedia/includes/RM_Initialize.php');
 //enableRichMediaExtension();
     
 $wgAllowExternalImagesFrom=$wgServer;   //This need to be set to allow the templates creating image links.
 
 $smwgNamespacesWithSemanticLinks[300] = true;
+$smwgNamespacesWithSemanticLinks[NS_USER_TALK] = true;
 
