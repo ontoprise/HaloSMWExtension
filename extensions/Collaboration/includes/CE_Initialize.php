36e55a565965d70f0a5e89f6cfdf0c1c753049d8
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-10-16 09:06:08 +0000

    renamed CE global vars
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@5606 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/Collaboration/includes/CE_Initialize.php
+++ extensions/Collaboration/includes/CE_Initialize.php
@@ -16,14 +16,14 @@ define('CE_VERSION', '1.0');
 # This is the path to your installation of Collaboration as seen on your
 # local filesystem. Used against some PHP file path issues.
 ##
-$smwgCEIP = $IP . '/extensions/Collaboration';;
+$cegIP = $IP . '/extensions/Collaboration';;
 
 ###
 # This is the path to your installation of CollaborationExtension as seen from the
 # web. Change it if required ($wgScriptPath is the path to the base directory
 # of your wiki). No final slash.
 ##
-$smwgCEScriptPath = $wgScriptPath . '/extensions/Collaboration';
+$cegScriptPath = $wgScriptPath . '/extensions/Collaboration';
 
 $wgHooks['BeforePageDisplay'][] = 'smwfCEAddHTMLHeader';
 
@@ -42,24 +42,24 @@ $wgArticleCommentDefaults['hideForm']=false;
 $wgArticleCommentDefaults['displaycomments'] = true;
 $wgArticleCommentDefaults['hidecomments'] = false;
 $wgArticleCommentDefaults['showurlfield'] = false;
-include_once($smwgCEIP.'/ArticleComments/ArticleComments.php');
+include_once($cegIP.'/ArticleComments/ArticleComments.php');
 
 # B:RatingBar
-include_once($smwgCEIP.'/RatingBar/ratingbar.php');
+include_once($cegIP.'/RatingBar/ratingbar.php');
 
 # C: CurrentUser
-include_once($smwgCEIP.'/CurrentUsers/CurrentUsers.php');
+include_once($cegIP.'/CurrentUsers/CurrentUsers.php');
 
 function smwfCEAddHTMLHeader(&$out) {
-	global $smwgCEScriptPath;
+	global $cegScriptPath;
 	// All scripts that have been added later on... 
-	$out->addScript("<script type=\"text/javascript\" src=\"".$smwgCEScriptPath .  "/scripts/CollapsibleTables.js\"></script>");
+	$out->addScript("<script type=\"text/javascript\" src=\"".$cegScriptPath .  "/scripts/CollapsibleTables.js\"></script>");
 	// style definitions
 	$out->addLink(array(
 		'rel'   => 'stylesheet',
 		'type'  => 'text/css',
 		'media' => 'screen, projection',
-		'href'  => $smwgCEScriptPath . '/skins/common.css'
+		'href'  => $cegScriptPath . '/skins/common.css'
 	));
 
 	return true;
