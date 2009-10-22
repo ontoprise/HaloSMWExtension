43e98ba5ce6bb379b4722094d42d746e35397855
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-10-22 10:00:06 +0000

    hide toolbar buttons, if the appropriate extension is not enabled.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@5671 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/FCKeditor/plugins/mediaupload/fckplugin.js
+++ extensions/FCKeditor/plugins/mediaupload/fckplugin.js
@@ -1,9 +1,14 @@
 // Register our toolbar buttons.
-var tbButton = new FCKToolbarButton( 'MW_MediaUpload', 'UploadMedia', 'Upload media' ) ;
-tbButton.IconPath = FCKConfig.PluginsPath + 'mediaupload/images/icon_mediaupload.gif' ;
-FCKToolbarItems.RegisterItem( 'MW_MediaUpload', tbButton );
-
-//FCKCommands.RegisterCommand( 'MW_MediaUpload', new FCKDialogCommand( 'MW_MediaUpload', 'UploadMedia', FCKConfig.PluginsPath + 'mediaupload/dialogs/uploadmedia.html', 400, 330 ) ) ;
+if (typeof window.parent.RichMediaPage != 'undefined') {
+    var tbButton = new FCKToolbarButton( 'MW_MediaUpload', 'UploadMedia', 'Upload media' ) ;
+    tbButton.IconPath = FCKConfig.PluginsPath + 'mediaupload/images/icon_mediaupload.gif' ;
+    FCKToolbarItems.RegisterItem( 'MW_MediaUpload', tbButton );
+}
+else {
+    var tbButton = new FCKToolbarButton( 'MW_MediaUpload', ' ', ' ' ) ;
+    tbButton.IconPath = FCKConfig.PluginsPath + 'mediawiki/images/tb_icon_blank.gif' ;
+    FCKToolbarItems.RegisterItem( 'MW_MediaUpload', tbButton );
+}
 
 var OpenUploadWindowCommand=function(){};
 OpenUploadWindowCommand.prototype.Execute=function(){ }
