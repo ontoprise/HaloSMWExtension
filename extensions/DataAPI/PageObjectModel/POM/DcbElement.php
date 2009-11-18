e508e4dc47789c773fd7c8cdbb29b6516ef2a2d1
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-11-18 14:49:44 +0000

    The DataAPI is now a module of the Halo extension
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@5872 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/DataAPI/PageObjectModel/POM/DcbElement.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Represents an abstraction of a POM element which starts and ends with curved brackets.
- * Used to express the differencies between templates and parser functions.
- */
-abstract class POMDcbElement extends POMElement
-{
-}
