e3d9296989920838e1084d9946facf346475fcc2
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-11-22 15:20:32 +0000

    alpha version on UP data rating
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@5913 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/SMWUserManual/scripts/languages/up_en.js
@@ -0,0 +1,21 @@
+/*
+ * Language file for Up Popup
+ */
+
+var UP_RatingPopupLang = {
+    headline        : 'Infobox',
+    data            : 'Data',
+    source          : 'Source',
+    dbpedia         : 'DBPedia',
+    read_only       : 'read only',
+    feedback        : 'Feedback',
+    data_correct    : 'Do you think the data is correct?',
+    yes             : 'yes',
+    no              : 'no',
+    reset_feedback  : 'Reset feedback',
+    submit_feedback : 'Submit feedback',
+    comments        : 'Comments',
+    rate_table      : 'Rate and comment complete table',
+    fieldsempty     : 'Please enter a comment and rate the data before submiting the request.',
+    err_comment_up  : 'An error occured when sending your request to Ultrapedia. The error message was "%s".'
+}
