2ad684a29d05139fd7212e78bbd23f191358f7a6
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-12-07 16:25:45 +0000

    tests for the DataAPI added
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@6118 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests/LocalSettings.php
+++ extensions/SMWHalo/tests/LocalSettings.php
@@ -9,3 +9,13 @@ enableSemantics('http://wiki', true);
 include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
 enableSMWHalo('SMWHaloStore2', 'SMWTripleStore', 'http://publicbuild/ob');
 $smwgWebserviceEndpoint="localhost:8090";
+
+#For DataAPI tests
+include_once('extensions/SemanticForms/includes/SF_Settings.php');
+$wgEnableWriteAPI = true;
+$pcpWSServer=true;
+include_once('extensions/SMWHalo/DataAPI/PageCRUD_Plus/PCP.php');
+$pomWSServer=true;
+include_once('extensions/SMWHalo/DataAPI/PageObjectModel/POM.php');
+include_once('extensions/SMWHalo/DataAPI/SemanticFormsAPI/SemanticFormsAPI.php');
+
