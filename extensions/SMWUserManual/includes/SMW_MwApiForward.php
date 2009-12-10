1fd53cef7425c27016ea298bea9a6caaca6f9331
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-12-10 14:02:34 +0000

    add some more error checking and increas time out values for connect and response.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@6169 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWUserManual/includes/SMW_MwApiForward.php
+++ extensions/SMWUserManual/includes/SMW_MwApiForward.php
@@ -16,21 +16,25 @@ function wfUprForwardApiCall(){
     curl_setopt($c, CURLOPT_POST, true);
     curl_setopt($c, CURLOPT_POSTFIELDS, $data);
     curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 120); // 2 min wait for connect
+    curl_setopt($c, CURLOPT_TIMEOUT, 300); // 5 min wait for response
     if (isset($_SERVER['HTTP_USER_AGENT']))
         curl_setopt($c, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
     $res = curl_exec($c);
     $httpErr = curl_getinfo($c, CURLINFO_HTTP_CODE);
+    $curlErr = curl_errno($c);
     curl_close($c);
     /* uncomment the following lines for debug */
     /*
     $fp = fopen(dirname(__FILE__).'/debug.log', "a+");
     if ($fp) {
-        fputs($fp, "new request: http result $httpErr\n");
+        fputs($fp, "new request: http result $httpErr, curl errno $curlErr\n");
         fputs($fp, print_r($data."\n", true));
         fputs($fp, print_r($res."\n", true));
         fclose($fp);
     }
     */
+    if ($curlErr != 0 && $httpErr != 200) $res='';
     return $res;
 }
 
