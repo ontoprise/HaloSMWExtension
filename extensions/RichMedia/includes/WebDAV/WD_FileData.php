7c0d3eb9dfd8b61709b6b564a0d2840e75b254fa
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-12-10 13:16:34 +0000

    bugfix for webdav encoding problem with question marks
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@6166 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/RichMedia/includes/WebDAV/WD_FileData.php
+++ extensions/RichMedia/includes/WebDAV/WD_FileData.php
@@ -190,7 +190,7 @@ class FileData {
 		$fileName = str_replace("-slash-", '/', $fileName);
 		$fileName = str_replace("-quote-", '"', $fileName);
 		$fileName = str_replace("-ampersize-", "&", $fileName);
-		$fileName = str_replace("-apostroph-", "'", $fileName);
+		$fileName = str_replace("-q-mark-", "?", $fileName);
 		$fileName = str_replace("-percent-", "%", $fileName);
 		return $fileName;
 	}
@@ -202,7 +202,7 @@ class FileData {
 		$fileName = str_replace('"', "-quote-", $fileName);
 		$fileName = str_replace('&', "-ampersize-", $fileName);
 		$fileName = str_replace('%', "-percent-", $fileName);
-		$fileName = str_replace("'", "-apostroph-", $fileName);
+		$fileName = str_replace("?", "-q-mark-", $fileName);
 		return urlencode($fileName);
 	}
 	
