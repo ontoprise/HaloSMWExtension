27e80d0a524d68195b08f1801dd08d0db1c98a57
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2009-12-18 12:13:12 +0000

    fix: 11545
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@6309 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SemanticMediaWiki/specials/Statistics/SMW_SpecialStatistics.php
+++ extensions/SemanticMediaWiki/specials/Statistics/SMW_SpecialStatistics.php
@@ -10,39 +10,42 @@
  */
 
 function smwfExecuteSemanticStatistics() {
-	global $wgOut, $wgLang;
-	$dbr =& wfGetDB( DB_SLAVE );
+    global $wgOut, $wgLang;
+    $dbr =& wfGetDB( DB_SLAVE );
 
-	// Do not give these statistics here. They are quite unrelated.
-	// 		$views = SiteStats::views();
-	// 		$edits = SiteStats::edits();
-	// 		$good = SiteStats::articles();
-	// 		$images = SiteStats::images();
-	// 		$users = SiteStats::users();
+    // Do not give these statistics here. They are quite unrelated.
+    //      $views = SiteStats::views();
+    //      $edits = SiteStats::edits();
+    //      $good = SiteStats::articles();
+    //      $images = SiteStats::images();
+    //      $users = SiteStats::users();
 
-	$semstats = smwfGetStore()->getStatistics();
+    $semstats = smwfGetStore()->getStatistics();
 
-	$page_table = $dbr->tableName( 'page' );
-	$sql = "SELECT Count(page_id) AS count FROM $page_table WHERE page_namespace=" . SMW_NS_PROPERTY;
-	$res = $dbr->query( $sql );
-	$row = $dbr->fetchObject( $res );
-	$property_pages = $row->count;
-	$dbr->freeResult( $res );
+    $page_table = $dbr->tableName( 'page' );
+    $sql = "SELECT Count(page_id) AS count FROM $page_table WHERE page_namespace=" . SMW_NS_PROPERTY;
+    $res = $dbr->query( $sql );
+    $row = $dbr->fetchObject( $res );
+    $property_pages = $row->count;
+    $dbr->freeResult( $res );
 
-	$sp = Title::makeTitle( NS_SPECIAL, 'Properties');
-	$purl = $sp->getFullURL();
-	$sp = Title::makeTitle( NS_SPECIAL, 'WantedProperties');
-	$wpurl = $sp->getFullURL();
-	$sp = Title::makeTitle( NS_SPECIAL, 'UnusedProperties');
-	$upurl = $sp->getFullURL();
-	wfLoadExtensionMessages('SemanticMediaWiki');
-	$out = wfMsg('smw_semstats_text',
-	             $wgLang->formatNum($semstats['PROPUSES']), $wgLang->formatNum($semstats['USEDPROPS']),
-	             $purl, $wgLang->formatNum($property_pages), $wgLang->formatNum($semstats['DECLPROPS']),
-	             $upurl, $wpurl);
-
-	$wgOut->addHTML( $out );
+    $sp = Title::makeTitle( NS_SPECIAL, 'Properties');
+    $purl = $sp->getFullURL();
+    $sp = Title::makeTitle( NS_SPECIAL, 'WantedProperties');
+    $wpurl = $sp->getFullURL();
+    $sp = Title::makeTitle( NS_SPECIAL, 'UnusedProperties');
+    $upurl = $sp->getFullURL();
+    wfLoadExtensionMessages('SemanticMediaWiki');
+    $out = wfMsg('smw_semstats_text',
+                 $wgLang->formatNum($semstats['PROPUSES']), $wgLang->formatNum($semstats['USEDPROPS']),
+                 $wgLang->formatNum($property_pages), $wgLang->formatNum($semstats['DECLPROPS']),
+                 $upurl, $wpurl);
+    // op-patch|KK|rendering not correct|start           
+    global $wgParser, $wgUser;
+    if (is_object($wgParser)) $psr =& $wgParser; else $psr = new Parser;
+    $opt = new ParserOptions();
+    $out = $psr->parse($out,Title::makeTitle( NS_SPECIAL, 'SemanticStatistics'),$opt,true,true)->getText();
+    // op-patch|KK|end
+    
+    $wgOut->addHTML( $out );
 }
-
-
-
