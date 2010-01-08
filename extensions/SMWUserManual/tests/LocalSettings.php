f0751fb19253286d7a9b5c700b18b6f1b0363ad5
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-01-08 16:16:05 +0000

    add webtests for CSH
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@6398 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/SMWUserManual/tests/LocalSettings.php
@@ -0,0 +1,15 @@
+<?php
+
+#Import SMW, SMWHalo
+include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
+enableSemantics('http://wiki', true);
+
+include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
+enableSMWHalo('SMWHaloStore2');
+
+# Parser functions (needed for copyright template)
+include_once('extensions/ParserFunctions/ParserFunctions.php');
+
+#UserManual extension
+require_once('extensions/SMWUserManual/includes/SMW_UserManual.php');
+enableSMWUserManual();
