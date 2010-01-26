2e5966f330b7be705aad6535a0fb4adc8a84a711
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-01-26 17:00:55 +0000

    fix: 11618
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@6490 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/SMW_WebInterfaces.php
+++ extensions/SMWHalo/includes/SMW_WebInterfaces.php
@@ -18,9 +18,11 @@ function smwf_ws_callEQI($query) {
 
 # same as smwf_ws_callEQI except that XML is returned
 function smwf_ws_callEQIXML($query) {
- global $IP;
- require_once( $IP . '/extensions/SMWHalo/includes/webservices/SMW_EQI.php' );
- return query($query, "xml");
+	global $IP;
+	require_once( $IP . '/extensions/SMWHalo/includes/webservices/SMW_EQI.php' );
+	$result= new AjaxResponse( query($query, "xml") );
+	$result->setContentType( "application/xml" );
+	return $result;
 }
 
 #
@@ -32,9 +34,9 @@ function smwf_ws_getWSDL($wsdlID) {
 		$handle = fopen($wsdl, "rb");
 		$contents = fread ($handle, filesize ($wsdl));
 		fclose($handle);
-        global $wgServer, $wgScript;
+		global $wgServer, $wgScript;
 		return str_replace("{{wiki-path}}", $wgServer.$wgScript, $contents);
-		
+
 	} else if ($wsdlID == 'get_sparql') {
 		$wsdl = "extensions/SMWHalo/includes/webservices/sparql.wsdl";
 		$handle = fopen($wsdl, "rb");
@@ -45,25 +47,25 @@ function smwf_ws_getWSDL($wsdlID) {
 		else echo "No webservice endpoint defined! Set \$smwgWebserviceEndpoint in your LocalSettings.php. E.g.: \$smwgWebserviceEndpoint = \"localhost:8080\"";
 		exit;
 	}  else if ($wsdlID == 'get_sparul') {
-        $wsdl = "extensions/SMWHalo/includes/webservices/sparul.wsdl";
-        $handle = fopen($wsdl, "rb");
-        $contents = fread ($handle, filesize ($wsdl));
-        fclose($handle);
-        global $smwgWebserviceEndpoint;
-        // NOTE: SPARUL updates on multiple endpoints make no sense, so it is assumed that there is only one endpoint. This is checked during initialization.
-        if (isset($smwgWebserviceEndpoint)) return str_replace("{{webservice-endpoint}}", $smwgWebserviceEndpoint, $contents);
-        else echo "No webservice endpoint defined! Set \$smwgWebserviceEndpoint in your LocalSettings.php. E.g.: \$smwgWebserviceEndpoint = \"localhost:8080\"";
-        exit;
-    }  else if ($wsdlID == 'get_manage') {
-        $wsdl = "extensions/SMWHalo/includes/webservices/manage.wsdl";
-        $handle = fopen($wsdl, "rb");
-        $contents = fread ($handle, filesize ($wsdl));
-        fclose($handle);
-        global $smwgWebserviceEndpoint;
-        if (isset($smwgWebserviceEndpoint)) return str_replace("{{webservice-endpoint}}", smwfgetWebserviceEndpoint($smwgWebserviceEndpoint), $contents);
-        else echo "No webservice endpoint defined! Set \$smwgWebserviceEndpoint in your LocalSettings.php. E.g.: \$smwgWebserviceEndpoint = \"localhost:8080\"";
-        exit;
-    } else if ($wsdlID == 'get_flogic') {
+		$wsdl = "extensions/SMWHalo/includes/webservices/sparul.wsdl";
+		$handle = fopen($wsdl, "rb");
+		$contents = fread ($handle, filesize ($wsdl));
+		fclose($handle);
+		global $smwgWebserviceEndpoint;
+		// NOTE: SPARUL updates on multiple endpoints make no sense, so it is assumed that there is only one endpoint. This is checked during initialization.
+		if (isset($smwgWebserviceEndpoint)) return str_replace("{{webservice-endpoint}}", $smwgWebserviceEndpoint, $contents);
+		else echo "No webservice endpoint defined! Set \$smwgWebserviceEndpoint in your LocalSettings.php. E.g.: \$smwgWebserviceEndpoint = \"localhost:8080\"";
+		exit;
+	}  else if ($wsdlID == 'get_manage') {
+		$wsdl = "extensions/SMWHalo/includes/webservices/manage.wsdl";
+		$handle = fopen($wsdl, "rb");
+		$contents = fread ($handle, filesize ($wsdl));
+		fclose($handle);
+		global $smwgWebserviceEndpoint;
+		if (isset($smwgWebserviceEndpoint)) return str_replace("{{webservice-endpoint}}", smwfgetWebserviceEndpoint($smwgWebserviceEndpoint), $contents);
+		else echo "No webservice endpoint defined! Set \$smwgWebserviceEndpoint in your LocalSettings.php. E.g.: \$smwgWebserviceEndpoint = \"localhost:8080\"";
+		exit;
+	} else if ($wsdlID == 'get_flogic') {
 		$wsdl = "extensions/SMWHalo/includes/webservices/flogic.wsdl";
 		$handle = fopen($wsdl, "rb");
 		$contents = fread ($handle, filesize ($wsdl));
@@ -82,6 +84,6 @@ function smwf_ws_getWSDL($wsdlID) {
 		else echo "No webservice endpoint defined! Set \$smwgWebserviceEndpoint in your LocalSettings.php. E.g.: \$smwgWebserviceEndpoint = \"localhost:8080\"";
 		exit;
 	}
-}	
+}
 
 
