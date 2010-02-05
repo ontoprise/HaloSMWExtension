207ae128e3a3138de531a54ca2c2a7a128ee720f
vulcan_jesse <vulcan_jesse@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-02-05 09:39:47 +0000

    Add empty hint in abstract tooltip Remove all html tag in abstract tooltip
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@6506 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/Ultrapedia/scripts/abstractilink.js
+++ extensions/Ultrapedia/scripts/abstractilink.js
@@ -7,13 +7,25 @@ Ext.onReady(function(){
 		var page = links[i].href.substring((AjaxInternalLinks.baseUrl + '/index.php/').length + idx);
 		if((idx=page.indexOf('#'))>0) continue;
 		if(links[i].id == '') links[i].id = id;
+		links[i].title='';
 		AjaxInternalLinks.data.push({id:id,page:page});
 	}
 	for(var i=0;i<AjaxInternalLinks.data.length;++i) {
 	    new Ext.ToolTip({
 	        target: AjaxInternalLinks.data[i].id,
 	        width: 200,
-	        autoLoad: {url: AjaxInternalLinks.baseUrl + '/index.php?action=ajax&rs=smwf_up_Access&rsargs[]=ajaxSparql&rsargs[]=' + i + ',[[' +AjaxInternalLinks.data[i].page + ']] | ?Abstract | format=list'},
+	        autoLoad: {
+	        	url: AjaxInternalLinks.baseUrl + '/index.php?action=ajax&rs=smwf_up_Access&rsargs[]=ajaxSparql&rsargs[]=' + i + ',[[' +AjaxInternalLinks.data[i].page + ']] | ?Abstract | format=list | headers=hide',
+	        	callback: function(el, success, response, options){
+	        		var html = el.dom.innerHTML.replace(/\<[^\>]*\>/mg, '');
+	        		if(html.replace(/(&nbsp;)/mg, '').trim() == '') {
+	        			html = 'There is no abstract in this article ...';
+	        		} 
+		        	el.dom.innerHTML = html;
+	        	}
+	        },
+	        
+	        dismissDelay: 0
 	        // dismissDelay: 15000 // auto hide after 15 seconds
 	    });
     }
