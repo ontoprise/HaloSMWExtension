5fa69c092ad61c0f0cde8808535e5e50d59ca53a
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-02-23 11:56:27 +0000

    fixed undefined offset when running on a non-windows machine (array of matches has been null).
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@6576 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- patches/patch.php
+++ patches/patch.php
@@ -143,11 +143,10 @@ function isWindows() {
 	//Get Systemstring
 	preg_match('!\nSystem(.*?)\n!is',strip_tags($info),$ma);
 	//Check if it consists 'windows' as string
-	preg_match('/[Ww]indows/',$ma[1],$os);
-	if($os[0]=='' && $os[0]==null ) {
-		$thisBoxRunsWindows= false;
-	} else {
+	if (preg_match('/[Ww]indows/',$ma[1])) {
 		$thisBoxRunsWindows = true;
+	} else {
+		$thisBoxRunsWindows= false;
 	}
 	return $thisBoxRunsWindows;
 }
