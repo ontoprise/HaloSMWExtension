fd951ccb6790155e1f93e4f61feb52d404961d9b
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-05-07 13:20:51 +0000

    fix: version hint
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7018 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/SMW_Initialize.php
+++ extensions/SMWHalo/includes/SMW_Initialize.php
@@ -10,7 +10,7 @@
  */
 if ( !defined( 'MEDIAWIKI' ) ) die;
 
-define('SMW_HALO_VERSION', '{{$VERSION}}-for-SMW-1.4.3');
+define('SMW_HALO_VERSION', '{{$VERSION}}-for-SMW-1.5.0');
 
 // constant for special schema properties
 define('SMW_SSP_HAS_DOMAIN_AND_RANGE_HINT', 1);
