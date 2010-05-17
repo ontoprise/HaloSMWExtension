d1ce694280c09198942ce01e55bb6cc9fd3f6b28
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-05-17 14:13:44 +0000

    First version of mapping exceptions
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7074 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/LinkedData/exceptions/LOD_MappingException.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * @file
+ * @ingroup LinkedDataException
+ */
+
+/*  Copyright 2010, ontoprise GmbH
+*   This file is part of the Linked Data Extension.
+*
+*   The Linked Data Extension is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   The Linked Data Extension is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * This file contains the class for all Linked Data Mapping Exceptions i.e. 
+ * LODMappingException.
+ * 
+ * @author Thomas Schweitzer
+ * Date: 12.05.2010
+ * 
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( "This file is part of the HaloACL extension. It is not a valid entry point.\n" );
+}
+
+/**
+ * Exceptions for the operations Linked Data Mappings.
+ *
+ */
+class LODMappingException extends LODException {
+
+	//--- Constants ---
+	
+	// No IO strategy is set in the LODMappingStore.
+	// Parameters:
+	// 	No parameter
+	const NO_IO_STRATEGY_SET = 1;	
+	
+	
+	/**
+	 * Constructor of the group exception.
+	 *
+	 * @param int $code
+	 * 		A user defined error code.
+	 */
+    public function __construct($code = 0) {
+    	$args = func_get_args();
+    	// initialize super class
+        parent::__construct($args);
+    }
+    
+    protected function createMessage($args) {
+    	$msg = "";
+    	switch ($args[0]) {
+    		case self::NO_IO_STRATEGY_SET:
+    			$msg = "Internal error. No IO strategy is set in class LODMappingStore.";
+    			break;
+    	}
+    	return $msg;
+    }
+}
