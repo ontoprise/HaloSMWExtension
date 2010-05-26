50dda7dbe672ec1123ebe5274b281168f1a204db
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-05-26 12:21:36 +0000

    Query interface: all style definitions (except of some "display:none") are now located in the file 'qi.css'.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7112 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/specials/SMWQueryInterface/SMWQueryInterface.php
+++ extensions/SMWHalo/specials/SMWQueryInterface/SMWQueryInterface.php
@@ -68,7 +68,7 @@ class SMWQueryInterface extends SpecialPage {
     }
 
     private function addTabHeaderForQIDefinition() {
-        $html = '<div id="qiDefTab"><table style="border-collapse:collapse;empty-cells:show;width:100%;height:100%;">
+        $html = '<div id="qiDefTab"><table>
                  <tr>
                  <td id="qiDefTab1" class="qiDefTabActive" onclick="qihelper.switchTab(1);"
                      onmouseover="Tip(\'' . wfMsg('smw_qi_tt_treeview') . '\')">'.wfMsg('smw_qi_queryastree').'</td>
@@ -80,10 +80,10 @@ class SMWQueryInterface extends SpecialPage {
                      onmouseover="Tip(\'' . wfMsg('smw_qi_tt_showAsk') . '\')">'.wfMsg('smw_qi_querysource').'</td>
                  </tr>
                  <tr>
-                 <td class="qiDefTabContent" colspan="5" style="width:100%;height:100%">'.
+                 <td class="qiDefTabContent" colspan="5" style="height:100%">'.
                  $this->addTreeView().
-                '<div id="qitextview" style="display: none;height:100%">Query as text</div>
-                 <div id="qisource" style="display: none;height:100%"><textarea id="fullAskText" style="height:98%" onchange="qihelper.sourceChanged=1"></textarea></div>
+                '<div id="qitextview">Query as text</div>
+                 <div id="qisource"><textarea id="fullAskText" onchange="qihelper.sourceChanged=1"></textarea></div>
                  </td></tr></table></div>
         ';
         return $html;
@@ -146,7 +146,7 @@ class SMWQueryInterface extends SpecialPage {
 		return '<div id="querylayout">
 					<div id="layouttitle" onclick="qihelper.switchlayout()" onmouseover="Tip(\'' . wfMsg('smw_qi_tt_qlm') . '\')"><a id="layouttitle-link" class="plusminus" href="javascript:void(0)"></a>' . wfMsg('smw_qi_layout_manager') . '</div>
 					<div id="layoutcontent" style="display:none">
-                        <table summary="Layout Manager for query" style="width:100%">
+                        <table summary="Layout Manager for query">
                             <tr>
         						<td onmouseover="Tip(\'' . wfMsg('smw_qi_tt_format') . '\')">
                 					Format:
@@ -164,7 +164,7 @@ class SMWQueryInterface extends SpecialPage {
                             </tr>
                         </table>
                     </div>
-                    <div id="queryprinteroptions" style="display:block"></div>
+                    <div id="queryprinteroptions"></div>
                 </div>';
 	}
 
@@ -185,7 +185,7 @@ class SMWQueryInterface extends SpecialPage {
 		if (isset($smwgWebserviceEndpoint)) {
 			$useTS = '<input class="btn" type="checkbox" id="usetriplestore">' . wfMsg('smw_qi_usetriplestore') . '</input>';
 		}
-		return '<div id="qimenubar" style="position: relative;">' .
+		return '<div id="qimenubar">' .
 		//'<span class="qibutton" onclick="qihelper.showLoadDialogue()">' . wfMsg('smw_qi_load') . '</span><span style="color:#C0C0C0">&nbsp;|&nbsp;</span>' .
 		//'<span class="qibutton" onclick="qihelper.showSaveDialogue()">' . wfMsg('smw_qi_save') . '</span><span style="color:#C0C0C0">&nbsp;|&nbsp;</span>' .
 		//'<span class="qibutton" onclick="qihelper.exportToXLS()">' . wfMsg('smw_qi_exportXLS') . '</span>' .
@@ -193,7 +193,7 @@ class SMWQueryInterface extends SpecialPage {
 						'<button class="btn" onclick="qihelper.copyToClipboard()" onmouseover="this.className=\'btn btnhov\'; Tip(\'' . wfMsg('smw_qi_tt_clipboard') . '\')" onmouseout="this.className=\'btn\'">' . wfMsg('smw_qi_clipboard') . '</button>'.
 		$buttons.
 		$useTS.
-						'<span style="position:absolute; right:13px;"><button class="btn" onclick="qihelper.resetQuery()" onmouseover="this.className=\'btn btnhov\'; Tip(\'' . wfMsg('smw_qi_tt_reset') . '\')" onmouseout="this.className=\'btn\'">' . wfMsg('smw_qi_reset') . '</button></span>'.
+						'<span><button class="btn" onclick="qihelper.resetQuery()" onmouseover="this.className=\'btn btnhov\'; Tip(\'' . wfMsg('smw_qi_tt_reset') . '\')" onmouseout="this.className=\'btn\'">' . wfMsg('smw_qi_reset') . '</button></span>'.
 					'</div>'.
 
 				'<div id="fullpreviewbox" style="display:none">'.
@@ -206,9 +206,9 @@ class SMWQueryInterface extends SpecialPage {
 						'<span class="qibutton" onclick="qihelper.doReset()">' . wfMsg('smw_qi_confirm') . '</span>&nbsp;<span class="qibutton" onclick="$(\'resetdialogue\', \'shade\').invoke(\'toggle\')">' . wfMsg('smw_qi_cancel') . '</span>' .
 						'</div>'.
 
-				'<div id="showAsk" class="topDialogue" style="display:none; width:350px">' .
-						'<span id="showParserAskButton" class="qibutton" style="font-weight: bold; text-decoration: none; cursor: default;">' . wfMsg('smw_qi_parserask') . '</span><br/><hr/>' .
-						'<div><textarea id="fullAskTextOld" style="width:95%" rows="10" readonly></textarea></div>' .
+				'<div id="showAsk" class="topDialogue" style="display:none;">' .
+						'<span id="showParserAskButton" class="qibutton">' . wfMsg('smw_qi_parserask') . '</span><br/><hr/>' .
+						'<div><textarea id="fullAskTextOld" rows="10" readonly></textarea></div>' .
 						'<span class="qibutton" onclick="$(\'showAsk\', \'shade\').invoke(\'toggle\')">' . wfMsg('smw_qi_close') . '</span>' .
 						'</div>'.
 
