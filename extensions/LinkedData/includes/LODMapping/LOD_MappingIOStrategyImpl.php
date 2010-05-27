f8401df181828df0edecf2c7883ab2dbca4e191d
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-05-27 17:06:58 +0000

    mapping IO strategy
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7132 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/LinkedData/includes/LODMapping/LOD_MappingIOStrategyImpl.php
@@ -0,0 +1,75 @@
+<?php
+
+global $smwgHaloIP;
+require_once($smwgHaloIP."/includes/storage/SMW_RESTWebserviceConnector.php");
+
+class MappingIOStrategyImpl implements ILODMappingIOStrategy {
+
+	private $_client;
+
+	function __construct() {
+		
+		// create 
+		global $smwgWebserviceEndpoint, $smwgWebserviceUser, $smwgWebservicePassword, $smwgWebserviceProtocol;
+
+		if (isset($smwgWebserviceProtocol) && strtolower($smwgWebserviceProtocol) === 'rest') {
+			list($host, $port) = explode(":", $smwgWebserviceEndpoint);
+			$credentials = isset($smwgWebserviceUser) ? $smwgWebserviceUser.":".$smwgWebservicePassword : "";
+			$this->_client = new RESTWebserviceConnector($host, $port, "mapping", $credentials);
+		} else {
+			trigger_error("Mapping endpoint can not be requested by SOAP.");
+			die();
+		}
+
+	}
+
+	public function existsMapping($mappingID) {
+		$payload = "sourceID=".urlencode($mapping->getSourceID())."&targetID=".urlencode($mapping->getTargetID());
+		list($header, $status, $res) = self::$_client->send($payload, "/existsMapping");
+		if ($status != 200) {
+			return false;
+		}
+		return trim($res) == 'true';
+	}
+
+	public function addMapping(LODMapping $mapping) {
+		$payload = "mappingText=".urlencode($mapping->getMappingText());
+		list($header, $status, $res) = self::$_client->send($payload, "/addMapping");
+		if ($status != 200) {
+			return false;
+		}
+		return true;
+	}
+
+	public function getAllMappings($source = null, $target = null) {
+		$payload = "sourceID=".urlencode($mapping->getSourceID())."&targetID=".urlencode($mapping->getTargetID());
+		list($header, $status, $res) = self::$_client->send($payload, "/getAllMappings");
+		if ($status != 200) {
+			return array();
+		}
+		return array(new LODMapping($res, $source, $target));
+	}
+
+	public function removeAllMappings($source = null, $target = null) {
+		$payload = "sourceID=".urlencode($mapping->getSourceID())."&targetID=".urlencode($mapping->getTargetID());
+		list($header, $status, $res) = self::$_client->send($payload, "/removeAllMappings");
+
+	}
+
+	public function getAllSources() {
+		$payload = "";
+		list($header, $status, $res) = self::$_client->send($payload, "/getAllSources");
+		if ($status != 200) {
+			return array();
+		}
+	}
+
+	public function getAllTargets() {
+		$payload = "";
+		list($header, $status, $res) = self::$_client->send($payload, "/getAllTargets");
+		if ($status != 200) {
+			return array();
+		}
+	}
+
+}
