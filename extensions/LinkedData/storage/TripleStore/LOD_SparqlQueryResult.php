b6d015abafc2791093b9aaf8dfed9958c5fb2941
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-06-23 14:16:53 +0000

    metadata support
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7267 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/LinkedData/storage/TripleStore/LOD_SparqlQueryResult.php
+++ extensions/LinkedData/storage/TripleStore/LOD_SparqlQueryResult.php
@@ -5,21 +5,21 @@
  */
 
 /*  Copyright 2010, ontoprise GmbH
-*  This file is part of the LinkedData-Extension.
-*
-*   The LinkedData-Extension is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   The LinkedData-Extension is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *  This file is part of the LinkedData-Extension.
+ *
+ *   The LinkedData-Extension is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The LinkedData-Extension is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * This file contains several classes for managing query results:
@@ -28,36 +28,36 @@
  * - LODSparqlResult        : The abstract base class of a single result value
  * - LODSparqlResultURI     : Representation of a result value which is a URI
  * - LODSparqlResultLiteral : Representation of a result value which is a literal
- * 
+ *
  * @author Thomas Schweitzer
  * Date: 30.04.2010
- * 
+ *
  */
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( "This file is part of the LinkedData extension. It is not a valid entry point.\n" );
 }
 
 /**
- * This class stores a complete query result. It contains the names of the 
+ * This class stores a complete query result. It contains the names of the
  * variables that are bound and a set of (table) rows that contain the actual
  * result values.
- * 
+ *
  * A query result can be imagined as a table with a column for each variable and
  * a row for each binding of these variables.
- * 
+ *
  * @author Thomas Schweitzer
- * 
+ *
  */
 class LODSparqlQueryResult {
-	
+
 	// array<LODSparqlResultRow>
 	// All rows of the result.
 	private $mRows = array();
-	
+
 	// array<string>
 	// The names of all variables in the result
 	private $mVariables = array();
-	
+
 	/**
 	 * Returns all rows of the result.
 	 *
@@ -77,7 +77,7 @@ class LODSparqlQueryResult {
 	 * 		String representation of the expected value
 	 * @return array<LODSparqlResultRow>
 	 * 		An array of rows where the condition is matched. The array is empty
-	 * 		if there are no matching results. 
+	 * 		if there are no matching results.
 	 */
 	public function getRowsWhere($variable, $value) {
 		$result = array();
@@ -89,7 +89,7 @@ class LODSparqlQueryResult {
 		}
 		return $result;
 	}
-	
+
 	/**
 	 * Returns all variables.
 	 *
@@ -99,7 +99,7 @@ class LODSparqlQueryResult {
 	public function getVariables() {
 		return $this->mVariables;
 	}
-	
+
 	/**
 	 * Adds a variable. This function is called from classes that assemble this
 	 * result object.
@@ -110,7 +110,7 @@ class LODSparqlQueryResult {
 	public function addVariable($variable) {
 		$this->mVariables[] = $variable;
 	}
-	
+
 	/**
 	 * Adds a row of results. This function is called from classes that assemble
 	 * this result object.
@@ -121,7 +121,7 @@ class LODSparqlQueryResult {
 	public function addRow(LODSparqlResultRow $row) {
 		$this->mRows[] = $row;
 	}
-	
+
 	/**
 	 * Converts this result to a table i.e. an array of arrays. The outer array
 	 * represents the rows and the inner the values for each column. The columns
@@ -149,15 +149,15 @@ class LODSparqlQueryResult {
 /**
  * This class represents a row in a query result. A row is an array of instances
  * of class LODSparqlResult. The keys in this array are the names of the variables.
- * 
+ *
  * @author Thomas Schweitzer
- * 
+ *
  */
 class LODSparqlResultRow {
 	// array<string => LODSparqlResult>
 	// All results of a row.
 	private $mResults = array();
-	
+
 	/**
 	 * Returns the results of the complete row.
 	 *
@@ -166,24 +166,24 @@ class LODSparqlResultRow {
 	public function getResults() {
 		return $this->mResults;
 	}
-	
+
 	/**
 	 * Returns the result for the given $variable.
 	 *
 	 * @param string $variable
 	 * 		Name of the variable whose value will be returned.
 	 * @return LODSparqlResult
-	 * 		The requested result or 
+	 * 		The requested result or
 	 * 		<null> if the variable is not bound
 	 */
 	public function getResult($variable) {
 		return array_key_exists($variable, $this->mResults)
-				? $this->mResults[$variable]
-				: null;
+		? $this->mResults[$variable]
+		: null;
 	}
-	
+
 	/**
-	 * Adds a result for the given $variable. This function is called from 
+	 * Adds a result for the given $variable. This function is called from
 	 * classes that assemble this result object.
 	 *
 	 * @param string $variable
@@ -200,31 +200,44 @@ class LODSparqlResultRow {
 /**
  * This is the abstract base class for a single result value of a SPARQL query.
  * It consists of a variable name and a value.
- * 
+ *
  * @author Thomas Schweitzer
- * 
+ *
  */
 
 abstract class LODSparqlResult {
 
-	// string: 
+	// string:
 	//Name of the variable that contains the result
 	private $mVariableName;
-	
+
 	// string
 	// The value of the result.
 	private $mValue;
-	
+
+	// metadata hashmap. Maps (pre-defined) metadata properties to values.
+	private $mMetadata;
+
 	// Returns the name of the variable
 	public function getVariableName() {
 		return $this->mVariableName;
 	}
-	
+
 	// Returns the value
 	public function getValue() {
 		return $this->mValue;
 	}
+    
+	// Returns a metadata property value
+	public function getMetadataValue($metadataProperty) {
+		return array_key_exists($metadataProperty, $this->mMetadata[$metadataProperty]) ? $this->mMetadata[$metadataProperty] : NULL;
+	}
 	
+	// Returns metadata hash map
+	public function getMetadata() {
+		return $this->mMetadata;
+	}
+
 	/**
 	 * Creates a new result object. As this class is abstract, it can only be
 	 * called from sub-classes.
@@ -234,9 +247,10 @@ abstract class LODSparqlResult {
 	 * @param string $value
 	 * 		Value of the variable
 	 */
-	protected function __construct($name, $value) {
+	protected function __construct($name, $value, $metadata = array()) {
 		$this->mVariableName = $name;
 		$this->mValue = $value;
+		$this->mMetadata = $metadata;
 	}
 }
 
@@ -245,7 +259,7 @@ abstract class LODSparqlResult {
  *
  */
 class LODSparqlResultURI extends LODSparqlResult{
-	
+
 	/**
 	 * Creates a URI result.
 	 *
@@ -254,9 +268,11 @@ class LODSparqlResultURI extends LODSparqlResult{
 	 * @param string $uri
 	 * 		The URI that is bound by the variable.
 	 */
-	public function __construct($name, $uri) {
-		parent::__construct($name, $uri);
+	public function __construct($name, $uri, $metadata) {
+		parent::__construct($name, $uri, $metadata);
 	}
+
+	
 }
 
 /**
@@ -269,11 +285,11 @@ class LODSparqlResultLiteral extends LODSparqlResult {
 	// The datatype of the literal which is typically a URI like xsd:string or
 	// http://www.w3.org/2001/XMLSchema#string
 	private $mDatatype;
-	
+
 	// string
 	// Language code of the literal e.g. "en" or "de"
 	private $mLanguage;
-	
+
 	/**
 	 * Creates a literal result.
 	 *
@@ -286,10 +302,12 @@ class LODSparqlResultLiteral extends LODSparqlResult {
 	 * @param string $language
 	 * 		Language code of the value.
 	 */
-	public function __construct($name, $value, $datatype = null, $language = null) {
-		parent::__construct($name, $value);
+	public function __construct($name, $value, $datatype = null, $language = null, $metadata = array()) {
+		parent::__construct($name, $value, $metadata);
 		$this->mDatatype = $datatype;
 		$this->mLanguage = $language;
 	}
+	
+   
 }
 
--- extensions/LinkedData/storage/TripleStore/LOD_TripleStoreAccess.php
+++ extensions/LinkedData/storage/TripleStore/LOD_TripleStoreAccess.php
@@ -5,36 +5,36 @@
  */
 
 /*  Copyright 2010, ontoprise GmbH
-*  This file is part of the LinkedData-Extension.
-*
-*   The LinkedData-Extension is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU General Public License as published by
-*   the Free Software Foundation; either version 3 of the License, or
-*   (at your option) any later version.
-*
-*   The LinkedData-Extension is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU General Public License
-*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *  This file is part of the LinkedData-Extension.
+ *
+ *   The LinkedData-Extension is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The LinkedData-Extension is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
  * This file contains the class LODTripleStoreAccess which allows modifying and
  * querying the content of the connected triple store.
- * 
+ *
  * @author Thomas Schweitzer
  * Date: 30.04.2010
- * 
+ *
  */
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( "This file is part of the LinkedData extension. It is not a valid entry point.\n" );
 }
 
- //--- Includes ---
- global $lodgIP;
+//--- Includes ---
+global $lodgIP;
 //require_once("$lodgIP/...");
 
 /**
@@ -44,70 +44,70 @@ if ( !defined( 'MEDIAWIKI' ) ) {
  * - delete triples
  * - insert triples
  * - query the triple store
- * 
+ *
  * All commands that modify the triple store are collected until they are flushed.
- * 
+ *
  * @author Thomas Schweitzer
- * 
+ *
  */
 class  LODTripleStoreAccess  {
-	
+
 	//--- Constants ---
-		
+
 	//--- Private fields ---
-	
+
 	// array<string>:
 	// Each string is a SPARUL command that is executed when the method
 	// "flushCommands()" is called.
 	private $mCommands = array();
-	
+
 	// string
-	// This string contains namespace prefixes like 
-	// PREFIX sd:<http://www.ontoprise.de/sd#> 
+	// This string contains namespace prefixes like
+	// PREFIX sd:<http://www.ontoprise.de/sd#>
 	// The prefixes are added to each command in $mCommands
 	private $mPrefixes = "";
-	
+
 	/**
 	 * Constructor for LODTripleStoreAccess
-	 * 
-	 */		
+	 *
+	 */
 	function __construct() {
 	}
-	
+
 
 	//--- getter/setter ---
-//	public function getXY()           {return $this->mXY;}
+	//	public function getXY()           {return $this->mXY;}
 
-//	public function setXY($xy)               {$this->mXY = $xy;}
-	
+	//	public function setXY($xy)               {$this->mXY = $xy;}
+
 	//--- Public methods ---
-	
+
 	/**
 	 * Adds the string with namespace prefixes to the current prefix string.
-	 * The prefixes are added to each new command that is added in the other 
+	 * The prefixes are added to each new command that is added in the other
 	 * methods e.g. deleteTriples or insertTriples. Consequently, prefixes must
 	 * be set before these methods are called.
 	 *
 	 * @param string $prefixes
 	 * 		One or more prefixes in a string. Each prefix must be of the form:
 	 * 		"PREFIX" prefix_name":<"prefix_uri"> "
-	 * 		e.g. 
-	 * 		PREFIX sd:<http://www.ontoprise.de/sd#> 
+	 * 		e.g.
+	 * 		PREFIX sd:<http://www.ontoprise.de/sd#>
 	 * 		Different prefixes in $prefixes must be separated by spaces.
 	 */
 	public function addPrefixes($prefixes) {
 		$this->mPrefixes .= "$prefixes ";
 	}
-	
+
 	/**
-	 * Creates a graph with the name $graph in the triple store. If the graph 
+	 * Creates a graph with the name $graph in the triple store. If the graph
 	 * already exists, nothing happens.
 	 *
 	 * This method creates a SPARUL command that is collected in this
-	 * instance. Prefixes that have been added with addPrefixes() are added to 
+	 * instance. Prefixes that have been added with addPrefixes() are added to
 	 * the new command.
 	 * Invoke flushCommands() to send all commands to the triple store.
-	 * 
+	 *
 	 * @param string $graph
 	 * 		Name of the new graph.
 	 */
@@ -116,14 +116,14 @@ class  LODTripleStoreAccess  {
 	}
 
 	/**
-	 * Drops the graph with the name $graph in the triple store. If the graph 
+	 * Drops the graph with the name $graph in the triple store. If the graph
 	 * does not exists, nothing happens.
 	 *
 	 * This method creates a SPARUL command that is collected in this
-	 * instance. Prefixes that have been added with addPrefixes() are added to 
+	 * instance. Prefixes that have been added with addPrefixes() are added to
 	 * the new command.
 	 * Invoke flushCommands() to send all commands to the triple store.
-	 * 
+	 *
 	 * @param string $graph
 	 * 		Name of the graph to drop.
 	 */
@@ -133,10 +133,10 @@ class  LODTripleStoreAccess  {
 
 	/**
 	 * Deletes triples in the graph $graph, where the $wherePattern matches with
-	 * the template $deleteTemplate. 
+	 * the template $deleteTemplate.
 	 * (See http://www.w3.org/TR/2009/WD-sparql11-update-20091022/#t514)
 	 * This method creates a SPARUL command that is collected in this
-	 * instance. Prefixes that have been added with addPrefixes() are added to 
+	 * instance. Prefixes that have been added with addPrefixes() are added to
 	 * the new command.
 	 * Invoke flushCommands() to send all commands to the triple store.
 	 *
@@ -146,27 +146,27 @@ class  LODTripleStoreAccess  {
 	 * 		The pattern of the triples that are candidates for the deletion e.g.
 	 * 		"MySubject ?p ?o" selects all triples of "MySubject"
 	 * @param string $deleteTemplate
-	 * 		The delete template is applied to all variable bindings of the 
-	 * 		where-pattern e.g. "YourSubject ?p ?o" deletes all triples of 
+	 * 		The delete template is applied to all variable bindings of the
+	 * 		where-pattern e.g. "YourSubject ?p ?o" deletes all triples of
 	 * 		"YourSubject" with the same predicates and object as "MySubject".
 	 */
 	public function deleteTriples($graph, $wherePattern, $deleteTemplate) {
-		$this->mCommands[] = 
-			$this->mPrefixes
-			."DELETE FROM <$graph> \n"
-			."  { $deleteTemplate } \n"
-			."WHERE\n" 
-			."  { $wherePattern } ";		
+		$this->mCommands[] =
+		$this->mPrefixes
+		."DELETE FROM <$graph> \n"
+		."  { $deleteTemplate } \n"
+		."WHERE\n"
+		."  { $wherePattern } ";
 	}
-	
+
 	/**
 	 * Inserts the given triples $triples into the graph $graph.
 	 * (See http://www.w3.org/TR/2009/WD-sparql11-update-20091022/#t515)
 	 * This method creates a SPARUL command that is collected in this
-	 * instance. Prefixes that have been added with addPrefixes() are added to 
+	 * instance. Prefixes that have been added with addPrefixes() are added to
 	 * the new command.
 	 * Invoke flushCommands() to send all commands to the triple store.
-	 * 
+	 *
 	 * @param string $graph
 	 * 		The name of the graph to which the triples are added.
 	 * @param array<LODTriple> $triples
@@ -177,19 +177,19 @@ class  LODTripleStoreAccess  {
 		foreach ($triples as $t) {
 			$cmds .= $t->toSPARUL()." \n";
 		}
-		
-		$this->mCommands[] = 
-			$this->mPrefixes
-			."INSERT DATA INTO <$graph> \n"
-			."{\n"
-			.$cmds
-			."\n}";
+
+		$this->mCommands[] =
+		$this->mPrefixes
+		."INSERT DATA INTO <$graph> \n"
+		."{\n"
+		.$cmds
+		."\n}";
 	}
-	
+
 	/**
 	 * Loads a file into a graph of the triple store.
 	 * @param string $file
-	 * 		URI of a file that can be accessed by the triple store e.g. 
+	 * 		URI of a file that can be accessed by the triple store e.g.
 	 * 		"http://somepath/somefile.n3" or "file://absolutePath/someFile.n3"
 	 * @param string $graph
 	 * @param string $format
@@ -197,7 +197,7 @@ class  LODTripleStoreAccess  {
 	public function loadFileIntoGraph($file, $graph, $format) {
 		$this->mCommands[] = "LOAD $file?format=$format INTO <$graph>\n";
 	}
-	
+
 	/**
 	 * Sends all collected SPARUL commands to the Triple Store. Afterwards all
 	 * commands are deleted and the definition of prefixes is reset.
@@ -208,7 +208,7 @@ class  LODTripleStoreAccess  {
 	 */
 	public function flushCommands() {
 		try {
-			$con = TSConnection::getConnector(); 
+			$con = TSConnection::getConnector();
 			$con->connect();
 			$con->update("/topic/WIKI.TS.UPDATE", $this->mCommands);
 			$con->disconnect();
@@ -216,14 +216,14 @@ class  LODTripleStoreAccess  {
 			// A soap fault exception occurred => flushing commands failed
 			return false;
 		}
-		
+
 		// Delete the array of commands and all prefixes
 		$this->mCommands = array();
 		$this->mPrefixes = "";
-		 
+			
 		return true;
 	}
-	
+
 	/**
 	 * Sends a query to the Triple Store.
 	 *
@@ -236,13 +236,13 @@ class  LODTripleStoreAccess  {
 	 * 		A string with the parameters for the query in the following format:
 	 * 		parameterName=parameterValue
 	 * 		Several parameters are separated by |
-	 * 
+	 *
 	 * @return LODSparqlQueryResult
 	 * 		The result of the query encapsulated in an object or <null> on failure.
 	 */
 	public function queryTripleStore($query, $graph = "", $params = null) {
 		try {
-			$con = TSConnection::getConnector(); 
+			$con = TSConnection::getConnector();
 			$con->connect();
 			$p = "merge=false";
 			if (isset($params)) {
@@ -254,35 +254,35 @@ class  LODTripleStoreAccess  {
 			// An exception occurred => no result
 			return null;
 		}
-		
+
 		$result = self::parseSparqlXMLResult($result);
 		return $result;
 	}
+	
 
-	
 	//--- Private methods ---
-	
-	
+
+
 	/**
-	 * This method parses the SPARQL XML Result that is returned by a query. 
+	 * This method parses the SPARQL XML Result that is returned by a query.
 	 * The result is returned as instance of LODSparqlQueryResult.
 	 *
 	 * @param string $sparqlXMLResult
-	 * 		A Sparql query result as XML string according to 
+	 * 		A Sparql query result as XML string according to
 	 * 		http://www.w3.org/TR/rdf-sparql-XMLres/
 	 * @return LODSparqlQueryResult
 	 * 		The result in an object oriented structure (which may contain zero
-	 * 		result rows or <null> if the result is invalid. 
-	 */	
+	 * 		result rows or <null> if the result is invalid.
+	 */
 	private function parseSparqlXMLResult($sparqlXMLResult) {
 		$dom = simplexml_load_string($sparqlXMLResult);
 		if ($dom === FALSE) {
 			return null;
 		}
-		
+
 		// Store results in a query result object
 		$queryResult = new LODSparqlQueryResult();
-		
+
 		// add variables to the query result
 		$variables = $dom->xpath('//variable');
 		foreach ($variables as $v) {
@@ -290,10 +290,10 @@ class  LODTripleStoreAccess  {
 			$name = (string) $attrib['name'];
 			$queryResult->addVariable($name);
 		}
-		
+
 		// add all rows to the query result
 		$results = $dom->xpath('//result');
-		
+
 		foreach ($results as $r) {
 			$binding = $r->binding;
 			$row = new LODSparqlResultRow();
@@ -305,22 +305,36 @@ class  LODTripleStoreAccess  {
 				$attrib = $b->attributes();
 				$name = (string) $attrib['name'];
 				if (isset($b->uri)) {
-					$row->addResult($name, new LODSparqlResultURI($name, (string) $b->uri));
+					$metadata = array();
+					foreach($b->uri->attributes() as $mdProperty => $mdValue) {
+						if (strpos($mdProperty, "_meta_") === 0) {
+							$metadata[substr($mdProperty,6)] = $mdValue;
+						}
+					}
+				
+					$row->addResult($name, new LODSparqlResultURI($name, (string) $b->uri, $metadata));
 				} else if (isset($b->literal)) {
 					$attrib = $b->literal->attributes();
-					$datatype = isset($attrib['datatype']) 
-									? (string) $attrib['datatype'] : null;
-					$lang     = isset($attrib['xml:lang']) 
-									? (string) $attrib['xml:lang'] : null;
-					$row->addResult($name, new LODSparqlResultLiteral($name, (string) $b->literal, $datatype, $lang));
+					$datatype = isset($attrib['datatype'])
+					? (string) $attrib['datatype'] : null;
+					$lang     = isset($attrib['xml:lang'])
+					? (string) $attrib['xml:lang'] : null;
+						
+					$metadata = array();
+					foreach($b->literal->attributes() as $mdProperty => $mdValue) {
+						if (strpos($mdProperty, "_meta_") === 0) {
+							$metadata[substr($mdProperty,6)] = $mdValue;
+						}
+					}
+					$row->addResult($name, new LODSparqlResultLiteral($name, (string) $b->literal, $datatype, $lang, $metadata));
 				}
 			}
 			$queryResult->addRow($row);
 		}
-		
+
 		return $queryResult;
-		
+
 	}
-	
+
 }
 
