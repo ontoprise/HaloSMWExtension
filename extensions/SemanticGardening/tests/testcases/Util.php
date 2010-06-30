85d481105a14e598c70d401912736d52380a2c40
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-06-30 12:53:08 +0000

    fixed pattern match to examine the os.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7316 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SemanticGardening/tests/testcases/Util.php
+++ extensions/SemanticGardening/tests/testcases/Util.php
@@ -11,8 +11,7 @@ function isWindows() {
     //Get Systemstring
     preg_match('!\nSystem(.*?)\n!is',strip_tags($info),$ma);
     //Check if it consists 'windows' as string
-    preg_match('/[Ww]indows/',$ma[1],$os);
-    if($os[0]=='' && $os[0]==null ) {
+    if(preg_match('/[Ww]indows/',$ma[1])) {
         $thisBoxRunsWindows= false;
     } else {
         $thisBoxRunsWindows = true;
