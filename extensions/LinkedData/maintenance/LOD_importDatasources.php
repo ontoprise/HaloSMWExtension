f924d6039ededb3b61acb2df221644653336deb3
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-07-08 13:35:36 +0000

    update LOD databases
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7421 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/LinkedData/maintenance/LOD_importDatasources.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @file
+ * @ingroup LOD_Maintenance
+ */
+
+/*  Copyright 2010, ontoprise GmbH
+*  This file is part of the LinkedData-Extension.
+*
+*   The LinkedData-Extension is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   The LinkedData-Extension is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Maintenance script for updating registered data sources.
+ * 
+ * @author Kai Kuehn
+ * Date: 05.07.2010
+ * 
+ */
+if (array_key_exists('SERVER_NAME', $_SERVER) && $_SERVER['SERVER_NAME'] != NULL) {
+    echo "Invalid access! A maintenance script MUST NOT accessed from remote.";
+    return;
+}
+
+$mediaWikiLocation = dirname(__FILE__) . '/../../..';
+require_once "$mediaWikiLocation/maintenance/commandLine.inc";
+$dir = dirname(__FILE__);
+$lodgIP = "$dir/../../LinkedData";
+
+
+require_once("$lodgIP/includes/LOD_Storage.php");
+require_once("$lodgIP/includes/LOD_GlobalFunctions.php");
+require_once("$smwgHaloIP/includes/storage/SMW_RESTWebserviceConnector.php");
+
+global $smwgWebserviceEndpoint;
+
+$ids = LODAdministrationStore::getInstance()->getAllSourceDefinitionIDs();
+list($host, $port) = explode(":", $smwgWebserviceEndpoint);
+
+//TODO: add credentials
+$con = new RESTWebserviceConnector($host, $port, "ldimporter");
+
+foreach($ids as $id) {
+    $sd = LODAdministrationStore::getInstance()->loadSourceDefinition($id);
+    print "Updating ".$sd->getLabel()." [$id] ...";
+    $payload = "dataSourceId=$id&update=false";
+    list($header, $status, $res) = $con->send($payload, "/runImport");
+    print "done.\n";
+    print "\nStatus: $status";
+    print "\nResult: $res";
+}
+
