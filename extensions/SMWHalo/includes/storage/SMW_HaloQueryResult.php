e356ad5c2fd5ad756f0e0e1915d9e83f734c7cfe
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-07-12 09:56:52 +0000

    Adapted the names of the class's fields (e.g. m_query => mQuery)
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7473 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/storage/SMW_HaloQueryResult.php
+++ extensions/SMWHalo/includes/storage/SMW_HaloQueryResult.php
@@ -20,8 +20,8 @@ class SMWHaloQueryResult extends SMWQueryResult {
      * advance the internal pointer.
      */
     public function getNext() {
-        $row = current($this->m_results);
-    	next($this->m_results);
+        $row = current($this->mResults);
+    	next($this->mResults);
         if ($row === false) return false;
        
         return $row;
@@ -29,15 +29,15 @@ class SMWHaloQueryResult extends SMWQueryResult {
     
 	public function getQueryLink($caption = false) {
 
-		$params = array(trim($this->m_query->getQueryString()));
-        foreach ($this->m_query->getExtraPrintouts() as $printout) {
+		$params = array(trim($this->mQuery->getQueryString()));
+        foreach ($this->mQuery->getExtraPrintouts() as $printout) {
             $params[] = $printout->getSerialisation();
         }
-        if ( count($this->m_query->sortkeys)>0 ) {
+        if ( count($this->mQuery->sortkeys)>0 ) {
             $psort  = '';
             $porder = '';
             $first = true;
-            foreach ( $this->m_query->sortkeys as $sortkey => $order ) {
+            foreach ( $this->mQuery->sortkeys as $sortkey => $order ) {
                 if ( $first ) {
                     $first = false;
                 } else {
@@ -56,7 +56,7 @@ class SMWHaloQueryResult extends SMWQueryResult {
             wfLoadExtensionMessages('SemanticMediaWiki');
             $caption = ' ' . wfMsgForContent('smw_iq_moreresults'); // the space is right here, not in the QPs!
         }
-        $askPage = $this->m_query instanceof SMWSPARQLQuery ? "AskTSC" : "Ask";
+        $askPage = $this->mQuery instanceof SMWSPARQLQuery ? "AskTSC" : "Ask";
         $result = SMWInfolink::newInternalLink($caption,':Special:'.$askPage, false, $params);
        	
 		// Note: the initial : prevents SMW from reparsing :: in the query string
@@ -70,13 +70,17 @@ class SMWHaloQueryResult extends SMWQueryResult {
  * Subclass is required to pre-set content for Halo result sets. 
  * They can not be loaded on demand. 
  * 
- * @author Kai Kühn
+ * @author Kai Kï¿½hn
  *
  */
 class SMWHaloResultArray extends SMWResultArray {
     public function SMWHaloResultArray(SMWWikiPageValue $resultpage, SMWPrintRequest $printrequest, SMWStore $store, $results) {
         parent::__construct($resultpage, $printrequest, $store);
-        $this->m_content = $results; // do not reload
+        $this->mContent = $results; // do not reload
+    }
+    
+    public function setContent($content) {
+    	$this->mContent = $content;
     }
     
     
