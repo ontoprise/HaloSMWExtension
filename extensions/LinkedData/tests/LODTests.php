25e7cf0e90c0aacdfa658d3a809256eeb4088aa2
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-07-16 08:14:04 +0000

    Tests for Mapping Language API require PHP 5.3
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7547 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/LinkedData/tests/LODTests.php
+++ extensions/LinkedData/tests/LODTests.php
@@ -11,7 +11,7 @@ require_once 'testcases/TestTripleStoreAccess.php';
 require_once 'testcases/TestMapping.php';
 require_once 'testcases/TestSparqlDataspaceRewriter.php';
 require_once 'testcases/TestOntologyBrowserSparql.php';
-require_once 'testcases/TestMappingLanguageAPI.php';
+//Requires PHP 5.3 require_once 'testcases/TestMappingLanguageAPI.php';
 
 class LODTests
 { 
@@ -29,7 +29,7 @@ class LODTests
         $suite->addTestSuite("TestMapping");
         $suite->addTestSuite("TestSparqlDataspaceRewriter");
         $suite->addTestSuite("TestOntologyBrowserSparql");
-        $suite->addTestSuite("TestMappingLanguageAPI");
+//        $suite->addTestSuite("TestMappingLanguageAPI");
 
         return $suite;
     }
