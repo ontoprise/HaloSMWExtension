9b97fcc73710786b429ed90324556f138da1dbfb
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-07-23 15:19:46 +0000

    added buildnumber for SMWHalo extension
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@7717 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- buildtools/replaceVersionHint.php
+++ buildtools/replaceVersionHint.php
@@ -15,6 +15,7 @@ require_once("../../../deployment/tools/smwadmin/DF_Tools.php");
 array_shift($argv);
 $dd_file = reset( $argv );
 $file_to_replaceHints = next($argv);
+$buildnumber = next($argv);
 
 $dd = new DeployDescriptor(file_get_contents($dd_file));
 $version = addSeparatorsForVersionNumber($dd->getVersion());
@@ -22,6 +23,7 @@ $patchlevel = $dd->getPatchlevel();
 
 $content = file_get_contents($file_to_replaceHints);
 $content = str_replace('{{$VERSION}}', $version."_".$patchlevel, $content);
+$content = str_replace('{{$BUILDNUMBER}}', $buildnumber, $content);
 $handle = fopen($file_to_replaceHints, "w");
 fwrite($handle, $content);
 fclose($handle);
