b80d3f98d96ec03c40a21f79c70ffe5adfc98c0d
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-09-27 17:08:48 +0000

    add empty database for Mediawiki 1.16.0 and use this instead of the older version
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8221 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- tests/tests_halo/init.php
+++ tests/tests_halo/init.php
@@ -41,7 +41,7 @@ tstInsertLocalSettings($testDir);
 echo "\ndone!\n";
 
 require_once( $mw_dir.'maintenance/commandLine.inc' );
-echo "\nInitializing database for use with MW 1.15 ...";
+echo "\nInitializing database for use with MW 1.16 ...";
 tstInitializeDatabase();
 echo "\ndone!\n";
 
@@ -59,9 +59,9 @@ function tstInitializeDatabase() {
 
 	// Import empty
 	echo "\nImporting database...";
-	echo "$mysqlExe -u $wgDBuser --password=$wgDBpassword < \"$mw_dir"."tests/tests_halo/mw15_db.sql\"";
+	echo "$mysqlExe -u $wgDBuser --password=$wgDBpassword < \"$mw_dir"."tests/tests_halo/mw16_db.sql\"";
 	//exec("\"$xamppDir/mysql/bin/mysql.exe\" -u $wgDBuser --password=$wgDBpassword < \"$mw_dir"."tests/tests_halo/mw15_db.sql\"");
-	runProcess("$mysqlExe -u $wgDBuser --password=$wgDBpassword < \"$mw_dir"."tests/tests_halo/mw15_db.sql\"");
+	runProcess("$mysqlExe -u $wgDBuser --password=$wgDBpassword < \"$mw_dir"."tests/tests_halo/mw16_db.sql\"");
 	echo "\ndone.\n";
 
 	// run setups
