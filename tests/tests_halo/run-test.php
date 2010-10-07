1ec22e4bf6ad6d4b558f1e5c0a8b366c77749c2f
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-10-07 17:07:24 +0000

    changes in including PHPUnit after upgrade to 3.5.0
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8354 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- tests/tests_halo/run-test.php
+++ tests/tests_halo/run-test.php
@@ -2,6 +2,7 @@
 
 require_once( dirname(__FILE__) . '/../../maintenance/commandLine.inc' );
 ini_set( 'include_path', get_include_path() . PATH_SEPARATOR . /*$_SERVER['PHP_PEAR_INSTALL_DIR']*/ 'C:\php\pear' );
-require( 'PHPUnit/TextUI/Command.php' );
+require_once( 'PHPUnit/Framework.php' );
+require_once( 'PHPUnit/Autoload.php' );
 PHPUnit_TextUI_Command::main(true);
 
