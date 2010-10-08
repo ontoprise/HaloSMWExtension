a12210dfab3d8c4f298fb5afc303008cad690407
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-10-08 10:01:47 +0000

    remove unneccessary include of PHPUnit Framework.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8364 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- tests/tests_halo/run-test.php
+++ tests/tests_halo/run-test.php
@@ -2,7 +2,6 @@
 
 require_once( dirname(__FILE__) . '/../../maintenance/commandLine.inc' );
 ini_set( 'include_path', get_include_path() . PATH_SEPARATOR . /*$_SERVER['PHP_PEAR_INSTALL_DIR']*/ 'C:\php\pear' );
-require_once( 'PHPUnit/Framework.php' );
 require_once( 'PHPUnit/Autoload.php' );
 PHPUnit_TextUI_Command::main(true);
 
