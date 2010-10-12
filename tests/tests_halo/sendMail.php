16698875e296fd10b0429a84e148a25a6e68faa2
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-10-12 13:36:07 +0000

    send mail report for webtests
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8415 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ tests/tests_halo/sendMail.php
@@ -0,0 +1,34 @@
+<?php
+
+/* Send mail to someone
+ *
+ * Usage:
+ *
+ *  php sendMail.php -t <recipient email address> [ -s <subject> ]
+ *                   [ -m <mail text> | -f <file with mail text> ]
+ *
+ */
+
+while ($arg = array_shift($argv)) {
+    if ($arg == '-t')
+        $to = array_shift($argv) or die ("Error: missing value for -t\n");
+    else if ($arg == '-s')
+        $subject = array_shift($argv) or die ("Error: missing value for -s\n");
+    else if ($arg == '-m')
+        $message = array_shift($argv) or die ("Error: missing value for -m\n");
+    else if ($arg == '-f') {
+        $filename = array_shift($argv) or die ("Error: missing value for -f\n");
+        if (!file_exists($filename)) die ("Error: file doesn't exist\n");
+        $message = file_get_contents($filename);
+    }
+}
+if (!isset($to)) die ("Error: no recipient given whom to send mail to\n");
+if (!isset($subject)) $subject = "";
+if (!isset($message)) $message = "";
+if (strlen($message) == 0 && strlen($subject) == 0)
+    die ("Error: empty subject and mail body\n");
+
+$headers = 'From: Hudson Buildserver <robotta@ontoprise.de>';
+
+mail($to, $subject, $message, $headers);
+
