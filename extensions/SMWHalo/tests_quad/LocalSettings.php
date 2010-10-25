93dfd580a613bab3c5b2a76d3fe1517000d341a8
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-10-25 13:12:08 +0000

    move quadstore from port 8091 to 8092
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8522 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests_quad/LocalSettings.php
+++ extensions/SMWHalo/tests_quad/LocalSettings.php
@@ -10,7 +10,7 @@ enableSemantics('http://wiki', true);
  
 include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
 enableSMWHalo('SMWHaloStore2', 'SMWTripleStoreQuad', 'http://mywiki');
-$smwgWebserviceEndpoint="localhost:8091";
+$smwgWebserviceEndpoint="localhost:8092";
 $smwhgAutoCompletionTSC=true;
 enableQueryResultsCache();
 
