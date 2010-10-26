411b6eeaf9f2bcd94b8992319e5241b54db77ba1
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-10-26 08:07:36 +0000

    scrptmanager is already loaded before
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8527 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWUserManual/tests/LocalSettings.php
+++ extensions/SMWUserManual/tests/LocalSettings.php
@@ -1,8 +1,5 @@
 <?php
 
-# Scriptmanager needed for Halo extensions
-include_once('extensions/ScriptManager/SM_Initialize.php');
-
 #Import SMW, SMWHalo
 include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
 enableSemantics('http://wiki', true);
