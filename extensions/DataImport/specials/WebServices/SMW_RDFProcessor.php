2b140b8f8e9a955e6671e6c6d69bd4c5be02ff5c
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-10-28 11:19:05 +0000

    removed arc from data import extension
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8569 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/DataImport/specials/WebServices/SMW_RDFProcessor.php
+++ extensions/DataImport/specials/WebServices/SMW_RDFProcessor.php
@@ -2,38 +2,39 @@
 /**
  * @file
  * @ingroup DIWebServices
- * 
+ *
  * @author Ingo Steinbauer
  */
 
-if(!class_exists('ARC2')){
-	//this is necessary, because other extensions also are shipped with that library
-	global $smwgDIIP;
-	require_once("$smwgDIIP/libs/arc/ARC2.php");
-}
+//Outcommented because this is now done by ARCLibrary extension
+//if(!class_exists('ARC2')){
+//	//this is necessary, because other extensions also are shipped with that library
+//	global $smwgDIIP;
+//	require_once("$smwgDIIP/libs/arc/ARC2.php");
+//}
 
 /**
  * This class provides a wrapper for the ARC2 library
  * which is used by SMWWebService to extract result parts
  * from serialized RDF
- * 
+ *
  * @author Ingo Steinbauer
  *
  */
 class SMWRDFProcessor {
-	
+
 	private static $instance;
-	
+
 	/*
 	 * singleton
 	 */
-		public static function getInstance(){
+	public static function getInstance(){
 		if(is_null(self::$instance)){
 			self::$instance = new self();
 		}
-		return self::$instance; 
+		return self::$instance;
 	}
-	
+
 	private $index;
 	private $processedIndex = array();
 	private $subject;
@@ -41,12 +42,12 @@ class SMWRDFProcessor {
 	private $allPropertysRequested = false; //user has used the DI_ALL_PROPERTIES property
 	private $allObjectsRequested = false; //user has used the DI_ALL_OBJECTS property
 	private $language;
-	
-	
-	
+
+
+
 	/**
 	 * Parse the WS result with the ARC2 library
-	 * 
+	 *
 	 * @param unknown_type $uri
 	 * @param unknown_type $subject
 	 * @param unknown_type $content
@@ -58,32 +59,32 @@ class SMWRDFProcessor {
 		$parser = ARC2::$parserName();
 		$parser->parse($uri, $content);
 		$this->index = $parser->getSimpleIndex(false);
-		
+
 		$this->subject = $subject;
 		$this->language = $language;
-		
+
 		$this->processedIndex = array();
 		$this->allPropertysRequested = false;
 		$this->allObjectsRequested = false;
 	}
-	
+
 	/**
 	 * Add namespace prefixes which will be used when
 	 * processing propertys
-	 * 
+	 *
 	 * @param $nsp
 	 * @return unknown_type
 	 */
 	public function setNamespacePrefixes($nsp){
 		$this->namespacePrefixes = $nsp;
 	}
-	
+
 	/**
 	 * Replace namespace prefixes with URIs
-	 * 
+	 *
 	 * @param $uri
 	 * @return unknown_type
-	 */ 
+	 */
 	private function resolveNamespacePrefix($uri){
 		foreach($this->namespacePrefixes as $nsp){
 			if(strpos($uri, "".$nsp['prefix'].":") === 0){
@@ -92,7 +93,7 @@ class SMWRDFProcessor {
 		}
 		return $uri;
 	}
-	
+
 	private function getRequestedLanguage($property){
 		$lang = $this->language;
 		if(strpos($property, "@") > 0){
@@ -101,24 +102,24 @@ class SMWRDFProcessor {
 		}
 		return array($lang, $property);
 	}
-		
+
 	/**
 	 * This method is called by SMWWebService
 	 * when processing result parts. It updates the internal
 	 * processed index, which can be accessed by
-	 * SMWWebService after all propertys have 
+	 * SMWWebService after all propertys have
 	 * been preprocessed.
-	 * 
+	 *
 	 * @param unknown_type $property
 	 * @return unknown_type
 	 */
 	public function preprocessProperty($property){
 		list($lang, $property) = $this->getRequestedLanguage($property);
 		$property = $this->resolveNamespacePrefix($property);
-		
+
 		if(strlen($this->subject) > 0){ //user has chosen to retrieve triples for a certain subject
 			$subject = $this->resolveNamespacePrefix($this->subject);
-			
+				
 			if(array_key_exists($subject, $this->index)){
 				if($property == DI_ALL_SUBJECTS){
 					if(array_key_exists($subject, $this->index)){
@@ -135,16 +136,16 @@ class SMWRDFProcessor {
 					foreach(array_keys($this->index[$subject]) as $property){
 						if(!array_key_exists($property, $this->processedIndex[$subject])){
 							$this->processedIndex[$subject][$property] = array();
-						}	
+						}
 					}
 				} else if($property == DI_ALL_OBJECTS){
 					$this->allObjectsRequested = true;
 					foreach($this->index[$subject] as $propertyId => $property){
 						$processedObjects = array();
 						foreach($property as $object){
-							if(array_key_exists('lang', $object)){ 
+							if(array_key_exists('lang', $object)){
 								if($object['lang'] == $lang || strlen($lang) == 0){
-									$processedObjects[] = $object['value']; 
+									$processedObjects[] = $object['value'];
 								}
 							} else {
 								$processedObjects[] = $object['value'];
@@ -152,13 +153,13 @@ class SMWRDFProcessor {
 						}
 						$this->processedIndex[$subject][$propertyId] = $processedObjects;
 					}
-				} else if(array_key_exists($property, $this->index[$subject])){		
+				} else if(array_key_exists($property, $this->index[$subject])){
 					$processedObjects = array();
-					
+						
 					foreach($this->index[$subject][$property] as $object){
-						if(array_key_exists('lang', $object)){ 
+						if(array_key_exists('lang', $object)){
 							if($object['lang'] == $lang || strlen($lang) == 0){
-								$processedObjects[] = $object['value']; 
+								$processedObjects[] = $object['value'];
 							}
 						} else {
 							$processedObjects[] = $object['value'];
@@ -173,7 +174,7 @@ class SMWRDFProcessor {
 					if(!array_key_exists($subjectId, $this->processedIndex)){
 						$this->processedIndex[$subjectId] = array();
 					}
-				}		
+				}
 			} else { // property != DI_ALL_SUBJECTS
 				foreach($this->index as $subjectId => $subject){
 					if($property == DI_ALL_PROPERTIES){
@@ -188,50 +189,50 @@ class SMWRDFProcessor {
 						foreach($subject as $propertyId => $objects){
 							$processedObjects = array();
 							foreach($objects as $object){
-								if(array_key_exists('lang', $object)){ 
+								if(array_key_exists('lang', $object)){
 									if($object['lang'] == $lang || strlen($lang) == 0){
-										$processedObjects[] = $object['value']; 
+										$processedObjects[] = $object['value'];
 									}
 								} else {
 									$processedObjects[] = $object['value'];
 								}
 							}
-							
+								
 							$this->processedIndex[$subjectId][$propertyId] = $processedObjects;
 						}
 					} else if(array_key_exists($property, $subject)){
-						
+
 						$processedObjects = array();
 						foreach($subject[$property] as $object){
-							if(array_key_exists('lang', $object)){ 
+							if(array_key_exists('lang', $object)){
 								if($object['lang'] == $lang || strlen($lang) == 0){
-									$processedObjects[] = $object['value']; 
+									$processedObjects[] = $object['value'];
 								}
 							} else {
 								$processedObjects[] = $object['value'];
 							}
 						}
-						$this->processedIndex[$subjectId][$property] = $processedObjects; 
-					} 
+						$this->processedIndex[$subjectId][$property] = $processedObjects;
+					}
 				}
 			}
 		}
-		
+
 		return DI_RDF_POSTPROCESS_REQUIRED;
 	}
-	
+
 	/**
 	 * This method is called by SMWWebService
 	 * after all propertys have been been Preprocessed
 	 * in order to get the final result part values.
-	 * 
+	 *
 	 * @param unknown_type $property
 	 * @return unknown_type
 	 */
 	public function getFinalResult($property){
 		list($lang, $property) = $this->getRequestedLanguage($property);
 		$requestProperty = $this->resolveNamespacePrefix($property);
-		
+
 		$result = array();
 		foreach($this->processedIndex as $subjectId => $subject){
 			if(count($subject) == 0){ //a subject with no associated triples
@@ -246,14 +247,14 @@ class SMWRDFProcessor {
 				if(!$this->allPropertysRequested && !$this->allObjectsRequested){ //all propertys was not requested => do not add spacers for them
 					//add spacers if some propertys have several values
 					$maxObjects = 1;
-					$objects = array();							
+					$objects = array();
 					foreach($subject as $propertyId => $property){
 						$maxObjects = max($maxObjects, count($property));
 						if($propertyId == $requestProperty){
 							$objects = $property;
 						}
 					}
-					
+						
 					if($requestProperty == DI_ALL_SUBJECTS){
 						for($i=0; $i < $maxObjects; $i++){
 							$result[] = $subjectId;
@@ -267,7 +268,7 @@ class SMWRDFProcessor {
 							}
 						}
 					}
-				} else { //all propertys were requested	
+				} else { //all propertys were requested
 					foreach($subject as $propertyId => $property){
 						if(count($property) == 0){ //no values for that property exist
 							if($requestProperty == DI_ALL_SUBJECTS){
@@ -298,10 +299,10 @@ class SMWRDFProcessor {
 				}
 			}
 		}
-		
+
 		return $result;
 	}
-	
-	
-	
+
+
+
 }
