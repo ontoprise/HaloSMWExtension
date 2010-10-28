b05426b5a39996ebef3b874873fac36312249e0a
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-10-28 16:36:51 +0000

    Added new tables and functions for rating feature
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8588 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/LinkedData/storage/LOD_StorageSQL.php
+++ extensions/LinkedData/storage/LOD_StorageSQL.php
@@ -41,8 +41,11 @@ require_once $lodgIP . '/storage/LOD_DBHelper.php';
  */
 class LODStorageSQL {
 	
-	const TRIPLE_PERSISTENCE_TABLE = "lod_triple_persistence"; 
-	const MAPPING_PERSISTENCE_TABLE = "lod_mapping_persistence"; 
+	const TRIPLE_PERSISTENCE_TABLE	= "lod_triple_persistence"; 
+	const MAPPING_PERSISTENCE_TABLE	= "lod_mapping_persistence"; 
+	const QUERY_TABLE				= "lod_rating_query"; 
+	const QUERY_RESULT_TABLE		= "lod_rating_query_result"; 
+	
 	
 	/**
 	 * Initializes the database tables of the Linked Data extensions.
@@ -80,7 +83,30 @@ class LODStorageSQL {
 	            'component'		=> 'VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_bin',
 	            'triples' 		=> 'Text CHARACTER SET utf8 COLLATE utf8_bin'),
 				$db, $verbose);
-		
+				
+		// lod_rating_query:
+		//		Stores queries in an article for the rating feature
+		$table = $db->tableName(self::QUERY_TABLE);
+
+		LODDBHelper::setupTable($table, array(
+				'query_id'	=> 'INT(8) UNSIGNED NOT NULL AUTO_INCREMENT',
+	            'query' 		=> 'Text CHARACTER SET utf8 COLLATE utf8_bin',
+	            'article_name'	=> 'VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin'),
+				$db, $verbose, 'query_id');
+				
+		// lod_rating_query_result:
+		//		Stores the result of queries in an article for the rating feature
+		$table = $db->tableName(self::QUERY_RESULT_TABLE);
+
+		LODDBHelper::setupTable($table, array(
+				'query_id'	=> 'INT(8) UNSIGNED NOT NULL',
+				'row'		=> 'INT(8) UNSIGNED NOT NULL',
+	            'variable'	=> 'VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_bin',
+	            'value' 	=> 'Text CHARACTER SET utf8 COLLATE utf8_bin',
+	            'datatype'	=> 'VARCHAR(128) CHARACTER SET utf8 COLLATE utf8_bin'),
+				$db, $verbose, 'query_id, row, variable');
+				
+				
 		LODDBHelper::reportProgress("   ... done!\n",$verbose);
 		
 		print "   ... done!\n";
@@ -102,7 +128,9 @@ class LODStorageSQL {
 		$db =& wfGetDB( DB_MASTER );
 		$tables = array(
 			self::MAPPING_PERSISTENCE_TABLE,
-			self::TRIPLE_PERSISTENCE_TABLE);
+			self::TRIPLE_PERSISTENCE_TABLE,
+			self::QUERY_TABLE.
+			self::QUERY_RESULT_TABLE);
 		foreach ($tables as $table) {
 			$name = $db->tableName($table);
 			$db->query('DROP TABLE' . ($wgDBtype=='postgres'?'':' IF EXISTS'). $name, 'LODStorageSQL::dropDatabaseTables');
@@ -112,6 +140,12 @@ class LODStorageSQL {
 		print("All data removed successfully.\n");
 	}
 
+	
+	/*
+	 * Functions for the persistency layer of the triple store. 
+	 * 
+	 */
+	
 	/**
 	 * Persists all triples given in $trig for the component $component with the
 	 * ID $tripleSetID.
@@ -203,4 +237,170 @@ class LODStorageSQL {
 		$db->delete($db->tableName(self::TRIPLE_PERSISTENCE_TABLE), "*");
 		
 	}
+	
+	
+	/*
+	 * Functions for storing queries and their results for the rating feature.
+	 * 
+	 */
+	
+	/**
+	 * Stores a query and the article where it is defined. Returns an ID for that
+	 * query.
+	 * 
+	 * @param string $queryString
+	 * 		The query to store.
+	 * @param string $articleName
+	 * 		The full name of the article (i.e. with namespace) where the query 
+	 * 		is defined.
+	 * @return int
+	 * 		Each query that is stored with this function gets a unique ID. 
+	 * @throws DBError
+	 * 		... when a database error occurs
+	 */
+	public function addQuery($queryString, $articleName) {
+		$db = & wfGetDB( DB_MASTER );
+		$t = $db->tableName(self::QUERY_TABLE);
+
+		$setValues = array(
+            'query'			=> $queryString,
+            'article_name'	=> $articleName);
+		
+		$db->insert($t, $setValues);
+		$queryID = $db->insertID();
+		
+		return $queryID;
+	}
+	
+	/**
+	 * Retrieves the content of a query by the given $queryID.
+	 * 
+	 * @param int $queryID
+	 * 		ID of the query to retrieve
+	 * @return string / null
+	 * 		The content of the query of <null> if it does not exist.
+	 */
+	public function getQueryByID($queryID) {
+		$db = & wfGetDB( DB_SLAVE );
+
+		$cond = array("query_id" => $queryID);
+		$res = $db->select($db->tableName(self::QUERY_TABLE), array("query"), $cond);
+										   
+		$row = $db->fetchObject($res);
+		$query = $row === false ? null : $row->query;
+		$db->freeResult($res);
+										   
+		return $query;
+		
+	}
+	
+	/**
+	 * Deletes all queries and their results that were store for the article
+	 * with the given name.
+	 * @param string $articleName
+	 * 		Name of the article that contains the queries.
+	 */
+	public function deleteQueries($articleName) {
+		$db = & wfGetDB( DB_MASTER );
+		$t = $db->tableName(self::QUERY_TABLE);
+		
+		$this->deleteQueryResults($articleName);
+
+		$condition = array("article_name" => $articleName);
+		$db->delete($t, $condition);
+		
+	}
+	
+	/**
+	 * Stores a result row of the query with the ID $queryID. A result row is 
+	 * stored with the 0-based index of the row, the variable in the query, the 
+	 * value and the data type
+	 * @param int $queryID
+	 * 		ID of the query to which the result belongs
+	 * @param int $row
+	 * 		The row where the variable binding occurs
+	 * @param string $variable
+	 * 		The variable that is bound to the value
+	 * @param string $value
+	 * 		The value of the variable
+	 * @param string $datatype
+	 * 		The data type of the value
+	 * @throws DBError
+	 * 		... when a database error occurs
+	 */
+	public function storeQueryResultRow($queryID, $row, $variable, $value, $datatype) {
+		$db = & wfGetDB( DB_MASTER );
+		$t = $db->tableName(self::QUERY_RESULT_TABLE);
+
+		$setValues = array(
+            'query_id'	=> $queryID,
+            'row'		=> $row,
+            'variable'	=> $variable,
+            'value'		=> $value,
+            'datatype'	=> $datatype
+		);
+		
+		$db->insert($t, $setValues);
+		
+	}
+	
+	/**
+	 * Reads a complete result row from the database with all variable bindings.
+	 * 
+	 * @param int $queryID
+	 * 		ID of the query.
+	 * @param int $row
+	 * 		0-based index of the row that contains the variable bindings
+	 * @return array / null
+	 * 		<null> if the $row is invalid
+	 * 		otherwise:
+	 * 		array(string variable name => array(string value, string datatype)) 
+	 */
+	public function readQueryResultRow($queryID, $row){
+		$db = & wfGetDB( DB_SLAVE );
+
+		$cond = array("query_id"	=> $queryID,
+					  "row"			=> $row);
+		$res = $db->select($db->tableName(self::QUERY_RESULT_TABLE), 
+											array("variable", "value", "datatype"), 
+											$cond);
+		$result = $res->numRows() === 0 ? null : array();					   
+		while ($row = $db->fetchObject($res)) {
+			$result[$row->variable] = array($row->value, $row->datatype);
+		}
+		$db->freeResult($res);
+										   
+		return $result;
+		
+	}
+	
+	/**
+	 * Deletes the results of all queries that are defined in the article
+	 * with the given name.
+	 *  
+	 * @param $articleName
+	 * 		Name of the article that contains the queries whose results will
+	 * 		be deleted.
+	 */
+	public function deleteQueryResults($articleName) {
+		// Find the IDs of all queries of the article
+		$db = & wfGetDB( DB_MASTER );
+
+		$cond = array("article_name" => $articleName);
+		$res = $db->select($db->tableName(self::QUERY_TABLE), 
+											array("query_id"), 
+											$cond);
+											
+		$t = $db->tableName(self::QUERY_RESULT_TABLE);
+		while ($row = $db->fetchObject($res)) {
+			// Delete all results with the query ID
+			$queryID = $row->query_id;
+			
+			$cond = array("query_id" => $queryID);
+			$db->delete($t, $cond);
+		}
+		$db->freeResult($res);
+		
+	}
+	
 }
