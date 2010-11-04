4f174a16cfb7787cb163d8fa529ae54de308dc91
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-11-04 10:05:00 +0000

    Changes for using the ARCLibrary extension
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8652 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/HaloACL/tests/LocalSettings.php
+++ extensions/HaloACL/tests/LocalSettings.php
@@ -10,6 +10,9 @@ $smwgWebserviceProtocol="rest";
 #Import SMW, SMWHalo and the Gardening extension
 include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
 enableSemantics('http://wiki', true);
+
+include_once('extensions/ARCLibrary/ARCLibrary.php');
+enableARCLibrary();
  
 include_once('extensions/SMWHalo/includes/SMW_Initialize.php');
 enableSMWHalo('SMWHaloStore2', "SMWTripleStore");
