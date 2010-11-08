d797ef40809f4ef958b3ceaea87d9012e778e27e
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-11-08 10:22:01 +0000

    fixed call of script that creates the ontology bundle descriptor.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@8715 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/maintenance/export.bat
+++ deployment/tools/maintenance/export.bat
@@ -3,4 +3,4 @@ IF EXIST %2 GOTO dump
 mkdir %2
 :dump
 php export.php --current --output=file:%2/dump.xml -b %1 %3 %4 %5
-php exportDesc.php -o %2/deploy.xml -b %1 -d dump.xml %3 %4 %5
+php exportOntologyBundleDeployDescriptor.php -o %2/deploy.xml -b %1 -d dump.xml %3 %4 %5
--- deployment/tools/maintenance/export.sh
+++ deployment/tools/maintenance/export.sh
@@ -8,4 +8,4 @@ then
 fi
 
 php export.php --current --output=file:$2/dump.xml -b $1 $3 $4 $5
-php exportDesc.php -o $2/deploy.xml -b $1 -d dump.xml $3 $4 $5
+php exportOntologyBundleDeployDescriptor.php -o $2/deploy.xml -b $1 -d dump.xml $3 $4 $5
