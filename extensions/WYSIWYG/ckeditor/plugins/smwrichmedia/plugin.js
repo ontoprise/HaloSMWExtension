9a26271343c705420cf9f561bba16549122a0f4d
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-12-09 14:01:04 +0000

    new version of the RichMedia WYSIWYG plugin to use the floatbox overlay
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@9183 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/WYSIWYG/ckeditor/plugins/smwrichmedia/plugin.js
+++ extensions/WYSIWYG/ckeditor/plugins/smwrichmedia/plugin.js
@@ -1,19 +1,75 @@
+//CKEDITOR.plugins.add('smw_richmedia', {
+//
+//    requires : [ 'mediawiki', 'dialog' ],
+//
+//	init : function( editor )
+//	{
+//		editor.addCommand( 'SMWrichmedia', commandDefinition );
+//        CKEDITOR.dialog.add( 'SMWrichmedia', this.path + 'dialogs/smwRmUploadDlg.js');
+//
+//        if ( editor.ui.addButton ) {
+//            editor.ui.addButton( 'SMWrichmedia',
+//                {
+//                    label : 'Upload media',
+//                    command : 'SMWrichmedia',
+//                    icon: this.path + 'images/icon_mediaupload.gif'
+//                });
+//        }
+//	}
+//});
+
+var plugin = CKEDITOR.plugins.smwtoolbar;
+var commandDefinition =
+{
+	preserveState : true,
+	editorFocus : false,
+	canUndo : false,
+	modes : { wysiwyg : 1, source : 1 },
+
+	exec: function( editor ) {
+		//get URL first
+		var src = window.parent.wgServer + window.parent.wgScript + '?title=Special:UploadWindow';
+		var article = window.parent.wgTitle;
+		if (window.parent.wgPageName == 'Special:AddData') {
+			//obviously we are in Special:AddData and wgTitle is not containing what we're loooking for...
+			// try target= ...first
+			var regexS = "[\\?&]target=([^&#]*)";
+			var regex = new RegExp( regexS );
+			var result = regex.exec( window.parent.location.href );
+			if (result == null) {
+				//target not found, it has to be the path now!
+				article = window.parent.location.pathname.match( /[^\/]+\/?$/ )[0];
+			}
+			else {
+				article = result[1];
+			}
+		}
+		var url = src + '&RMUpload[RelatedArticles]='+article+'&wpIgnoreWarning=true';
+		jQuery.fancybox({
+			'href' : url,
+			'width' : '75%',
+			'height' : '75%',
+			'autoScale' : false,
+			'transitionIn' : 'none',
+			'transitionOut' : 'none',
+			'type' : 'iframe',
+			'overlayColor'  : '#222',
+			'overlayOpacity' : '0.8',
+			'hideOnContentClick' : true
+		});
+	}
+};
+
 CKEDITOR.plugins.add('smw_richmedia', {
-
-    requires : [ 'mediawiki', 'dialog' ],
-
-	init : function( editor )
-	{
-		editor.addCommand( 'SMWrichmedia', new CKEDITOR.dialogCommand( 'SMWrichmedia' ) );
-        CKEDITOR.dialog.add( 'SMWrichmedia', this.path + 'dialogs/smwRmUploadDlg.js');
-
-        if ( editor.ui.addButton ) {
-            editor.ui.addButton( 'SMWrichmedia',
-                {
-                    label : 'Upload media',
-                    command : 'SMWrichmedia',
-                    icon: this.path + 'images/icon_mediaupload.gif'
-                });
-        }
+	requires : [ 'mediawiki'],
+	init : function( editor ) {
+		editor.addCommand( 'SMWrichmedia', commandDefinition);
+		if ( editor.ui.addButton ) {
+			editor.ui.addButton( 'SMWrichmedia', {
+				label : 'Upload media',
+				command : 'SMWrichmedia',
+				icon: this.path + 'images/icon_mediaupload.gif'
+			});
+		}
 	}
 });
