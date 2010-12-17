fa86723839482556054139f504302118bd0edde8
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-12-17 09:49:11 +0000

    fix url encoding problems in links.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@9298 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/WYSIWYG/ckeditor/plugins/mediawiki/plugin.js
+++ extensions/WYSIWYG/ckeditor/plugins/mediawiki/plugin.js
@@ -265,7 +265,7 @@ CKEDITOR.plugins.add( 'mediawiki',
                         'FCK__MWNoinclude',
                         'FCK__MWOnlyinclude'
                      ])
-                   ) return { MWSpecialTags: CKEDITOR.TRISTATE_ON };
+                   ) return {MWSpecialTags: CKEDITOR.TRISTATE_ON};
             });
         }
 		editor.on( 'doubleclick', function( evt )
@@ -615,7 +615,7 @@ CKEDITOR.customprocessor.prototype =
 							}
 							// #2223
 							if( htmlNode.getAttribute( '_fcknotitle' ) && htmlNode.getAttribute( '_fcknotitle' ) == "true" ){
-								var testHref = unescape(htmlNode.getAttribute('href'));
+								var testHref = htmlNode.getAttribute('href').urldecode();
 								var testInner = htmlNode.textContent || '';
 								if ( href.toLowerCase().StartsWith( 'category:' ) )
 									testInner = 'Category:' + testInner;
@@ -629,7 +629,7 @@ CKEDITOR.customprocessor.prototype =
 								stringBuilder.push(':');
 								href = href.substring(8);
 							}
-                            if ( isWikiUrl ) href = unescape(href);
+                            if ( isWikiUrl ) href = href.urldecode();
 							stringBuilder.push( href );
 							if ( pipeline && htmlNode.innerHTML != '[n]' && ( !isWikiUrl || href != htmlNode.innerHTML || !href.toLowerCase().StartsWith( "category:" ) ) ){
 								stringBuilder.push( isWikiUrl? '|' : ' ' );
@@ -1336,4 +1336,92 @@ if (!String.prototype.htmlEntities) {
     string = string.replace(/&nbsp;/g, '&#160;');
     return string;
   }
+}
+
+/**
+*
+*  URL encode / decode
+*  http://www.webtoolkit.info/
+*
+**/
+var WTKUrl = {
+
+	// public method for url encoding
+	encode : function (string) {
+		return escape(this._utf8_encode(string));
+	},
+
+	// public method for url decoding
+	decode : function (string) {
+		return this._utf8_decode(unescape(string));
+	},
+
+	// private method for UTF-8 encoding
+	_utf8_encode : function (string) {
+		string = string.replace(/\r\n/g,"\n");
+		var utftext = "";
+
+		for (var n = 0; n < string.length; n++) {
+
+			var c = string.charCodeAt(n);
+
+			if (c < 128) {
+				utftext += String.fromCharCode(c);
+			}
+			else if((c > 127) && (c < 2048)) {
+				utftext += String.fromCharCode((c >> 6) | 192);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+			else {
+				utftext += String.fromCharCode((c >> 12) | 224);
+				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				utftext += String.fromCharCode((c & 63) | 128);
+			}
+
+		}
+
+		return utftext;
+	},
+
+	// private method for UTF-8 decoding
+	_utf8_decode : function (utftext) {
+		var string = "";
+		var i = 0;
+		var c = c1 = c2 = 0;
+
+		while ( i < utftext.length ) {
+
+			c = utftext.charCodeAt(i);
+
+			if (c < 128) {
+				string += String.fromCharCode(c);
+				i++;
+			}
+			else if((c > 191) && (c < 224)) {
+				c2 = utftext.charCodeAt(i+1);
+				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				i += 2;
+			}
+			else {
+				c2 = utftext.charCodeAt(i+1);
+				c3 = utftext.charCodeAt(i+2);
+				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				i += 3;
+			}
+
+		}
+
+		return string;
+	}
+
+}
+if (!String.prototype.urlencode) {
+    String.prototype.urlencode = function() {
+        return WTKUrl.encode(this)
+    }
+}
+if (!String.prototype.urldecode) {
+    String.prototype.urldecode = function() {
+        return WTKUrl.decode(this)
+    }
 }
