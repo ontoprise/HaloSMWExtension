70cf50d471675a463385f1f6691d706158f6a0c4
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2010-12-22 14:26:30 +0000

    fix weird bug when the CKeditor was left and the page was saved. This caused that the getData() function of the CKeditor was called which had the old version of the WYSIWYG field, that overwrote the textarea of the wikitext editor/source code.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@9391 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/WYSIWYG/ckeditor/plugins/mediawiki/plugin.js
+++ extensions/WYSIWYG/ckeditor/plugins/mediawiki/plugin.js
@@ -364,11 +364,17 @@ CKEDITOR.customprocessor.prototype =
 	 *            for human reading. Not all Data Processors may provide it.
 	 */
 	toDataFormat : function( data, fixForBody ){
-        if (window.parent.wgCKeditorCurrentMode)
+        if (typeof window.parent.wgCKeditorCurrentMode != 'undefined') {
+            if (window.parent.wgCKeditorCurrentMode == 'source')
+                return window.parent.document.getElementById(wgCKeditorInstance.name).value;
             window.parent.wgCKeditorCurrentMode = 'source';
-        else if (window.parent.popup && window.parent.popup.parent.wgCKeditorCurrentMode)
+        }
+        else if (window.parent.popup &&
+                 typeof window.parent.popup.parent.wgCKeditorCurrentMode != 'undefined') {
+            if (window.parent.popup.parent.wgCKeditorCurrentMode == 'source')
+                return window.parent.popup.parent.document.getElementById(wgCKeditorInstance.name).value;
             window.parent.popup.parent.wgCKeditorCurrentMode = 'source';
-        
+        }
         data = '<body>' + data.htmlEntities()+ '</body>';
         // fix <img> tags
         data = data.replace(/(<img[^>]*)([^/])>/gi, '$1$2/>' );
