21c1df318504de9c6ab77de816ce7bc74a23bf4e
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-02-24 17:26:58 +0000

    fix bug #13579
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@9861 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/WYSIWYG/CKeditor.body.php
+++ extensions/WYSIWYG/CKeditor.body.php
@@ -224,6 +224,14 @@ class CKeditor_MediaWiki {
 		return false;
 	}
 
+    public function onDoEditSectionLink ($skin, $title, $section, $tooltip, $result, $lang = false ) {
+        global $wgCKEditorUrlparamMode;
+        if (isset($wgCKEditorUrlparamMode) && $wgCKEditorUrlparamMode === true) {
+            $result = str_replace( '&amp;action=edit', '&amp;action=edit&amp;mode=wysiwyg', $result);
+        }
+        return true;
+    }
+    
 	public function onEditPageBeforePreviewText( &$editPage, $previewOnOpen ) {
 		global $wgUser, $wgRequest;
 
