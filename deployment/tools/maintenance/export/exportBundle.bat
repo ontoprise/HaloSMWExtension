ca66596ca1f6a208324ab672530f9f5e33499b0f
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-03-13 16:22:50 +0000

    use of 7z instead of zip
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@9986 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/maintenance/export/exportBundle.bat
+++ deployment/tools/maintenance/export/exportBundle.bat
@@ -11,8 +11,14 @@ REM
 REM Author: Kai Kühn / ontoprise / 2011
 REM
 
-REM IMPORTANT: Make sure zip.exe is in PATH!
-SET ZIP=zip
+IF "%~1"=="" GOTO help
+IF "%~2"=="" GOTO help
+
+REM IMPORTANT: Make sure 7z.exe is in PATH!
+SET ZIP=7z
+
+@%ZIP% > null
+IF %ERRORLEVEL% NEQ 0 GOTO install7z
 
 REM Set output dir
 SET OUTPUTDIR=C:\TEMP\%2\extensions\%1
@@ -21,20 +27,29 @@ mkdir %OUTPUTDIR%
 :dump
 
 REM Export bundle
-ECHO Export bundle
+ECHO Export bundle %1
 php export.php --current --output=file:%OUTPUTDIR%/dump.xml -b %1 %3 %4 %5
 php exportOntologyBundleDeployDescriptor.php -o %OUTPUTDIR%/deploy.xml -b %1 -d dump.xml %3 %4 %5
 
 REM Zip bundle
 ECHO Zip bundle
-set OLDDIR=%CD%
-cd C:\TEMP\%2
-%ZIP% -r %1.zip *
-cd %OLDDIR%
+%ZIP% a -r C:\TEMP\%2\%1.zip C:\TEMP\%2\*
 
 REM Remove temp dir
 ECHO Remove temporary directory
 RMDIR /S /Q C:\TEMP\%2\extensions
 
-:end
 ECHO The output file is at: C:\TEMP\%2
+GOTO end
+
+:help
+echo.
+echo Usage: exportBundle.bat bundle-id temporary-name (arbitrary)
+GOTO end
+
+:install7z
+echo.
+echo 7-zip is required to run this utility [http://www.7-zip.org/]. Make sure it is in the PATH variable.
+GOTO end
+
+:end
