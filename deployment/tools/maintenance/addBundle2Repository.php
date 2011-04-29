e9a7045e58eb402d7c544307d42da2cfa171ebc0
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-04-29 14:56:48 +0000

    add support for mediawiki deployable
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@10763 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/maintenance/addBundle2Repository.php
+++ deployment/tools/maintenance/addBundle2Repository.php
@@ -5,11 +5,12 @@
  *
  * Adds a bundle or a set of bundles to a repository.
  *
- * Usage: php addBundle2Repository.php 
- *                  -r <repository-dir>         The repository root directory 
+ * Usage: php addBundle2Repository.php
+ *                  -r <repository-dir>         The repository root directory
  *                  -b <bundle file or dir>     The bundle file or a directory containing bundle files
- *                  --url <repository-url>      The download base URL  
+ *                  --url <repository-url>      The download base URL
  *                  [--latest]                  Latest version?
+ *                  [--mediawiki]               Include Mediawiki?
  *
  * @author: Kai Kühn / ontoprise / 2011
  *
@@ -47,11 +48,15 @@ for( $arg = reset( $argv ); $arg !== false; $arg = next( $argv ) ) {
 		$repositoryURL = next($argv);
 		continue;
 	}
-	
-    if ($arg == '--latest') {
-    	$latest = true;
-        continue;
-    }
+
+	if ($arg == '--latest') {
+		$latest = true;
+		continue;
+	}
+	if ($arg == '--mediawiki') {
+		$mediawiki = true;
+		continue;
+	}
 }
 
 if (!isset($repositoryDir) || !isset($bundlePath) || !isset($repositoryURL)) {
@@ -61,10 +66,10 @@ if (!isset($repositoryDir) || !isset($bundlePath) || !isset($repositoryURL)) {
 
 // create symlinks for Linux and Windows 7
 if (Tools::isWindows($os) && $latest) {
-    $createSymlinks = ($os == 'Windows 7');
-    if (!$createSymlinks) {
-        echo "Be careful: Cannot create symbolic links on Windows <= 7!";
-    }
+	$createSymlinks = ($os == 'Windows 7');
+	if (!$createSymlinks) {
+		echo "Be careful: Cannot create symbolic links on Windows <= 7!";
+	}
 }
 
 // create binary path
@@ -85,7 +90,7 @@ $extensionsNode = $nodeList->item(0);
 
 foreach($descriptors as $tuple) {
 	list($dd, $zipFilepath) = $tuple;
-	
+
 	// 1. create extensions substructure
 	$id = $dd->getID();
 	$version = $dd->getVersion();
@@ -109,51 +114,85 @@ foreach($descriptors as $tuple) {
 		}
 		if ($res == 0) print "\n\tCreated link: ".$repositoryDir."/extensions/".$id.'/deploy.xml';
 	}
-    echo "\n..done.";
-    
+	echo "\n..done.";
+
 	// 2. Add to repository.xml
-	$newExt = $repoDoc->createElement("extension");
-	$idAttr = $repoDoc->createAttribute("id");
-	$idAttr->value = $dd->getID();
-	$newExt->appendChild($idAttr);
-	
-	$newVer = $repoDoc->createElement("version");
-    $newExt->appendChild($newVer);
-    
-	$urlAttr = $repoDoc->createAttribute("url");
-	$urlAttr->value = $repositoryURL."/bin/".$dd->getID()."-".Tools::addSeparators($dd->getVersion(), $dd->getPatchlevel()).".zip";
-	$newVer->appendChild($urlAttr);
-
-	$versionAttr = $repoDoc->createAttribute("ver");
-	$versionAttr->value = $dd->getVersion();
-	$newVer->appendChild($versionAttr);
-
-
-	$patchlevelAttr = $repoDoc->createAttribute("patchlevel");
-	$patchlevelAttr->value = $dd->getPatchlevel();
-	$newVer->appendChild($patchlevelAttr);
-
-	$maintainerAttr = $repoDoc->createAttribute("maintainer");
-	$maintainerAttr->value = $dd->getMaintainer();
-	$newVer->appendChild($maintainerAttr);
-
-	$descriptionAttr = $repoDoc->createAttribute("description");
-	$descriptionAttr->value = $dd->getDescription();
-	$newVer->appendChild($descriptionAttr);
-
-	$helpurlAttr = $repoDoc->createAttribute("helpurl");
-	$helpurlAttr->value = $dd->getHelpURL();
-	$newVer->appendChild($helpurlAttr);
-
-	echo "\nAdd to repository: ".$dd->getID();
+    echo "\nAdd to repository: ".$dd->getID();
+	$newExt = createRepositoryEntry($repoDoc, $dd, $repositoryURL);
 	$extensionsNode->appendChild($newExt);
-    echo "..done.";
-    	
+	echo "..done.";
+	 
 	// 3. copy binary package
 	$targetPackageFile = $repositoryDir."/bin/$id-".Tools::addSeparators($dd->getVersion(), $dd->getPatchlevel()).".zip";
 	echo "\nCopy package $id to $targetPackageFile";
-    copy($zipFilepath, $targetPackageFile);	
+	copy($zipFilepath, $targetPackageFile);
+	echo "..done.";
+}
+
+if ($mediawiki) {
+	$version = Tools::getMediawikiVersion(realpath($rootDir."/../"));
+	$version = str_replace(".", "", $version);
+	$xml = <<<ENDS
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<deploydescriptor>
+    <global>
+        <version>$version</version>
+        <id>mw</id>
+        <instdir></instdir>
+        <vendor>Mediawiki</vendor>
+        <description>Mediawiki</description>
+
+        <dependencies>
+    </dependencies>
+    </global>
+    <wikidumps>
+    
+    </wikidumps>
+    <resources>
+    
+    </resources>
+    <configs>
+     <new>
+
+       <script file="maintenance/update.php" />
+     </new>
+    </configs>
+</deploydescriptor>
+	
+
+ENDS;
+    $id = 'mw';
+	Tools::mkpath($repositoryDir."/extensions/$id");
+	$handle = fopen($repositoryDir."/extensions/$id/deploy-$version.xml", "w");
+	fwrite($handle, $xml);
+	fclose($handle);
+
+	// creates links
+	if ($createSymlinks && $latest) {
+		// remove symbolic link if existing
+		if (file_exists($repositoryDir."/$id/deploy.xml")) {
+			unlink($repositoryDir."/$id/deploy.xml");
+		}
+		// create symbolic link
+		if (Tools::isWindows()) {
+			$target = str_replace("/", "\\", "$repositoryDir/extensions/$id/deploy-$version.xml");
+			$link = str_replace("/", "\\", "$repositoryDir/extensions/$id/deploy.xml");
+			exec("mklink \"$link\" \"$target\"", $out, $res);
+		} else{
+			exec("ln -s $repositoryDir/extensions/$id/deploy-$version.xml $repositoryDir/extensions/$id/deploy.xml", $out, $res);
+		}
+		if ($res == 0) print "\n\tCreated link: ".$repositoryDir."/extensions/".$id.'/deploy.xml';
+	}
+	
+	$dd = new DeployDescriptor($xml);
+	
+	echo "\nAdd to repository: ".$id;
+    $newExt = createRepositoryEntry($repoDoc, $dd, $repositoryURL);
+    $extensionsNode->appendChild($newExt);
     echo "..done.";
+    
+    // assume binary package exists
+    
 }
 
 // save repository.xml
@@ -192,7 +231,7 @@ function loadRepository($filePath) {
  * Extracts the deploy descriptor(s) from a bundle or a set of bundles.
  *
  * @param $bundlePath (file or directory)
- * 
+ *
  * @return array of (DeployDescriptor, Bundle file path)
  */
 function extractDeployDescriptors($bundlePath) {
@@ -214,6 +253,43 @@ function extractDeployDescriptors($bundlePath) {
 		$dd = Tools::unzipDeployDescriptor($bundlePath, $tmpFolder);
 		return array(array($dd, $bundlePath));
 	}
+}
+
+function createRepositoryEntry($repoDoc, $dd, $repositoryURL) {
+	$newExt = $repoDoc->createElement("extension");
+    $idAttr = $repoDoc->createAttribute("id");
+    $idAttr->value = $dd->getID();
+    $newExt->appendChild($idAttr);
+
+    $newVer = $repoDoc->createElement("version");
+    $newExt->appendChild($newVer);
+
+    $urlAttr = $repoDoc->createAttribute("url");
+    $urlAttr->value = $repositoryURL."/bin/".$dd->getID()."-".Tools::addSeparators($dd->getVersion(), $dd->getPatchlevel()).".zip";
+    $newVer->appendChild($urlAttr);
+
+    $versionAttr = $repoDoc->createAttribute("ver");
+    $versionAttr->value = $dd->getVersion();
+    $newVer->appendChild($versionAttr);
+
+
+    $patchlevelAttr = $repoDoc->createAttribute("patchlevel");
+    $patchlevelAttr->value = $dd->getPatchlevel();
+    $newVer->appendChild($patchlevelAttr);
+
+    $maintainerAttr = $repoDoc->createAttribute("maintainer");
+    $maintainerAttr->value = $dd->getMaintainer();
+    $newVer->appendChild($maintainerAttr);
+
+    $descriptionAttr = $repoDoc->createAttribute("description");
+    $descriptionAttr->value = $dd->getDescription();
+    $newVer->appendChild($descriptionAttr);
+
+    $helpurlAttr = $repoDoc->createAttribute("helpurl");
+    $helpurlAttr->value = $dd->getHelpURL();
+    $newVer->appendChild($helpurlAttr);
+    
+    return $newExt;
 }
 
 
