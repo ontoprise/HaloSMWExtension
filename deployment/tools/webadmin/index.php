63404e007d718bb3c9f0aea6a26a7440fc0cc892
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-05-18 23:25:59 +0000

    added jquery/jquery-ui and some mockup code
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@10942 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/webadmin/index.php
+++ deployment/tools/webadmin/index.php
@@ -29,5 +29,41 @@
  *
  */
  
-echo "This is the web administration tool of the deployment framework.";
+$html = <<<ENDS
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-gb" xml:lang="en-gb">
+<head>
+<link type="text/css" href="skins/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />	
+<script type="text/javascript" src="scripts/jquery-1.6.1.min.js"></script>
+<script type="text/javascript" src="scripts/jquery-ui-1.8.13.custom.min.js"></script>
+<script type="text/javascript">
+			$(function(){
+		
+				// Tabs
+				$('#tabs').tabs();
+			});
+</script>
+</head>
+ENDS
+;
+$html .= "<body>This is the web administration tool of the deployment framework.";
+$html .= <<<ENDS
+<div id="tabs">
+
+			<ul>
+				<li><a href="#tabs-1">First</a></li>
+				<li><a href="#tabs-2">Second</a></li>
+				<li><a href="#tabs-3">Third</a></li>
+			</ul>
+			<div id="tabs-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+			<div id="tabs-2">Phasellus mattis tincidunt nibh. Cras orci urna, blandit id, pretium vel, aliquet ornare, felis. Maecenas scelerisque sem non nisl. Fusce sed lorem in enim dictum bibendum.</div>
+
+			<div id="tabs-3">Nam dui erat, auctor a, dignissim quis, sollicitudin eu, felis. Pellentesque nisi urna, interdum eget, sagittis et, consequat vestibulum, lacus. Mauris porttitor ullamcorper augue.</div>
+		</div>
+ENDS
+;
+$html .= "</body>";
+
+echo $html;
+
 die();
