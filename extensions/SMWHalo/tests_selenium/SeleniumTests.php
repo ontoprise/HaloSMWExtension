568c7b8c1ce1bbd72be156b0f0fbf87c3b02825c
rizhikov <rizhikov@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-05-18 14:07:54 +0000

    Added/updated Selenium tests for QueryInterface
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@10927 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests_selenium/SeleniumTests.php
+++ extensions/SMWHalo/tests_selenium/SeleniumTests.php
@@ -10,7 +10,7 @@
  */
 
 require_once 'testcases/TestSubquery.php';
-require_once 'testcases/TestPreviewResult.php';
+require_once 'testcases/TestPreviewResult_short.php';
 
 class SeleniumTests
 {
@@ -18,8 +18,8 @@ class SeleniumTests
     {
 		define('UNIT_TEST_RUNNING', true);
         $suite = new PHPUnit_Framework_TestSuite('SMWHaloSeleniumTestSuite');
-        $suite->addTestSuite("TestSubquery");
-        $suite->addTestSuite("TestPreviewResult");
+//        $suite->addTestSuite("TestSubquery");
+        $suite->addTestSuite("TestPreviewResult_short");
         return $suite;
     }
 }
