17d2f44f20595a12c3cdc92e402f247fcc482b5f
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-05-18 10:33:09 +0000

    add reqired dependency of the ARC Library extension
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@10924 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/tests_selenium/LocalSettings.php
+++ extensions/SMWHalo/tests_selenium/LocalSettings.php
@@ -5,6 +5,10 @@ $wgDefaultSkin = 'ontoskin3';
 $wgGroupPermissions['*']['ontologyediting']=true;
 $wgGroupPermissions['user']['ontologyediting']=true;
 
+#Import ARC2 library
+require_once('extensions/ARCLibrary/ARCLibrary.php');
+enableARCLibrary();
+
 #Import SMW, SMWHalo and the Gardening extension
 include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
 enableSemantics('http://wiki', true);
