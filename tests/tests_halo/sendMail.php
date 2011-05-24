6f7d7030655ab2c4a43711ab2586ef10e377b35a
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-05-24 10:08:50 +0000

    allow also sending mail using an smtp server config and not only the localhost with an installed smtp server
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@10992 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- tests/tests_halo/sendMail.php
+++ tests/tests_halo/sendMail.php
@@ -4,8 +4,13 @@
  *
  * Usage:
  *
- *  php sendMail.php -t <recipient email address> [ -s <subject> ]
+ *  php sendMail.php -t <recipient email address>
+ *                   [ -s <subject> ]
  *                   [ -m <mail text> | -f <file with mail text> ]
+ *                   [ -F <from adress> ]
+ *                   [ -S <smtp server with port> ]
+ *                   [ -U <smtp user> ]
+ *                   [ -P <smtp password> ]
  *
  */
 
@@ -23,6 +28,25 @@ while ($arg = array_shift($args)) {
         if (!file_exists($filename)) die ("Error: file doesn't exist\n");
         $message = file_get_contents($filename);
     }
+    else if ($arg == '-F')
+        $from = array_shift($args) or die ("Error: missing value for -F\n");
+    else if ($arg == '-S') {
+        $smtp= array_shift($args) or die ("Error: missing value for -S\n");
+        if (preg_match('/:(\d+)$/', $smtp, $matches)) {
+           $host = str_replace(':'.$matches[1], '', $smtp);
+           $port = $matches[1];
+        }
+        else {
+            $host = $smtp;
+            $port = "465";
+        }
+    }
+    else if ($arg == '-U') {
+        $username= array_shift($args) or die ("Error: missing value for -U\n");
+    }
+    else if ($arg == '-P') {
+        $password= array_shift($args) or die ("Error: missing value for -P\n");
+    }
 }
 if (!isset($to)) die ("Error: no recipient given whom to send mail to\n");
 if (!isset($subject)) $subject = "";
@@ -30,7 +54,32 @@ if (!isset($message)) $message = "";
 if (strlen($message) == 0 && strlen($subject) == 0)
     die ("Error: empty subject and mail body\n");
 
-$headers = 'From: Hudson Buildserver <robotta@ontoprise.de>';
+// set from address to some default
+if (! isset($from)) $from = "Hudson Buildserver <robotta@ontoprise.de>";
 
-mail($to, $subject, $message, $headers);
+// send mail using the php mail command when we do not have smtp settings defined
+if (! isset($smtp)) {
+    $headers = 'From: '.trim($from). "\r\n";
+    mail($to, $subject, $message, $headers);
+    exit(0);
+}
 
+// smtp server config set, then use the Pear Mail package
+require_once "Mail.php";
+
+$headers = array ('From' => $from,
+   'To' => $to,
+   'Subject' => $subject);
+$smtp = Mail::factory('smtp',
+   array ('host' => $host,
+     'port' => $port,
+     'auth' => true,
+     'username' => $username,
+     'password' => $password));
+
+$mail = $smtp->send($to, $headers, $message);
+
+if (PEAR::isError($mail))
+   echo("\n" . $mail->getMessage() );
+else
+   echo("\nMessage successfully sent!\n");
