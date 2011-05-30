eadc55df8df37f823797e62b5c72ecf77d4aa3e3
rizhikov <rizhikov@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-05-30 09:02:57 +0000

    Fixed invalid test suit name
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11041 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/WYSIWYG/tests_selenium_ext_img/WYSIWYGSeleniumTests.php
+++ extensions/WYSIWYG/tests_selenium_ext_img/WYSIWYGSeleniumTests.php
@@ -9,10 +9,6 @@
  * @author OP
  */
 
-require_once 'testcases/TestAnnotationsAndIcons.php';
-require_once 'testcases/TestPropertiesAndCategoriesChanges.php';
-require_once 'testcases/TestQueryInterfaceInWysiwyg.php';
-require_once 'testcases/TestTransformationOfSemanticData.php';
 require_once 'testcases/TestAddingExternalImages.php';
 
 class WYSIWYGSeleniumTests
@@ -20,11 +16,7 @@ class WYSIWYGSeleniumTests
 	public static function suite()
 	{
 		$suite = new PHPUnit_Framework_TestSuite('WYSIWYGSeleniumTestSuite');
-		$suite->addTestSuite("TestAnnotationsAndIcons");
-		$suite->addTestSuite("TestPropertiesAndCategoriesChanges");
-		$suite->addTestSuite("TestQueryInterfaceInWysiwyg");
-		$suite->addTestSuite("TestTransformationOfSemanticData");
-		$suite->addTestSuite("TestAddingExternalImages.php");
+		$suite->addTestSuite("TestAddingExternalImages");
 		return $suite;
 	}
 }
