0b0890206f23929fe001618b8afc33c3feded463
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-06-10 08:38:48 +0000

    header added
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11146 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/webadmin/logout.php
+++ deployment/tools/webadmin/logout.php
@@ -1,4 +1,33 @@
 <?php
+/*  Copyright 2011, ontoprise GmbH
+ *
+ *   The deployment tool is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The deployment tool is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file
+ * @ingroup WebAdmin
+ *
+ * @defgroup WebAdmin Web-administration tool
+ * @ingroup DeployFramework
+ *
+ * Installation tool.
+ *
+ * @author: Kai Kühn / ontoprise / 2011
+ *
+ *
+ */
 session_start();
 session_destroy();
 
