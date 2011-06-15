6c51f97a3d8c66812dcb83cb81f94c37745f8f9f
alami <alami@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-06-15 11:19:27 +0000

    Issue 14450 - IE 8: AC in STB: AC is behind STB
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11183 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/WYSIWYG/ckeditor/plugins/smwtoolbar/plugin.js
+++ extensions/WYSIWYG/ckeditor/plugins/smwtoolbar/plugin.js
@@ -1130,10 +1130,10 @@ CKEDITOR.plugins.add('smwtoolbar', {
         });
         editor.on("resize", function(event) {
             if (plugin.stbIsActive) {
-                var ontomenu = window.parent.document.getElementById('ontomenuanchor');
+                //var ontomenu = window.parent.document.getElementById('ontomenuanchor');
                 // I have no clue how to know in which mode we are, so just set the z-index to some
                 // value that works in both modes
-                ontomenu.style.zIndex = editor.config.baseFloatZIndex + 10;
+                //ontomenu.style.zIndex = editor.config.baseFloatZIndex + 10;
             }
         })
 
