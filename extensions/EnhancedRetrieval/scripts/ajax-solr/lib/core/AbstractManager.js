f0c9450dd16f053041890ed19c50a6d8e9809679
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-06-27 09:08:28 +0000

    The presence of the Firefox console object was not correctly detected.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11282 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/EnhancedRetrieval/scripts/ajax-solr/lib/core/AbstractManager.js
+++ extensions/EnhancedRetrieval/scripts/ajax-solr/lib/core/AbstractManager.js
@@ -175,13 +175,13 @@ AjaxSolr.AbstractManager = AjaxSolr.Class.extend(
   handleResponse: function (data) {
     this.response = data;
 
-	if (console && console.time) {
+	if (typeof console !== 'undefined' && console.time) {
 		console.time("handleResponse");
 	}
     for (var widgetId in this.widgets) {
       this.widgets[widgetId].afterRequest();
     }
-	if (console && console.timeEnd) {
+	if (typeof console !== 'undefined' && console.timeEnd) {
 		console.timeEnd("handleResponse");
 	}
 	
