93f1a1e4358936a1486f30e96115a5218317c568
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-07-04 22:21:10 +0000

    check for TSCDIR added
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11452 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/onto2mwxml/onto2mwxml.sh
+++ deployment/tools/onto2mwxml/onto2mwxml.sh
@@ -6,6 +6,15 @@ TSCDIR=
 
 # ---------------------------------------------
 
+# check if TSCDIR is set
+if [ -n "$TSCDIR" ]
+then
+echo "Using TSCDIR=$TSCDIR"
+else
+echo "TSCDIR is empty. Please set."
+exit 1
+fi
+
 #
 # Starts onto2mwxml and returns the error code
 #
