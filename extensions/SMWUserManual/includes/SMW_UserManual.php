936c57a19e65ee71a53f9fe2acf7bfd0b65c8054
robotta <robotta@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-07-05 08:48:22 +0000

    some changes: - setup script modifies help articles only. - Rationale annotation for helparticles in setup script - documentation adjusted
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11454 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWUserManual/includes/SMW_UserManual.php
+++ extensions/SMWUserManual/includes/SMW_UserManual.php
@@ -130,14 +130,8 @@ define('SMW_FORUM_COMMENT_URL', 'http://smwforum.ontoprise.com/smwforum/extensio
 define('SMW_FORUM_QUERY_CSH', '[[Category:Context sensitive help article]]');
 // Porperty for discourse state
 define('SMW_UME_PROPERTY_DISCOURSE_STATE', 'UME discourse state');
-// text for this property page
-define('SMW_UME_PROPERTYTEXT_DISCOURSE_STATE', '[[Has type::Type:String| ]]');
 // Property for link to real help article
 define('SMW_UME_PROPERTY_LINK', 'UME link');
-// text for this property page
-define('SMW_UME_PROPERTYTEXT_LINK', '[[Has type::Type:URL| ]]');
-// Templates used in the SMW Forum that are required for this extension
-define('SMW_TEMPLATES_USED', 'Copyright of article,GNU Free Documentation License,Cite web in help');
 // prefix for SMW forum template pages
 define('SMW_TEMPLATE_PREFIX', 'Template');
 // broken articles that sholuld be skiped
