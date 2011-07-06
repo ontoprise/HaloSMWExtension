b2d1f6fb4ff9692ffb08c36532fe7cc52ddeb986
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-07-06 11:53:50 +0000

    print warning if TSCDIR is not set
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11488 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/onto2mwxml/onto2mwxml.bat
+++ deployment/tools/onto2mwxml/onto2mwxml.bat
@@ -6,8 +6,15 @@ SET TSCDIR=
 
 REM --------------------------------------
 
+IF [%TSCDIR%]==[] goto installationHint
 SET CURRENT=%cd%
 cd %TSCDIR%
 %TSCDIR%\onto2mwxml.exe %*
 IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 CD %CURRENT%
+GOTO end
+
+:installationHint
+echo "Please set TSCDIR before using onto2mwxml."
+
+:end
