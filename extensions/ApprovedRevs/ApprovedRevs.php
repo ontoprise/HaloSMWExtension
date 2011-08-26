b0fb0069b9018ea59bf7ad48d371708f06ae13f2
erdmann <erdmann@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-08-26 11:33:20 +0000

    new extension ApprovedRevs
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11962 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ extensions/ApprovedRevs/ApprovedRevs.php
@@ -0,0 +1,83 @@
+<?php
+
+if ( !defined( 'MEDIAWIKI' ) ) die();
+
+/**
+ * @file
+ * @ingroup Extensions
+ *
+ * @author Yaron Koren
+ */
+
+define( 'APPROVED_REVS_VERSION', '0.5.9' );
+
+// credits
+$wgExtensionCredits['other'][] = array(
+	'path'            => __FILE__,
+	'name'            => 'Approved Revs',
+	'version'         => APPROVED_REVS_VERSION,
+	'author'          => 'Yaron Koren',
+	'url'             => 'http://www.mediawiki.org/wiki/Extension:Approved_Revs',
+	'descriptionmsg'  => 'approvedrevs-desc'
+);
+
+// global variables
+$egApprovedRevsIP = dirname( __FILE__ ) . '/';
+$egApprovedRevsScriptPath = $wgScriptPath . '/extensions/ApprovedRevs';
+$egApprovedRevsNamespaces = array( NS_MAIN, NS_TEMPLATE, NS_HELP, NS_PROJECT );
+$egApprovedRevsBlankIfUnapproved = false;
+$egApprovedRevsAutomaticApprovals = true;
+$egApprovedRevsShowApproveLatest = false;
+
+// internationalization
+$wgExtensionMessagesFiles['ApprovedRevs'] = $egApprovedRevsIP . 'ApprovedRevs.i18n.php';
+$wgExtensionAliasesFiles['ApprovedRevs'] = $egApprovedRevsIP . 'ApprovedRevs.alias.php';
+
+// register all classes
+$wgAutoloadClasses['ApprovedRevs'] = $egApprovedRevsIP . 'ApprovedRevs_body.php';
+$wgAutoloadClasses['ApprovedRevsHooks'] = $egApprovedRevsIP . 'ApprovedRevs.hooks.php';
+$wgSpecialPages['ApprovedRevs'] = 'SpecialApprovedRevs';
+$wgAutoloadClasses['SpecialApprovedRevs'] = $egApprovedRevsIP . 'SpecialApprovedRevs.php';
+$wgSpecialPageGroups['ApprovedRevs'] = 'pages';
+
+// hooks
+$wgHooks['ParserBeforeInternalParse'][] = 'ApprovedRevsHooks::setApprovedRevForParsing';
+$wgHooks['ArticleSaveComplete'][] = 'ApprovedRevsHooks::setLatestAsApproved';
+$wgHooks['PersonalUrls'][] = 'ApprovedRevsHooks::removeRobotsTag';
+$wgHooks['ArticleFromTitle'][] = 'ApprovedRevsHooks::showApprovedRevision';
+$wgHooks['ArticleAfterFetchContent'][] = 'ApprovedRevsHooks::showBlankIfUnapproved';
+$wgHooks['DisplayOldSubtitle'][] = 'ApprovedRevsHooks::setSubtitle';
+// it's 'SkinTemplateNavigation' for the Vector skin, 'SkinTemplateTabs' for
+// most other skins
+$wgHooks['SkinTemplateTabs'][] = 'ApprovedRevsHooks::changeEditLink';
+$wgHooks['SkinTemplateNavigation'][] = 'ApprovedRevsHooks::changeEditLinkVector';
+$wgHooks['PageHistoryBeforeList'][] = 'ApprovedRevsHooks::storeApprovedRevisionForHistoryPage';
+$wgHooks['PageHistoryLineEnding'][] = 'ApprovedRevsHooks::addApprovalLink';
+$wgHooks['UnknownAction'][] = 'ApprovedRevsHooks::setAsApproved';
+$wgHooks['UnknownAction'][] = 'ApprovedRevsHooks::unsetAsApproved';
+$wgHooks['BeforeParserFetchTemplateAndtitle'][] = 'ApprovedRevsHooks::setTranscludedPageRev';
+$wgHooks['ArticleDeleteComplete'][] = 'ApprovedRevsHooks::deleteRevisionApproval';
+$wgHooks['MagicWordwgVariableIDs'][] = 'ApprovedRevsHooks::addMagicWordVariableIDs';
+$wgHooks['LanguageGetMagic'][] = 'ApprovedRevsHooks::addMagicWordLanguage';
+$wgHooks['ParserBeforeTidy'][] = 'ApprovedRevsHooks::handleMagicWords';
+$wgHooks['AdminLinks'][] = 'ApprovedRevsHooks::addToAdminLinks';
+$wgHooks['LoadExtensionSchemaUpdates'][] = 'ApprovedRevsHooks::describeDBSchema';
+$wgHooks['EditPage::showEditForm:initial'][] = 'ApprovedRevsHooks::addWarningToEditPage';
+$wgHooks['sfHTMLBeforeForm'][] = 'ApprovedRevsHooks::addWarningToSFForm';
+$wgHooks['ArticleViewHeader'][] = 'ApprovedRevsHooks::onArticleViewHeader';
+
+// logging
+$wgLogTypes['approval'] = 'approval';
+$wgLogNames['approval'] = 'approvedrevs-logname';
+$wgLogHeaders['approval'] = 'approvedrevs-logdesc';
+$wgLogActions['approval/approve'] = 'approvedrevs-approveaction';
+$wgLogActions['approval/unapprove'] = 'approvedrevs-unapproveaction';
+
+// user rights
+$wgAvailableRights[] = 'approverevisions';
+$wgGroupPermissions['sysop']['approverevisions'] = true;
+$wgAvailableRights[] = 'viewlinktolatest';
+$wgGroupPermissions['*']['viewlinktolatest'] = true;
+
+// page properties
+$wgPageProps['approvedrevs'] = 'Whether or not the page is approvable';
