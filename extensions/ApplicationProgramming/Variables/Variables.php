19908a4b22bc4bbf6c8ce8b4f0b0a0a58669b6d2
erdmann <erdmann@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-08-30 10:02:20 +0000

    * updated to ParserFunctions v1.4.0 (r87170) * removed StringFunctions: they are subsumed by ParserFunctions now * updated to Variable extension v1.3.1
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@11998 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/ApplicationProgramming/Variables/Variables.php
+++ extensions/ApplicationProgramming/Variables/Variables.php
@@ -1,71 +1,87 @@
 <?php
- 
+/**
+ * Variables extension -- define page-scoped variables
+ *
+ * @file
+ * @ingroup Extensions
+ * @version 1.3.1
+ * @author Rob Adams
+ * @author Tom Hempel
+ * @author Daniel Werner
+ * @link http://www.mediawiki.org/wiki/Extension:VariablesExtension Documentation
+ */
+
 if ( !defined( 'MEDIAWIKI' ) ) {
-    die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
- 
-$wgExtensionFunctions[] = 'wfSetupVariables';
- 
+
+// Extension credits that will show up on Special:Version
 $wgExtensionCredits['parserhook'][] = array(
-        'name' => 'Variables',
-        'version' => '1.3',
-        'url' => 'http://www.mediawiki.org/wiki/Extension:VariablesExtension',
-        'author' => 'Rob Adams, Tom Hempel and Daniel Werner',
-        'description' => 'Define page-scoped variables',
+	'name' => 'Variables',
+	'version' => '1.3.1',
+	'author' => array( 'Rob Adams', 'Tom Hempel', 'Daniel Werner' ),
+	'description' => 'Define page-scoped variables',
+	'url' => 'http://www.mediawiki.org/wiki/Extension:VariablesExtension',
 );
- 
-$wgHooks['LanguageGetMagic'][]       = 'wfVariablesLanguageGetMagic';
- 
+
+// @todo FIXME: could this use ParserFirstCallInit hook instead of
+// $wgExtensionFunctions?
+$wgExtensionFunctions[] = 'wfSetupVariables';
+
+$wgHooks['LanguageGetMagic'][] = 'wfVariablesLanguageGetMagic';
+
 class ExtVariables {
-    var $mVariables = array();
- 
-   function ExtVariables() { //Constructor
-      global $wgHooks;
-      $wgHooks['ParserClearState'][] = &$this;
-   }
- 
-    function onParserClearState( &$parser ) {
-        $this->mVariables = array(); //remove all variables to avoid conflicts with job queue or Special:Import
-        return true;
-    }
- 
-    function vardefine( &$parser, $expr = '', $value = '' ) {
-        $this->mVariables[$expr] = $value;
-        return '';
-    }
- 
-    function vardefineecho( &$parser, $expr = '', $value = '' ) {
-        $this->mVariables[$expr] = $value;
-        return $value;
-    }
- 
-    function varf( &$parser, $expr = '', $defaultVal = '' ) {        
-        if ( isset($this->mVariables[$expr]) and $this->mVariables[$expr] != '' ) {
-            return $this->mVariables[$expr];
-        } else {
-            return $defaultVal;
-        }
-    }
- 
-    function varexists( &$parser, $expr = '' ) {
-        return array_key_exists($expr, $this->mVariables);
-    }
+	var $mVariables = array();
+
+	function __construct() {
+		global $wgHooks;
+		$wgHooks['ParserClearState'][] = &$this;
+	}
+
+	function onParserClearState( &$parser ) {
+		$this->mVariables = array(); //remove all variables to avoid conflicts with job queue or Special:Import
+		return true;
+	}
+
+	function vardefine( &$parser, $expr = '', $value = '' ) {
+		$this->mVariables[$expr] = $value;
+		return '';
+	}
+
+	function vardefineecho( &$parser, $expr = '', $value = '' ) {
+		$this->mVariables[$expr] = $value;
+		return $value;
+	}
+
+	function varf( &$parser, $expr = '', $defaultVal = '' ) {
+		if ( isset( $this->mVariables[$expr] ) && $this->mVariables[$expr] != '' ) {
+			return $this->mVariables[$expr];
+		} else {
+			return $defaultVal;
+		}
+	}
+
+	function varexists( &$parser, $expr = '' ) {
+		return array_key_exists( $expr, $this->mVariables );
+	}
 }
- 
+
 function wfSetupVariables() {
-    global $wgParser, $wgExtVariables, $wgHooks;
- 
-    $wgExtVariables = new ExtVariables;
- 
-    $wgParser->setFunctionHook( 'vardefine', array( &$wgExtVariables, 'vardefine' ) );
-    $wgParser->setFunctionHook( 'vardefineecho', array( &$wgExtVariables, 'vardefineecho' ) );
-    $wgParser->setFunctionHook( 'var', array( &$wgExtVariables, 'varf' ) );
-    $wgParser->setFunctionHook( 'varexists', array( &$wgExtVariables, 'varexists' ) );
+	global $wgParser, $wgExtVariables;
+
+	$wgExtVariables = new ExtVariables;
+
+	$wgParser->setFunctionHook( 'vardefine', array( &$wgExtVariables, 'vardefine' ) );
+	$wgParser->setFunctionHook( 'vardefineecho', array( &$wgExtVariables, 'vardefineecho' ) );
+	$wgParser->setFunctionHook( 'var', array( &$wgExtVariables, 'varf' ) );
+	$wgParser->setFunctionHook( 'varexists', array( &$wgExtVariables, 'varexists' ) );
 }
- 
+
 function wfVariablesLanguageGetMagic( &$magicWords, $langCode = 0 ) {
-        require_once( dirname( __FILE__ ) . '/Variables.i18n.php' );
-        foreach( efVariablesWords( $langCode ) as $word => $trans )
-            $magicWords[$word] = $trans;
-        return true;
+	require_once( dirname( __FILE__ ) . '/Variables.i18n.php' );
+	foreach( efVariablesWords( $langCode ) as $word => $trans ) {
+		$magicWords[$word] = $trans;
+	}
+	return true;
 }
+
