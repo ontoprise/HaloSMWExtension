056c10b70887752b94d4e42b561843cb5752e9cf
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-10-21 11:30:11 +0000

    Update ImageMap extension. Added deploy descriptor and build file.
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@12537 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/ImageMap/ImageMap.php
+++ extensions/ImageMap/ImageMap.php
@@ -3,24 +3,21 @@
 $dir = dirname(__FILE__) . '/';
 $wgExtensionMessagesFiles['ImageMap'] = $dir . 'ImageMap.i18n.php';
 $wgAutoloadClasses['ImageMap'] = $dir . 'ImageMap_body.php';
-if ( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
-	$wgHooks['ParserFirstCallInit'][] = 'wfSetupImageMap';
-} else {
-	$wgExtensionFunctions[] = 'wfSetupImageMap';
-}
+$wgHooks['ParserFirstCallInit'][] = 'wfSetupImageMap';
+
+define('IM_VERSION', '{{$VERSION}} [B{{$BUILDNUMBER}}]');
 
 $wgExtensionCredits['parserhook']['ImageMap'] = array(
+	'path'           => __FILE__,
 	'name'           => 'ImageMap',
-	'svn-date' => '$LastChangedDate: 2008-06-06 20:38:04 +0000 (Fri, 06 Jun 2008) $',
-	'svn-revision' => '$LastChangedRevision: 35980 $',
 	'author'         => 'Tim Starling',
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:ImageMap',
 	'description'    => 'Allows client-side clickable image maps using <nowiki><imagemap></nowiki> tag.',
 	'descriptionmsg' => 'imagemap_desc',
+	'version'        => IM_VERSION
 );
 
-function wfSetupImageMap() {
-	global $wgParser;
-	$wgParser->setHook( 'imagemap', array( 'ImageMap', 'render' ) );
+function wfSetupImageMap( &$parser ) {
+	$parser->setHook( 'imagemap', array( 'ImageMap', 'render' ) );
 	return true;
 }
