27e552a11989672f5992ab6c35e4ba079b4a258f
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-10-24 13:30:52 +0000

    fix for 15857
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@12567 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/LiveQueries/SMW_LiveQueriesAjaxAccess.php
+++ extensions/SMWHalo/includes/LiveQueries/SMW_LiveQueriesAjaxAccess.php
@@ -5,22 +5,25 @@ $wgAjaxExportList[] = 'smwf_lq_refresh';
 
 function smwf_lq_refresh($id, $query){
 	
-			$query = trim(substr($query, 5, strlen($query)-9));
-			$query = str_replace(array('&lt;', '&gt;'), array('<','>'), $query);
+	//todo:find a better solution
+	//this is necessary, since occurences of ' ?' in the first line of the query are replaced by this string
+	$query = str_replace('&nbsp;?', ' ?', $query);
 	
-			global $wgParser, $wgOut;
-			if ( ($wgParser->getTitle() instanceof Title) && ($wgParser->getOptions() instanceof ParserOptions) ) {
-				$result = $wgParser->recursiveTagParse($query);
-			} else {
-				global $wgTitle;
-				$popt = new ParserOptions();
-				$popt->setEditSection(false);
-				$pout = $wgParser->parse($query . '__NOTOC__', $wgTitle, $popt);
-				// NOTE: as of MW 1.14SVN, there is apparently no better way to hide the TOC
-				SMWOutputs::requireFromParserOutput($pout);
+	$query = trim(substr($query, 5, strlen($query)-9));
+	$query = str_replace(array('&lt;', '&gt;'), array('<','>'), $query);
+	
+	global $wgParser, $wgOut;
+	if ( ($wgParser->getTitle() instanceof Title) && ($wgParser->getOptions() instanceof ParserOptions) ) {
+		$result = $wgParser->recursiveTagParse($query);
+	} else {
+		global $wgTitle;
+		$popt = new ParserOptions();
+		$popt->setEditSection(false);
+		$pout = $wgParser->parse($query . '__NOTOC__', $wgTitle, $popt);
+		// NOTE: as of MW 1.14SVN, there is apparently no better way to hide the TOC
+		SMWOutputs::requireFromParserOutput($pout);
 				
-				$result = $pout->getText();
-			}
-			return $result;
-	
+		$result = $pout->getText();
+	}
+	return $result;
 }
