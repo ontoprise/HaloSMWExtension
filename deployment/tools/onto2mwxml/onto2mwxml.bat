081f67002d96025e41bbb4cc9ec6bcac8d8083c9
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-10-24 13:06:20 +0000

    fix: 15894 - Ontology Upload broken
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@12565 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/onto2mwxml/onto2mwxml.bat
+++ deployment/tools/onto2mwxml/onto2mwxml.bat
@@ -1,21 +1,28 @@
 @echo off
 
-REM --- set this variable before using ---
+REM ------ set this variable to the TSC --------
+REM --- installation directory before using ----
  
 SET TSCDIR=
 
-REM --------------------------------------
+REM --------------------------------------------
 
-IF [%TSCDIR%]==[] goto installationHint
+IF ["%TSCDIR%"]==[""] goto installationHint
+IF NOT EXIST "%TSCDIR%" goto installationHint2
 SET CURRENT=%cd%
-cd %TSCDIR%
-%TSCDIR%\onto2mwxml.exe %*
-IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
-CD %CURRENT%
+cd "%TSCDIR%"
+%TSCDIR:~0,2%
+"%TSCDIR%\onto2mwxml.exe" %*
+REM IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
+CD "%CURRENT%"
 GOTO end
 
 :installationHint
-echo "Please set TSCDIR before using onto2mwxml."
+echo Please set TSCDIR before using onto2mwxml.
+EXIT 1
+
+:installationHint2
+echo Path of TSCDIR does not exist. Please set correctly.
 EXIT 1
 
 :end
