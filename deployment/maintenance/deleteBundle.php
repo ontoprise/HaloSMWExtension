8ae8b7b3901bc9e3c9a0ebf1988fe4b2995f5fc6
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-11-02 16:33:06 +0000

    deleteBundle maintenance script
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@12629 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

---
+++ deployment/maintenance/deleteBundle.php
@@ -0,0 +1,67 @@
+<?php
+/*  Copyright 2011, ontoprise GmbH
+ *
+ *   The deployment tool is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The deployment tool is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * @file
+ * @ingroup DFMaintenance
+ *
+ * Utility scripts for handling bundles.
+ * 
+ * This script removes pages of a bundle which are explicitly tagged 
+ * as part of the given bundle.
+ * 
+ * Usage: php deleteBundle.php -b <bundlename>
+ *
+ * @author Kai Kühn / ontoprise / 2011
+ *
+ */
+global $rootDir;
+$rootDir = dirname(__FILE__);
+$rootDir = str_replace("\\", "/", $rootDir);
+$rootDir = realpath($rootDir."/../");
+
+$mwrootDir = dirname(__FILE__);
+$mwrootDir = str_replace("\\", "/", $mwrootDir);
+$mwrootDir = realpath($mwrootDir."/../../");
+
+require_once($mwrootDir.'/deployment/io/DF_BundleTools.php');
+require_once($mwrootDir.'/deployment/io/DF_Log.php');
+require_once($mwrootDir.'/deployment/io/DF_PrintoutStream.php');
+
+$mediaWikiLocation = dirname(__FILE__) . '/../..';
+require_once "$mediaWikiLocation/maintenance/commandLine.inc";
+
+$dfgOut = DFPrintoutStream::getInstance(DF_OUTPUT_FORMAT_TEXT);
+$dfgOut->start(DF_OUTPUT_TARGET_STDOUT);
+$dfgOut->setMode(DF_OUTPUT_FORMAT_TEXT);
+
+
+$args = $_SERVER['argv'];
+for( $arg = reset( $args ); $arg !== false; $arg = next( $args ) ) {
+    if ($arg == '-b') {
+        $bundleID = next($args);
+        continue;
+    } 
+}
+
+if (!isset($bundleID)) {
+	print "\nUsage: php deleteBundle.php -b <bundlename>\n";
+	die();
+}
+
+$dfgOut->outputln("Delete pages of bundle '$bundleID'");
+DFBundleTools::deletePagesOfBundle($bundleID);
+
