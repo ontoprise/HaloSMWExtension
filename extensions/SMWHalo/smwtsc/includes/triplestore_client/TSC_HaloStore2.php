0b238eede39b1da547003e35dc8e7e1d03e82f28
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-11-15 11:49:18 +0000

    fix
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@12765 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/smwtsc/includes/triplestore_client/TSC_HaloStore2.php
+++ extensions/SMWHalo/smwtsc/includes/triplestore_client/TSC_HaloStore2.php
@@ -31,7 +31,7 @@ class SMWHaloStore2 extends SMWStoreAdapter {
 		$smw_ids =  $db->tableName('smw_ids');
 		$smw_urimapping = $db->tableName('smw_urimapping');
 		$id = $db->selectRow($smw_ids, array('smw_id'), array('smw_title'=>$subjectTitle->getDBkey(), 'smw_namespace'=>$subjectTitle->getNamespace()));
-		if (is_null($id)) return; // something is wrong. stop here
+		if ($id === false) return; // something is wrong. stop here
 		// delete mappings
 		$db->delete($smw_urimapping, array('smw_id' => $id->smw_id));
 	}
