077b271e233021db09cabbed4f67f8822d1afec7
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-11-17 10:19:29 +0000

    Bugfix 16063 - Adding a link to an article that contains " breaks the wikitext
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@12798 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/WYSIWYG/CKeditorSkin.body.php
+++ extensions/WYSIWYG/CKeditorSkin.body.php
@@ -262,6 +262,7 @@ class CKeditorSkin {
 		}
 
 		$u = $nt->getFullText();
+		$u = urlencode($u);  // Fix for links containing "
 		//#Updating links tables -> #Updating_links_tables
 		$u = str_replace( "#" . $nt->getFragment(), $nt->getFragmentForURL(), $u );
 
@@ -294,7 +295,7 @@ class CKeditorSkin {
 		$u = preg_replace( "/^RTECOLON/", ":", $u ); // change 'RTECOLON' => ':'
 		if( substr( $text, 0, 10 ) == 'RTENOTITLE' ){ // starts with RTENOTITLE
 			$args .= '_fcknotitle="true" ';
-			$title = $u;
+			$title = urldecode($u);
 			$trail = substr( $text, 10 ) . $trail;
 		}
 
@@ -314,6 +315,8 @@ class CKeditorSkin {
 		wfProfileIn( __METHOD__ );
 
 		$u = $nt->getFullText();
+		$u = urlencode($u);  // Fix for links containing "
+		
 		//#Updating links tables -> #Updating_links_tables
 		$u = str_replace( "#" . $nt->getFragment(), $nt->getFragmentForURL(), $u );
 
@@ -330,7 +333,7 @@ class CKeditorSkin {
 		$u = preg_replace( "/^RTECOLON/", ":", $u ); // change 'RTECOLON' => ':'
 		if( substr( $text, 0, 10 ) == 'RTENOTITLE' ){	// starts with RTENOTITLE
 			$args .= '_fcknotitle="true" ';
-			$title = $u;
+			$title = urldecode($u);
 			$trail = substr( $text, 10 ) . $trail;
 		}
 		$s = "<a {$args}href=\"{$u}\">{$title}</a>{$trail}";
