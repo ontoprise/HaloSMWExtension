9991c3cb36875e5d2a35417f35c27c701965bb72
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-12-05 09:50:45 +0000

    fix: 16233 - OntologyBrowser strips annotations when editing the range of a property
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@13000 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SMWHalo/includes/SMW_OntologyManipulator.php
+++ extensions/SMWHalo/includes/SMW_OntologyManipulator.php
@@ -667,7 +667,12 @@ function smwf_om_EditProperty($pagename, $newType, $newCard, $newRange, $oldRang
 	$oldDomainCategory = Title::newFromText($domainCategory, NS_CATEGORY);
 	if ($oldRange != '') {
 		$oldRangeCategory = Title::newFromText($oldRange, NS_CATEGORY);
-		$search = '/(\[\[(\s*)' . $hasDomainAndRangeProperty . '(\s*)::\s*'.$oldDomainCategory->getPrefixedText().'\s*;\s*'.$oldRangeCategory->getPrefixedText().'\s*(\|)?\s*\]\])/i';
+		$oldDomainCategoryQuoted = preg_quote($oldDomainCategory->getPrefixedText());
+		$oldDomainCategoryQuoted = str_replace("/", '\/', $oldDomainCategoryQuoted);
+		$oldRangeCategoryQuoted = preg_quote($oldRangeCategory->getPrefixedText());
+        $oldRangeCategoryQuoted = str_replace("/", '\/', $oldRangeCategoryQuoted);
+        
+		$search = '/(\[\[(\s*)' . $hasDomainAndRangeProperty . '(\s*)::\s*'.$oldDomainCategoryQuoted.'\s*;\s*'.$oldRangeCategoryQuoted.'\s*(\|)?\s*\]\])/i';
 		if ($newRange != '') {
 			$newRangeCategory = Title::newFromText($newRange, NS_CATEGORY);
 			$replace = '[[' . $hasDomainAndRangeProperty . '::'.$oldDomainCategory->getPrefixedText().'; '.$newRangeCategory->getPrefixedText().']]';
@@ -675,7 +680,9 @@ function smwf_om_EditProperty($pagename, $newType, $newCard, $newRange, $oldRang
 			$replace = '[[' . $hasDomainAndRangeProperty . '::'.$oldDomainCategory->getPrefixedText().']]';
 		}
 	} else {
-		$search = '/(\[\[(\s*)' . $hasDomainAndRangeProperty . '(\s*)::\s*'.$oldDomainCategory->getPrefixedText().'\s*(;)?\s*(\|)?\s*\]\])/i';
+		$oldDomainCategoryQuoted = preg_quote($oldDomainCategory->getPrefixedText());
+        $oldDomainCategoryQuoted = str_replace("/", '\/', $oldDomainCategoryQuoted);
+		$search = '/(\[\[(\s*)' . $hasDomainAndRangeProperty . '(\s*)::\s*'.$oldDomainCategoryQuoted.'\s*(;)?\s*(\|)?\s*\]\])/i';
 		if ($newRange != '') {
 			$newRangeCategory = Title::newFromText($newRange, NS_CATEGORY);
 			$replace = '[[' . $hasDomainAndRangeProperty . '::'.$oldDomainCategory->getPrefixedText().'; '.$newRangeCategory->getPrefixedText().']]';
@@ -683,7 +690,7 @@ function smwf_om_EditProperty($pagename, $newType, $newCard, $newRange, $oldRang
 			$replace = '[[' . $hasDomainAndRangeProperty . '::'.$oldDomainCategory->getPrefixedText().']]';
 		}
 	}
-
+//echo $search;die();
 	if (preg_match($search, $text) === 0) {
 		// replacement does not yet exist, so add it simply
 		$text .= "\n$replace";
