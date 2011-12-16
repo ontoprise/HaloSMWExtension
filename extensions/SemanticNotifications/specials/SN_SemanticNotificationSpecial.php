6e738f530d588ce33028de4b601f5635ac35a409
schweitz <schweitz@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-12-16 09:27:00 +0000

    Bugfix 16242 - Test directories in product download
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@13100 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/SemanticNotifications/specials/SN_SemanticNotificationSpecial.php
+++ extensions/SemanticNotifications/specials/SN_SemanticNotificationSpecial.php
@@ -1,21 +1,21 @@
 <?php
 /*
  * Copyright (C) Vulcan Inc.
- *
+* 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- *
+*
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
  * You should have received a copy of the GNU General Public License along
  * with this program.If not, see <http://www.gnu.org/licenses/>.
  *
- */
+*/
 
 /**
  * @file
@@ -56,10 +56,10 @@ class SemanticNotificationSpecial extends SpecialPage {
 		$imagepath = $sngScriptPath . '/skins/SemanticNotifications/images';
 		
 		// Development
-//		$wgOut->addScript("<script type='text/javascript' src='$sngScriptPath/scripts/steal/steal.js?sngui,development'> </script>");
+		$wgOut->addScript("<script type='text/javascript' src='$sngScriptPath/scripts/steal/steal.js?sngui,development'> </script>");
 
 		// Production
-		$wgOut->addScript("<script type='text/javascript' src='$sngScriptPath/scripts/steal/steal.js?sngui,production'> </script>");
+//		$wgOut->addScript("<script type='text/javascript' src='$sngScriptPath/scripts/steal/steal.js?sngui,production'> </script>");
 		
 		$wgOut->addHTML("<div id='sn-main-div' style='overflow:hidden'/>");
 		
