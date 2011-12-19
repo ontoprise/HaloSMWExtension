733d7ff5ecef02a2710c5e5c70fe28e8a27bc124
steinbauer <steinbauer@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2011-12-19 16:03:49 +0000

    web service component migration v2
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@13135 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/DataImport/tests/DataImportTests.php
+++ extensions/DataImport/tests/DataImportTests.php
@@ -44,7 +44,7 @@ class DataImportTests
 		//$suite->addTestSuite("TestWSUsage");
 		//$suite->addTestSuite("TestJSONProcessor");
 		//$suite->addTestSuite("TestTIReadPOP3");
-//		$suite->addTestSuite("TestWikipediaUltrapediaMerger");
+		//$suite->addTestSuite("TestWikipediaUltrapediaMerger");
 		//$suite->addTestSuite("TestLDConnector");
 		//$suite->addTestSuite("TestWSTriplifier");
 
