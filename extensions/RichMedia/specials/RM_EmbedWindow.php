00355179afcb920f792234cc475482a6514b590c
langguth <langguth@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2012-02-16 11:35:09 +0000

    FileViewer overlay improvements: Better scaling and less JavaScript overhead
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@13642 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- extensions/RichMedia/specials/RM_EmbedWindow.php
+++ extensions/RichMedia/specials/RM_EmbedWindow.php
@@ -21,12 +21,13 @@
 /**
  * @file
   * @ingroup RichMedia
-  * 
+  *
   * RM_EmbedWindow - used for displaying various types of files inline.
  *
  * @addtogroup SpecialPage
  *
  * @author Benjamin Langguth
+ * @author michel from our SMWforum community
  */
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( "This file is part of the RichMedia extension. It is not a valid entry point.\n" );
@@ -50,7 +51,6 @@ class RMEmbedWindow extends UnlistedSpecialPage {
 		$this->outputHeader();
 
 		doSpecialEmbedWindow();
-		
 	}
 }
 
@@ -71,35 +71,26 @@ function doSpecialEmbedWindow() {
 	$sk->initPage( $wgOut ); // need to call this to set skin name correctly
 	$form = new EmbedWindowForm( $wgRequest );
 	$form->execute();
-	if ( method_exists( $wgOut, 'addModules' ) ) {
-		$head_scripts = '';
-		$body_scripts = $wgOut->getHeadScripts( $sk );
-	} else {
-		$head_scripts = '';
-		$body_scripts = '';
-	}
 	$user_js =<<<HTML
-	<script type="{$wgJsMimeType}">
-var headID = document.getElementsByTagName("head")[0];
-var cssNode = document.createElement('link');
-	
-cssNode.type = 'text/css';
-cssNode.rel = 'stylesheet';
-cssNode.href = "{$smwgRMScriptPath}" + '/skins/richmedia_embedwindow.css';
-cssNode.media = 'screen, projection';
-headID.appendChild(cssNode);
+<script type="{$wgJsMimeType}">
+	var headID = document.getElementsByTagName( 'head' )[0],
+		cssNode = document.createElement( 'link' );
+
+	cssNode.type = 'text/css';
+	cssNode.rel = 'stylesheet';
+	cssNode.href = "{$smwgRMScriptPath}" + '/skins/richmedia_embedwindow.css';
+	cssNode.media = 'screen, projection';
+	headID.appendChild(cssNode);
 </script>
 HTML;
 	$text = <<<END
 <html xmlns="{$wgXhtmlDefaultNamespace}">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-$head_scripts
-$user_js
+	$user_js
 </head>
 <body>
-{$wgOut->getHTML()}
-$body_scripts
+	{$wgOut->getHTML()}
 </body>
 </html>
 
@@ -127,7 +118,7 @@ class EmbedWindowForm {
 	 * Variables
 	 * @access private
 	 */
-	var $mTarget, $mfullResSize, $mPdfHeight, $mPdfWidth;	
+	var $mTarget, $mfullResSize, $mPdfHeight, $mPdfWidth;
 
 	/**
 	 * start doing stuff
@@ -135,7 +126,7 @@ class EmbedWindowForm {
 	 */
 	function execute(){
 		wfProfileIn( __METHOD__ . ' [Rich Media]' );
-		global $wgOut, $wgUser, $smwgRMScriptPath, 
+		global $wgOut, $wgUser, $smwgRMScriptPath,
 			$smwgRMEWEnableResizing, $smwgRMEWAllowScroll,
 			$smwgRMEWMinWidth, $smwgRMEWMinHeight,
 			$wgServer, $wgJsMimeType;
@@ -155,17 +146,18 @@ class EmbedWindowForm {
 		$embedHeight = $image->getHeight();
 		$embedMIMEType = $image->getMimeType();
 
-		$fullRes_fit_section = "";
+		$fullRes_fit_section = '';
 		list( $major, $minor ) = LocalFile::splitMime( $image->getMimeType() );
-		if ($major == "image") {
+		if( $major == 'image' ) {
 			$embedObject = <<<END
 			<table id="rmEWEmbedTable">
 				<tr><td id="rmEWAlCenter">
-					<img id="rmEWEmbeddedObject" src="{$imagePath}" width="{$embedWidth}" height="{$embedHeight}"/>
+					<img id="rmEWEmbeddedObject" src="{$imagePath}"
+						width="{$embedWidth}" height="{$embedHeight}"/>
 				</td></tr>
 			</table>
 END;
-			
+
 			if( $smwgRMEWEnableResizing ) {
 				$newWidth = $embedWidth;
 				$newHeight = $embedHeight;
@@ -175,7 +167,7 @@ END;
 				if ( $embedHeight < $smwgRMEWMinHeight ) {
 					$newHeight = $smwgRMEWMinHeight;
 				}
-				switch($smwgRMEWAllowScroll) {
+				switch( $smwgRMEWAllowScroll ) {
 					case true:
 						$scroll = 1;
 						break;
@@ -183,37 +175,72 @@ END;
 						$scroll = 0;
 						break;
 				}
-				
+
 				$embedObject .= <<<HTML
 <script type="{$wgJsMimeType}">
-	var screenHeight = top.screen.availHeight, screenWidth = top.screen.availWidth,
-		paddingTotal = 20, extraHeight = 60; // some extra space to avoid Scrollbars
-	extraWidth = 20, outer = top.jQuery('#fancybox-wrap'), 
-	inner = top.jQuery('#fancybox-content');
+	var outer = top.jQuery('#fancybox-wrap'),
+		inner = top.jQuery('#fancybox-content'),
+		// size of all the borders, paddings and margins around the embedded object.
+		// defined in CSS file. Includes 10px padding, 8px margin, 2px borderspacing
+		// (all both left&right), 22px main text height
+		paddingHeight = 67,
+		// includes 10px padding, 8px margin, 2px borderspacing (all both left&right)
+		paddingWidth = 40,
+		// size of overlay window should be relative to browser window size.
+		// Minimum size can be specified
+		windowHeight = Math.max( top.window.outerHeight - 200,
+			{$smwgRMEWMinHeight} + paddingHeight + 20 ),
+		windowWidth  = Math.max( top.window.outerWidth - 100,
+			{$smwgRMEWMinWidth} + paddingWidth + 20 ),
+		// the maximum size an image should have after resize
+		imageMaxHeight = windowHeight - paddingHeight,
+		imageMaxWidth  = windowWidth - paddingWidth,
+		scaleHeight,
+		scaleWidth,
+		scaleFactor;
 
-	if ({$scroll} !== 1 && ( {$newWidth} > screenWidth || {$newHeight} > screenHeight) ) {
-		outer.css({ 
-			height: screenHeight - 250, 
-			width: screenWidth - 100 
-		}); 
-		inner.css({ 
-			height: screenHeight - 220 - extraHeight, 
-			width: screenWidth - 100 - extraWidth 
+	// now check if we have to resize
+	// --> image size > image max size in one direction at least
+	if ( {$scroll} !== 1 && ( {$embedWidth} > imageMaxWidth ||
+		{$embedHeight} > imageMaxHeight) )
+	{
+		// first, calculate the scaling factors required to resize image
+		scaleHeight = imageMaxHeight / {$embedHeight};
+		scaleWidth = imageMaxWidth / {$embedWidth};
+
+		// dimension which has to be scaled down most wins!
+		scaleFactor = Math.min( scaleHeight, scaleWidth );
+
+		// now set outer and inner size. If one dimension falls below minimum size,
+		// then set to minimum. This avoids too little overlays and takes care
+		// that the caption line is displayed correctly
+		outer.css({
+			height: Math.max( {$embedHeight} * scaleFactor,
+				{$smwgRMEWMinHeight} ) + paddingHeight + 20,
+			width: Math.max( {$embedWidth} * scaleFactor,
+				{$smwgRMEWMinWidth} ) + paddingWidth + 20
 		});
-		picture = document.getElementById('rmEWEmbeddedObject');
-		picture.setAttribute( 'height', screenHeight - 220 - extraHeight - paddingTotal );
-		picture.setAttribute( 'width', screenWidth - 100 - extraWidth - paddingTotal );
+		inner.css({
+			height: Math.max( {$embedHeight} * scaleFactor,
+				{$smwgRMEWMinHeight} ) + paddingHeight,
+			width: Math.max( {$embedWidth} * scaleFactor,
+				{$smwgRMEWMinWidth} ) + paddingWidth
+		});
+		picture = document.getElementById( 'rmEWEmbeddedObject' );
+		picture.setAttribute( 'height', {$embedHeight} * scaleFactor );
+		picture.setAttribute( 'width', {$embedWidth} * scaleFactor );
 	} else {
-		outer.css({ 
-			height: {$newHeight} + paddingTotal + extraHeight, 
-			width: {$newWidth} + paddingTotal + extraWidth 
-		}); 
-		inner.css({ 
-				height: {$newHeight} + extraHeight, 
-				width: {$newWidth} + extraWidth 
-			});
+		// adjusted to match above variables and calculations
+		outer.css({
+			height: {$newHeight} + paddingHeight + 20,
+			width: {$newWidth} + paddingWidth + 20
+		});
+		inner.css({
+			height: {$newHeight} + paddingHeight,
+			width: {$newWidth} + paddingWidth
+		});
 	}
-	top.jQuery.fancybox.center(true); 
+	top.jQuery.fancybox.center( true );
 </script>
 HTML;
 			}
@@ -222,12 +249,13 @@ HTML;
 			// We could also use the new HTML5 tags <audio> and <video>.
 			// But they are not supported by IE and Firefox is already playing
 			// some audio files (i.e. ogg vorbis) without the need of an additional plugins.
-			$noEmbedMsg = wfMsg('smw_rm_noembed', $embedMIMEType);
-			$embedLoading = wfMsg('smw_rm_embedload');
+			$noEmbedMsg = wfMsg( 'smw_rm_noembed', $embedMIMEType );
+			$embedLoading = wfMsg( 'smw_rm_embedload' );
 			$embedObject = <<<END
 			<div id="rmEWEmbedded">
-				<object data="{$imagePath}" type="{$embedMIMEType}" id="rmEWEmbeddedObject"
-					width="95%" height="85%" standby="{$embedLoading}">
+				<object data="{$imagePath}" type="{$embedMIMEType}"
+					id="rmEWEmbeddedObject" width="95%" height="85%"
+					standby="{$embedLoading}">
 					<p>{$noEmbedMsg}</p>
 				</object>
 			</div>
@@ -245,25 +273,27 @@ END;
 			<span id="rmEWMainText">FileViewer</span>
 			{$fullRes_fit_section}
 			<span id="rmEWright">
-				<a class="rmEWWhite" href="{$nt->getFullURL()}" title="{$descLinkAlt}" target="_top">>>&nbsp;{$descText}</a>
+				<a class="rmEWWhite" href="{$nt->getFullURL()}"
+					title="{$descLinkAlt}" target="_top">>>&nbsp;{$descText}
+				</a>
 			</span>
 		</div>
 END;
 		$html .= $embedObject;
-		$wgOut->addHTML($html);
+		$wgOut->addHTML( $html );
 
 		wfProfileOut( __METHOD__ . ' [Rich Media]' );
 	}
-	
+
 	/**
 	 * Show an Error
 	 */
-	function showError($description){
+	function showError( $description ) {
 		global $wgOut;
-		$wgOut->setPageTitle( wfMsg( "internalerror" ) );
-		$wgOut->setRobotPolicy( "noindex,nofollow" );
+		$wgOut->setPageTitle( wfMsg( 'internalerror' ) );
+		$wgOut->setRobotPolicy( 'noindex,nofollow' );
 		$wgOut->setArticleRelated( false );
 		$wgOut->enableClientCache( false );
-		$wgOut->addWikiText( $description );		
+		$wgOut->addWikiText( $description );
 	}
 }
