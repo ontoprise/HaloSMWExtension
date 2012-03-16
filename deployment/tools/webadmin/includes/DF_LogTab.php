4302f7534125b3ba91cbbd8da337601bc322dd3c
kuehn <kuehn@f80b7aa0-dfcc-4e65-a0d9-74108174cbc8>
2012-03-16 21:52:01 +0000

    multi-column log file list
    git-svn-id: http://halodev.ontoprise.com/svn/HaloSMWExtension/trunk@13910 f80b7aa0-dfcc-4e65-a0d9-74108174cbc8

--- deployment/tools/webadmin/includes/DF_LogTab.php
+++ deployment/tools/webadmin/includes/DF_LogTab.php
@@ -48,38 +48,58 @@ class DFLogTab {
 
 	public function getHTML() {
 		global $dfgLang, $wgServer, $wgScriptPath;
-		
+
 		$html = "<div style=\"margin-bottom: 10px;\">".$dfgLang->getLanguageString('df_webadmin_logtab_description')."</div>";
 		$html .= "<input type=\"button\" value=\"".$dfgLang->getLanguageString('df_webadmin_clearlog')."\" id=\"df_clearlog\"></input>";
-		
+
 		$html .= "<div id=\"df_log_results_table_container\">";
 		$html .= "<table id=\"df_log_results_table\">";
-		$html .= "<th>";
-		$html .= $dfgLang->getLanguageString('df_webadmin_loglink');
-		$html .= "</th>";
-		$html .= "<th>";
-		$html .= $dfgLang->getLanguageString('df_webadmin_logdate');
-		$html .= "</th>";
-		
+		$header = "<th>";
+		$header .= $dfgLang->getLanguageString('df_webadmin_loglink');
+		$header .= "</th>";
+		$header .= "<th>";
+		$header .= $dfgLang->getLanguageString('df_webadmin_logdate');
+		$header .= "</th>";
+		$header = str_repeat($header, 4);
+		$html .= $header;
+
 		$logLinkHint = $dfgLang->getLanguageString('df_loglink_hint');
-		
+
 		$readLogLinkTemplate = '<a target="_blank" href="'.$wgServer.$wgScriptPath.'/deployment/tools/webadmin/index.php'.
 						'?action=ajax&rs=readLog&rsargs[]=$1$2" title="'.$logLinkHint.'">Log</a>';
-		
+
 		$logs = $this->getLogs();
 		$i = 0;
-		foreach($logs as $l) {
-			list($name, $date, $type) = $l;
+		$totalHeight = (count($logs) % 4 == 0) ? intval(count($logs) / 4) : intval(count($logs) / 4) + 1;
+
+		for($k = 0; $k < $totalHeight; $k++) {
+			list($name1, $date1, $type1) = $logs[$k];
 			$j = $i % 2;
 			$html .= "<tr class=\"df_row_$j\">";
-			$html .= "<td class=\"df_log_link\">";
-			$readLogLink = str_replace('$1', $name, $readLogLinkTemplate);
-			$readLogLink = str_replace('$2', "&rsargs[]=$type", $readLogLink);
-			$html .= "$readLogLink";
-			$html .= "</td>";
-			$html .= "<td class=\"df_log_link\">";
-			$html .= date ("F d Y H:i:s.", $date);
-			$html .= "</td>";
+
+			$html .= $this->getColumn($name1, $date1, $type1, $readLogLinkTemplate);
+
+			if ($k+$totalHeight < count($logs)) {
+				list($name2, $date2, $type2) = $logs[$k+$totalHeight];
+				$html .= $this->getColumn($name2, $date2, $type2, $readLogLinkTemplate);
+			} else {
+				$html .= $this->getEmptyColumn();
+			}
+
+			if ($k+$totalHeight*2 < count($logs)) {
+				list($name3, $date3, $type3) = $logs[$k+$totalHeight*2];
+				$html .= $this->getColumn($name3, $date3, $type3, $readLogLinkTemplate);
+			} else {
+				$html .= $this->getEmptyColumn();
+			}
+
+			if ($k+$totalHeight*3 < count($logs)) {
+				list($name4, $date4, $type4) = $logs[$k+$totalHeight*3];
+				$html .= $this->getColumn($name4, $date4, $type4, $readLogLinkTemplate);
+			} else {
+				$html .= $this->getEmptyColumn();
+			}
+
 			$html .= "</tr>";
 			$i++;
 		}
@@ -88,9 +108,27 @@ class DFLogTab {
 		return $html;
 	}
 
+	private function getColumn($name, $date, $type, $readLogLinkTemplate) {
+		$html = "<td class=\"df_log_link\">";
+		$readLogLink = str_replace('$1', $name, $readLogLinkTemplate);
+		$readLogLink = str_replace('$2', "&rsargs[]=$type", $readLogLink);
+		$html .= "$readLogLink";
+		$html .= "</td>";
+		$html .= "<td class=\"df_log_link\">";
+		$html .= date ("F d Y H:i:s.", $date);
+		$html .= "</td>";
+		return $html;
+	}
+
+	private function getEmptyColumn() {
+		$html = "<td class=\"df_log_link\"></td>";
+		$html .= "<td class=\"df_log_link\"></td>";
+		return $html;
+	}
+
 	/**
 	 * Read log directory and returns a descending sorted list of entries.
-	 * 
+	 *
 	 * @return tuple[] (filename, modification timestamp)
 	 */
 	private function getLogs() {
@@ -120,7 +158,7 @@ class DFLogTab {
 		usort($result, array($this, "cmpLogEntry"));
 		return $result;
 	}
-	
+
 	private function cmpLogEntry($a, $b) {
 		list($file1, $ts1, $type1) = $a;
 		list($file2, $ts2, $type2) = $b;
